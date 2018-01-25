c8e34da3a034852d9a101f91e395e8d5fcc761d3
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2018-01-25 16:12:43 +0100

    cleaned description

--- R/magpie4-package.R
+++ R/magpie4-package.R
@@ -2,22 +2,13 @@
 
 #' MAgPIE outputs R library for MAgPIE version 4.x
 #' 
-#' A library containing the most common output routines for extracting outputs from the MAgPIE model
+#' Common output routines for extracting results from the MAgPIE framework (versions 4.x).
 #' 
-#' \tabular{ll}{ 
-#' Package: \tab magpie4\cr 
-#' Type: \tab Package\cr 
-#' Version: \tab 0.0026\cr 
-#' Date: \tab 2016-11-03\cr 
-#' License: \tab LGPL-3\cr 
-#' LazyLoad: \tab
-#' yes\cr }
 #' 
 #' @name magpie4-package
 #' @aliases magpie4-package magpie4
 #' @docType package
-#' @author Jan Philipp Dietrich, Florian Humpenoeder, Markus Bonsch, Benjamin Leon Bodirsky, Misko Stevanovic, Ina Neher
-#' 
+#' @author 
 #' Maintainer: Jan Philipp Dietrich <dietrich@pik-potsdam.de>
 NULL
 
