3c1db22e49145cb1b1d16985025a430701635506
Kristine Karstens <karstens@pik-potsdam.de>
2018-04-27 14:01:04 +0200

    add description of summation placement to getReport

--- R/getReport.R
+++ R/getReport.R
@@ -10,6 +10,26 @@
 #' @param detail Crop specific (TRUE) or aggregated outputs (FALSE)
 #' @param ... additional arguments for write.report. Will only be taken into account if argument "file" is not NULL. 
 #' @return A MAgPIE object containing the report in the case that "file" is NULL.
+#' @details Reports are organize with '|' as level delimiter and summation symbols for grouping subcategories into entities e.g. for stackplots. Notice the following hints for the summation symbol placement:
+#' \itemize{
+#'   \item Every name should just contain one summation symbol (mostly '+').
+#'   \item The position of the symbol (counted in '|' from left side) will determine the level.
+#'   \item Every subitem containing the same summation symbol in the same level with the same supercategory name will be summed.
+#'   \item Items without any summation symbol will ge ignored.
+#'   \item Items with different summation symbols will be summed up separately.
+#'   \item In most of the cases a summation symbol will be just placed before the last level (counted in '|' from left side).
+#'   \item It is helpful to think about which group of items should be stacked in a stackplot.
+#' }
+#'   An example how a summation symbol placement could look like: 
+#'   \preformatted{  Toplevel  
+#'   Toplevel|+|Item 1
+#'   Toplevel|+|Item 2
+#'   Toplevel|Item 2|+|Subitem 1
+#'   Toplevel|Item 2|+|Subitem 1
+#'   Toplevel|++|Item A
+#'   Toplevel|++|Item B
+#'   Toplevel|Item ?} 
+#'  
 #' @author Florian Humpenoeder
 #' @importFrom magclass write.report2 getSets<- getSets add_dimension
 #' @importFrom methods is
