fb0b99f92cb1f5abdcfb2804e2130691f1c3ccc7
bodirsky <bodirsky@pik-potsdam.de>
2019-02-06 09:40:14 +0100

    Adding SDG1 and SDG12

---
+++ R/reportSDG1.R
@@ -0,0 +1,29 @@
+#' @title reportSDG1
+#' @description reports all SDG indicators relevant for SDG1 - Poverty
+#' 
+#' @export
+#' 
+#' @param gdx GDX file
+#' @return MAgPIE object
+#' @author Benjamin Bodirsky
+#' @examples
+#' 
+#'   \dontrun{
+#'     x <- reportSDG3(gdx)
+#'   }
+#' 
+
+reportSDG1 <- function(gdx) {
+  x <- NULL
+  
+  indicatorname="SDG|SDG1|Per-capita income"
+  unit="USD05/cap/yr"
+  out <- income(gdx,level="regglo",per_capita = TRUE,after_shock = TRUE)
+  getNames(out) <- paste0(indicatorname, " (",unit,")")
+  x <- mbind(x,out)
+  
+  
+  #x <- x[,,sort(getNames(x))]  
+  return(x)
+}
+
