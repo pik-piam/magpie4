fcdb15411a5720310ec60f44dbf2c3dbb797c222
FeBeier <felicitas.beier@pik-potsdam.de>
2019-02-07 14:29:15 +0100

    SDG6 and SDG9

--- R/reportSDG6.R
+++ R/reportSDG6.R
@@ -48,9 +48,10 @@ reportSDG6 <- function(gdx) {
   
   indicatorname="SDG|SDG6|Fertilizer use"	
   unit="Mt N/yr"
-  #missing  (in MAgPIE!)
-  # getNames(out) <- paste0(indicatorname, " (",unit,")")
-  # x <- mbind(x,out)
+  # Def.: Nitrogen fertilizer use (retrieved from NitrogenBudget.R --> UNIT??)
+  out <- collapseNames(readGDX(gdx,"ov_nr_inorg_fert_reg",format="first_found",select=list(type="level"))[,,"crop"])
+  getNames(out) <- paste0(indicatorname, " (",unit,")")
+  x <- mbind(x,out)
   
   indicatorname="SDG|SDG6|Nitrate concentration in water"	
   unit="tN/km3"
@@ -61,13 +62,15 @@ reportSDG6 <- function(gdx) {
   indicatorname="SDG|SDG6|Water use efficiency"	
   unit="rate"
   #missing
+  # Def.: a country's total gross domestic product (GDP) divided by total freshwater withdrawals (OurWorldInData: sdg-tracker.org????)
   # getNames(out) <- paste0(indicatorname, " (",unit,")")
   # x <- mbind(x,out)
   
-  indicatorname="SDG|SDG6|Water stress"	
+  indicatorname="SDG|SDG6|Water stress|Agriculture"	
   unit="fraction"
-  # freshwater withdrawals as a proportion of available freshwater
-  out <- water_avail(gdx,level="regglo")
+  # Def.: total quantity of freshwater withdrawals (agriculture, industry, domestic) as a share of available freshwater resources
+  # Def. (HERE): freshwater withdrawals [here: for agriculture] (water_usage, km^3/yr) as a proportion of available freshwater (water_avail, km^3)
+  out <- water_usage(gdx,level="regglo",users="kcr",sum=TRUE,digits=3)/water_avail(gdx,level="regglo")
   getNames(out) <- paste0(indicatorname, " (",unit,")")
   x <- mbind(x,out)
   
@@ -80,12 +83,16 @@ reportSDG6 <- function(gdx) {
   indicatorname="SDG|SDG6|Environmental flow exceedance"	
   unit="percentage of land area"
   #missing (given in MAgPIE)
-  # getNames(out) <- paste0(indicatorname, " (",unit,")")
-  # x <- mbind(x,out)
+  # Def.: Level of environmental water flow exceedance (of agirculture in growing season)
+  # Def. (MAgPIE): water environmental flow violation volume
+  #out <- WaterEFVvolume(gdx)
+  #getNames(out) <- paste0(indicatorname, " (",unit,")")
+  #x <- mbind(x,out)
   
   indicatorname="SDG|SDG6|Agricultural water use"	
   unit="km3/yr"
-  out <- reportWaterUsage(gdx)
+  # Def.: water usage in agriculture 
+  out <- water_usage(gdx,level="regglo",users="kcr",sum=TRUE,digits=3)
   getNames(out) <- paste0(indicatorname, " (",unit,")")
   x <- mbind(x,out)
   
