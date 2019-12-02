a1b86d61bf416a4f685b8bcbb31ec668acc4b085
florianh <humpenoeder@pik-potsdam.de>
2019-12-02 10:27:29 +0900

    added food price index for plant-based and animal-based products to reporting

--- R/reportPriceFoodIndex.R
+++ R/reportPriceFoodIndex.R
@@ -14,8 +14,20 @@
 #' 
 
 reportPriceFoodIndex <- function(gdx){
+  a <- NULL
+  
   x <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2010")
   getNames(x) <- "Prices|Food Price Index (Index 2010=100)"
+  a <- mbind(a,x)
+
+  x <- priceIndex(gdx,level="regglo", products="kfo_pp", baseyear = "y2010")
+  getNames(x) <- "Prices|Food Price Index|Plant-based Products (Index 2010=100)"
+  a <- mbind(a,x)
+  
+  x <- priceIndex(gdx,level="regglo", products="kfo_ap", baseyear = "y2010")
+  getNames(x) <- "Prices|Food Price Index|Animal-based Products (Index 2010=100)"
+  a <- mbind(a,x)
+  
   # x <- summationhelper(x)
-  return(x)
+  return(a)
 }
