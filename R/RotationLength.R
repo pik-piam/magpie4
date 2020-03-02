75ca504cdac59c57bc4ddcdebf64da62619f4d30
abhimishr <mishra.abhijeet28@gmail.com>
2020-03-02 15:06:34 +0100

    Bugfix in rotation length reporting

--- R/RotationLength.R
+++ R/RotationLength.R
@@ -24,8 +24,8 @@ RotationLength <- function(gdx, file=NULL, level="regglo"){
   if (level != "regglo"){
     cat("NULL. Set level to regglo.")
   } else{
-    harvest_rl   <- readGDX(gdx,"p32_rotation_cellular_harvesting")[,readGDX(gdx,"t"),]
-    estb_rl <- readGDX(gdx,"pm_rot_length_estb")[,readGDX(gdx,"t"),]
+    harvest_rl   <- readGDX(gdx,"p32_rotation_cellular_harvesting")[,readGDX(gdx,"t"),] * 5
+    estb_rl <- readGDX(gdx,"p32_rotation_cellular_estb")[,readGDX(gdx,"t"),] * 5
     a <- mbind(setNames(estb_rl,"Establishment"),setNames(harvest_rl,"Harvest"))
     a <- superAggregate(data = a,aggr_type = "mean",level = "regglo")
     a <- round(a,0)
