186b55c1cdceb8e8e1fbbc95dbadaa175d6bbb13
florianh <humpenoeder@pik-potsdam.de>
2020-05-09 23:01:22 +0200

    added option to use pop as weight for global co2 price reporting

--- R/PriceGHG.R
+++ R/PriceGHG.R
@@ -6,8 +6,11 @@
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
+#' @param aggr aggregation used for global value; "max" (maxium value accross regions) or "weight" (weighted by population)
 #' @return GHG emission prices as MAgPIE object (US$2005/tCO2, US$2005/tN2O, US$2005/tCH4)
 #' @author Florian Humpenoeder, Amsalu W. Yalew
+#' @importFrom gdx readGDX
+#' @importFrom luscale superAggregate
 #' @seealso \code{\link{reportPriceGHG}}
 #' @examples
 #' 
@@ -16,13 +19,19 @@
 #'   }
 #' 
 
-PriceGHG <- function(gdx, file=NULL, level="reg") {
+PriceGHG <- function(gdx, file=NULL, level="reg", aggr="max") {
   reg <- readGDX(gdx,"im_pollutant_prices")
-  reg <- reg[,,c("co2_c","n2o_n_direct","ch4")]
+  reg <- reg[,readGDX(gdx,"t"),c("co2_c","n2o_n_direct","ch4")]
 #  reg <- reg/0.967 #USD2004 -> USD2005
   reg[,,"co2_c"] <- reg[,,"co2_c"]*12/44 #US$/tC -> US$/tCO2
   reg[,,"n2o_n_direct"] <- reg[,,"n2o_n_direct"]*28/44 #US$/tN -> US$/tN2O
-  glo <- as.magpie(apply(reg,2:3,max))
+  if(aggr=="max") {
+    glo <- as.magpie(apply(reg,2:3,max))
+  } else if (aggr=="weight") {
+    weight <- reg
+    weight[,,] <- population(gdx,level="reg")
+    glo <- superAggregate(reg,level="glo",weight=weight,aggr_type = "weighted_mean")
+  }
   if(level == "reg") x <- reg
   else if (level == "glo") x <- glo
   else if (level == "regglo") x <- mbind(reg,glo)
