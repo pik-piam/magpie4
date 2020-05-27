b9f63df628d7d447ceb4058534340693ed80a85a
pvjeetze <vjeetze@pik-potsdam.de>
2020-05-27 23:38:50 +0200

    Functions BII.R and reportBII.R for extraction of biodiversity intactness index

---
+++ R/BII.R
@@ -0,0 +1,35 @@
+#' @title Biodiversity Intactness Index
+#' @description reads biodiversity intactness index out of a MAgPIE gdx file
+#' 
+#' @export
+#'
+#' @param gdx GDX file
+#' @param file a file name the output should be written to using write.magpie
+#' @details Calculates global and regional biodiversity intactness index
+#' @return Biodiversity intactness index (unitless)
+#' @author Patrick v. Jeetze, Florian Humpenoeder
+#' @importFrom gdx readGDX out
+#' @importFrom magclass dimSums 
+#' @importFrom luscale superAggregate
+#' @examples
+#' 
+#'   \dontrun{
+#'     x <- bii(gdx)
+#'   }
+
+bii <- function(gdx, file=NULL, level="glo") {
+  a <- readGDX(gdx,"ov44_biodiv",select = list(type="level"))
+  if(!is.null(a)) {
+  a <- dimSums(a,dim=3)
+  rr <- readGDX(gdx,"f44_rr_layer") #includes range rarity layer
+  area <- land(gdx,level="cell",sum = TRUE)
+  reg <- superAggregate(a,level="reg",aggr_type = "sum")/superAggregate(area*rr,level="reg",aggr_type = "sum")
+  glo <- superAggregate(a,level="glo",aggr_type = "sum")/superAggregate(area*rr,level="glo",aggr_type = "sum")
+  if(level == "reg") {x <- reg}
+  else if (level == "glo") {x <- glo}
+  else if (level == "regglo") {x <- mbind(reg,glo)}
+  } else x <- NULL
+  out(x,file)
+}
+
+
--- R/getReport.R
+++ R/getReport.R
@@ -136,6 +136,7 @@ getReport <- function(gdx,file=NULL,scenario=NULL,filter=c(1,2,7),detail=TRUE,..
                     "reportTimberDemand(gdx)",
                     "reportTimberProductionVolumetric(gdx)",
                     "reportTimberDemandVolumetric(gdx)",
+                    "reportBII.R",
                     gdx=gdx))
   
   message(paste0("Total runtime:  ",format(t["elapsed"], nsmall = 2, digits = 2),"s"))
