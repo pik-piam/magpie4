a264e098b29217655b32e55b5a72c8980f86ebd1
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2020-07-01 12:52:05 +0200

    removed dependency of priceIndex.R to superAggregateX added priceIndexFood estimating costs closer to final demand.

--- R/priceIndexFood.R
+++ R/priceIndexFood.R
@@ -1,4 +1,4 @@
-#' @title priceIndex
+#' @title priceIndexFood
 #' @description calcluates price indicies based on a MAgPIE gdx file
 #' 
 #' @export
@@ -6,56 +6,70 @@
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
-#' @param products Selection of products (either by naming products, e.g. "tece", or naming a set,e.g."kcr")
 #' @param index "lasp" (Laspeyres-Index: baseyear weighting), "paas" (Paasche-Index: current weighting), "fish" (Fisher-Index: geometric mean of "lasp" and "paas")
 #' @param chain Chain Index: if true, the base period for each time period is the immediately preceding time period. Can be combined with all of the above indices
-#' @param baseyear baseyear of the price index
+#' @param baseyear baseyear of the price index. type model to take baseyear 2010 with literature prices
 #' @param round shall the results be rounded?
-#' @param type For whom are the prices important? "producer" are the prices that farmer face, as they also produce intermediate products (seed, feed). "consumer" are the prices for the end consumer faces (supermarket, bioenergy plant). Currently, the only difference is the basket composition (ideally, also prices should differ between regions)
 #' @param product_aggr aggregate over products or not (boolean)
 #' @return A MAgPIE object containing price indices for consumers or producers (depending on type)
 #' @author Jan Philipp Dietrich, Florian Humpenoeder, Benjamin Bodirsky
 #' @examples
 #' 
 #'   \dontrun{
-#'     x <- priceIndex(gdx)
+#'     x <- priceIndexFood(gdx)
 #'   }
 #' 
 #' @importFrom magclass as.magpie
 
-priceIndex <- function (gdx, file=NULL, level = "reg", products = "kall", index = "lasp", chain=FALSE, baseyear="y2005", round=TRUE, type="consumer",product_aggr=TRUE) {
+priceIndexFood <- function (gdx, file=NULL, level = "reg", index = "lasp", chain=FALSE, baseyear="y2005", round=TRUE, product_aggr=TRUE) {
+  products="kfo"
   if (chain==FALSE) {
     if (index == "lasp" | index == "paas") {
-      if(type=="producer") {
-        q_t <- readGDX(gdx,"ov_supply",select = list(type="level"),react = "warning")
-        p_t <- prices(gdx,type="producer")
-      } else if (type =="consumer") {        
-        q_t <- dimSums(demand(gdx)[,,c("food","other_util","bioenergy")],dim=3.1)
-        p_t <- prices(gdx,type="consumer",products=products)                                
-      } else {stop("invalid type")}
+     
+      q_t <-  Kcal(gdx=gdx,
+                   level="iso",
+                   calibrated=TRUE,
+                   after_shock = TRUE,
+                   products=products,
+                   product_aggr = FALSE,
+                   per_capita=FALSE
+      )
+      
+      p_t <- readGDX(gdx,"p15_prices_kcal")    
+      
+      
       if (!all(products%in%findset("kall"))) products<-readGDX(gdx, products)
-      if (suppressWarnings(is.null(readGDX(gdx,"fcostsALL")))) {
-        # products <- products[-which(products=="wood")]
-        # products <- products[-which(products=="woodfuel")]
-        if ("wood" %in% products)     products <- products[-which(products=="wood")]
-        if ("woodfuel" %in% products) products <- products[-which(products=="woodfuel")]
-      } 
+
 
       p_t <- p_t[, , products]
       q_t <- q_t[, , products]
+      
+
+      if(baseyear=="model"){
+        modelyear=TRUE
+        baseyear="y2010"
+      } else {
+        modelyear=FALSE
+      }
+      
       # check if the baseyear is contained in the gdx  
       if(!baseyear %in% getYears(q_t)) {
         miss_year <- baseyear
         q_t <- time_interpolate(q_t, baseyear, integrate_interpolated_years=TRUE)
         p_t <- time_interpolate(p_t, baseyear, integrate_interpolated_years=TRUE)
       }
-      q_0 <- q_t
-      p_0 <- p_t
-      for (year in getYears(q_0)) {
-        q_0[, year, ] <- setYears(q_t[, baseyear,],year)
-        p_0[, year, ] <- setYears(p_t[, baseyear,],year)
-      }        
       
+      q_0 <- q_t
+      q_0[, , ] <- setYears(q_t[, baseyear,],NULL)
+      
+      if(modelyear==TRUE){
+        p_0 <- p_t
+        p_0 [,,] <- readGDX(gdx,"i15_prices_initial_kcal")[, , products]
+      } else {
+        p_0 <- p_t
+        p_0[, , ] <- setYears(p_t[, baseyear,],NULL)
+      }
+
       if (index == "lasp") {
         if (product_aggr) {
           dividend <- dimSums(p_t * q_0,dim=3,na.rm=TRUE)
@@ -77,7 +91,6 @@ priceIndex <- function (gdx, file=NULL, level = "reg", products = "kall", index 
       dividend <- gdxAggregate(gdx = gdx, x=dividend, absolute = TRUE, to=level)
       divisor <- gdxAggregate(gdx = gdx, x=divisor, absolute = TRUE, to=level)
       px <- dividend/divisor
-      
     } else if (index == "fish") {
       lasp <- priceIndex(gdx, level = level, products = products, 
                          chain=chain, baseyear=baseyear,
