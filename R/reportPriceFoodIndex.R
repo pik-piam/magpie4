38ea0b234b2464daac5630679179323e2550c76b
FeBeier <felicitas.beier@pik-potsdam.de>
2020-12-16 15:27:42 +0100

    added sub-food-price indeces: plant-based food products and livestock food products

--- R/reportPriceFoodIndex.R
+++ R/reportPriceFoodIndex.R
@@ -14,8 +14,19 @@
 #' 
 
 reportPriceFoodIndex <- function(gdx){
-  x <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2010")
-  getNames(x) <- "Prices|Food Price Index (Index 2010=100)"
+  # all food products
+  x1 <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2010")
+  getNames(x1) <- "Prices|Food Price Index (Index 2010=100)"
+  
+  # plant-based food products
+  x2 <- priceIndex(gdx,level="regglo", products="kfo_pp", baseyear = "y2010")
+  getNames(x2) <- "Prices|Food Price Index|Plant-based food products (Index 2010=100)"
+  
+  # livestock food products
+  x3 <- priceIndex(gdx,level="regglo", products="kfo_lp", baseyear = "y2010")
+  getNames(x3) <- "Prices|Food Price Index|Livestock food products (Index 2010=100)"
+  
+  x <- mbind(x1, x2,x3)
   # x <- summationhelper(x)
   return(x)
 }
