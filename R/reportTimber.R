b2dfe1a0ad79a9879f11651dfae6dfb04798c3bf
abhimishr <mishra.abhijeet28@gmail.com>
2020-12-23 14:25:07 +0100

    Merging timber production and demand reporting in one

---
+++ R/reportTimber.R
@@ -0,0 +1,37 @@
+#' @title reportTimber
+#' @description reports MAgPIE demand for timber.
+#' 
+#' @export
+#' 
+#' @param gdx GDX file
+#' @return Timber demand
+#' @author Abhijeet Mishra
+#' @examples
+#' 
+#'   \dontrun{
+#'     x <- reportTimber(gdx)
+#'   }
+#' 
+
+reportTimber<-function(gdx){
+  a <- NULL
+  
+  if(suppressWarnings(!is.null(readGDX(gdx,"fcostsALL")))){
+    a <- Timber(gdx,level = "regglo")
+    getNames(a,dim=2) <- reportingnames(getNames(a,dim=2))
+    a <- add_columns(x = a,dim = 3.2,addnm = "Roundwood")
+    a[,,"Roundwood"] <- a[,,"Industrial roundwood"] + a[,,"Wood fuel"]
+    
+    dem <- collapseNames(a[,,"Demand"])
+    getNames(dem) <- paste0("Timber|volumetric|Demand|",getNames(dem))
+    getNames(dem) <- paste0(getNames(dem)," (Mm3/yr)")
+    
+    prod <- collapseNames(a[,,"Production"])
+    getNames(prod) <- paste0("Timber|volumetric|Production|",getNames(prod))
+    getNames(prod) <- paste0(getNames(prod)," (Mm3/yr)")
+    
+    out <- mbind(dem,prod)
+  } else {cat("Disabled for magpie run without dynamic forestry. ")}
+  
+  return(out)
+}
