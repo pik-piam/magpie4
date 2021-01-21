422312b5f0f64713446a1b4f571011fe8f27cf8c
FeBeier <felicitas.beier@pik-potsdam.de>
2021-01-21 15:51:25 +0100

    included change of baseyear option in reportPriceFoodIndex as argument

--- R/reportPriceFoodIndex.R
+++ R/reportPriceFoodIndex.R
@@ -4,6 +4,8 @@
 #' @export
 #' 
 #' @param gdx GDX file
+#' @param baseyear baseyear of the price index
+
 #' @return Food price index as MAgPIE object Unit: see names
 #' @author Florian Humpenoeder
 #' @examples
@@ -13,18 +15,18 @@
 #'   }
 #' 
 
-reportPriceFoodIndex <- function(gdx){
+reportPriceFoodIndex <- function(gdx, baseyear = "y2010"){
   # all food products
-  x1 <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2010")
-  getNames(x1) <- "Prices|Food Price Index (Index 2010=100)"
+  x1 <- priceIndex(gdx,level="regglo", products="kfo", baseyear = baseyear)
+  getNames(x1) <- paste0("Prices|Food Price Index (Index ",gsub("\\y","",baseyear),"=100)")
   
   # plant-based food products
-  x2 <- priceIndex(gdx,level="regglo", products="kfo_pp", baseyear = "y2010")
-  getNames(x2) <- "Prices|Food Price Index|Plant-based food products (Index 2010=100)"
+  x2 <- priceIndex(gdx,level="regglo", products="kfo_pp", baseyear = baseyear)
+  getNames(x2) <- paste0("Prices|Food Price Index|Plant-based food products (Index ",gsub("\\y","",baseyear),"=100)")
   
   # livestock food products
-  x3 <- priceIndex(gdx,level="regglo", products="kfo_lp", baseyear = "y2010")
-  getNames(x3) <- "Prices|Food Price Index|Livestock food products (Index 2010=100)"
+  x3 <- priceIndex(gdx,level="regglo", products="kfo_lp", baseyear = baseyear)
+  getNames(x3) <- paste0("Prices|Food Price Index|Livestock food products (Index ",gsub("\\y","",baseyear),"=100)")
   
   x <- mbind(x1, x2,x3)
   # x <- summationhelper(x)
