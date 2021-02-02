fd8bb2295949f673eb01023b739a218cb8290a38
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2021-02-02 12:28:55 +0100

    bugfix

--- R/reportNitrogenBudgetNonagland.R
+++ R/reportNitrogenBudgetNonagland.R
@@ -23,18 +23,31 @@ reportNitrogenBudgetNonagland<-function(gdx, grid=FALSE, dir=".", spamfiledirect
   if (grid==FALSE){
     
     budget<-NitrogenBudgetNonagland(gdx,level="reg",dir=dir)
+    budget<-dimSums(budget,dim=3.2)
+
+    surplus = budget[,,"surplus"]
+    inputs = budget[,,c("fixation_freeliving","deposition")]
+    
+    helper=function(prefix,x) {
+      total = dimSums(x,dim=3) 
+      getNames(total)<-prefix
+      getNames(x)<-reportingnames(getNames(x))
+      getNames(x)<-paste0(prefix,"|+|",getNames(x))
+      return(mbind(total,x))
+    }
+    surplus = helper(prefix="Resources|Nitrogen|Non-Agricultural Land Budget|Balance",x=surplus)
+    inputs  = helper(prefix="Resources|Nitrogen|Non-Agricultural Land Budget|Inputs",x=inputs)
+    out<-mbind(surplus, inputs)
     
   } else if (grid == TRUE){
-    
+  
     
     budget<-NitrogenBudgetNonagland(gdx,level="grid",dir=dir)
     out<-dimSums(budget,dim=3.2)
-    
-    getNames(out)<-reportingnames(getNames(out))
-    
-    #out=metadata_comments(out = out, unit = "Mt Nr/yr", description = "Nitrogen budget for non-agricultural land, sum over all land types excluding pasture and cropland",note = "")
-    
-  } else {warning("grid has to be boolean")}
+    getNames(surplus)<-reportingnames(getNames(out))
+  }else {warning("grid has to be boolean")}
+
   
+  #out=metadata_comments(out = out, unit = "Mt Nr/yr", description = "Nitrogen budget for non-agricultural land, sum over all land types excluding pasture and cropland",note = "")
   return(out)
 }
