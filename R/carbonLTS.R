09fa8232d82257365eac897f1d2b70c67d5c0377
abhimishr <mishra.abhijeet28@gmail.com>
2021-03-17 09:10:27 +0100

    updated HWP accounting in emissions

--- R/carbonLTS.R
+++ R/carbonLTS.R
@@ -7,11 +7,11 @@
 #' @param file a file name the output should be written to using write.magpie
 #' @param level Level of regional aggregation; "cell", "reg" (regional), "glo" (global), "regglo" (regional and global) or any secdforest aggregation level defined in superAggregate
 #' @param unit element" or "gas"; "element": co2_c in Mt C/yr, n2o_n in Mt N/yr, ch4 in Mt CH4/yr; "gas": co2_c Mt CO2/yr, n2o_n in Mt NO2/yr, ch4 in Mt CH4/yr
-#' @param half_life Half life in years for decay in wood products loosing half theor carbon content. (35 yrs is deafault)
-#' @details Annual (and cumulative) Carbon stored in harvested wood products as well as slow emissions from half life deacy.
+#' @param sl_oir_pw Half life values for sawlogs (sl) other industrial roundwood (oir) and pulpwood (pw). DO NOT DEVIATE FROM DEFAULT VALUES WITHOUT EXTENSIVE TESTING.
 #' @param cumulative Logical; Determines if cHWP emissions are reported annually (FALSE) or cumulative (TRUE). The starting point for cumulative emissions is y1995.
 #' @param baseyear Baseyear used for cumulative emissions (default = 1995)
 #' @return carbon stocks in MtC from harvested timber
+#' @details Annual (and cumulative) Carbon stored in harvested wood products as well as slow emissions from half life deacy.
 #' @author Abhijeet Mishra, Florian Humpenoeder
 #' @importFrom gdx readGDX out
 #' @importFrom magclass clean_magpie dimSums collapseNames setYears write.magpie
@@ -19,137 +19,233 @@
 #' @importFrom utils tail
 #' @examples
 #'
-#'   \dontrun{
-#'     x <- carbonLTS(gdx)
-#'   }
-
-carbonLTS <- function(gdx, file=NULL, level="cell",unit="element", half_life=35, cumulative=FALSE, baseyear=1995){
-
+#' \dontrun{
+#' x <- carbonLTS(gdx)
+#' }
+#'
+carbonLTS <- function(gdx,
+                      file = NULL,
+                      level = "cell",
+                      unit = "element",
+                      cumulative = FALSE,
+                      baseyear = 1995,
+                      sl_oir_pw = c(30, 20, 2)) {
   timber <- FALSE
-  fore_red <- readGDX(gdx,"ov_hvarea_forestry","ov32_hvarea_forestry","ov32_land_reduction","ov_forestry_reduction",select = list(type="level"),react = "silent", format="first_found")
+  fore_red <- readGDX(gdx, "ov_hvarea_forestry", "ov32_hvarea_forestry", "ov32_land_reduction", "ov_forestry_reduction", select = list(type = "level"), react = "silent", format = "first_found")
   if (!is.null(fore_red)) {
     if (max(fore_red) > 1) {
-      if(readGDX(gdx,"s73_timber_demand_switch","sm_timber_demand_switch")){
+      if (readGDX(gdx, "s73_timber_demand_switch", "sm_timber_demand_switch")) {
         timber <- TRUE
       }
     }
   }
 
-  if (timber){
+  if (timber) { ## read wood and woodfuel from a model run
+    kforestry <- readGDX(gdx, "kforestry")
 
-    kforestry <- readGDX(gdx,"kforestry")
+    ## Production
+    inflow <- collapseNames(readGDX(gdx, "ov_prod")[, , kforestry][, , "level"]) ## mio. tDM
 
-    ### Convert from annual values to total values
-    timestep_length <- readGDX(gdx,"im_years",react="silent")
-    if(is.null(timestep_length)) timestep_length <- timePeriods(gdx)
-    timestep_length[,1,] <- 5
+    ### Read volume information
+    volume <- readGDX(gdx, "f73_volumetric_conversion")
+    if (is.null(volume)) volume <- 0.6
 
-    ## AgeClass
-    #ac_sub <- readGDX(gdx,"ac_sub")
+    ## Convert to Volume -- mio.tDM to mio.m3
+    inflow <- inflow / volume
 
-    ### Production of wood and woodfuel (tDM)
-    # pm_carbon_density_ac_forestry <- collapseNames(readGDX(gdx,"pm_carbon_density_ac_forestry")[,,"vegc"])
-    # pm_carbon_density_ac          <- collapseNames(readGDX(gdx,"pm_carbon_density_ac")[,,"vegc"])
-    # vm_forestry_reduction         <- collapseNames(readGDX(gdx,"ov32_land_reduction")[,,"plant"][,,"level"])
-    # vm_secdforest_reduction       <- collapseNames(readGDX(gdx,"ov35_secdforest_reduction")[,,"level"])
-    # vm_primforest_reduction       <- collapseNames(readGDX(gdx,"ov35_primforest_reduction")[,,"level"])
-    # vm_other_reduction            <- collapseNames(readGDX(gdx,"ov35_other_reduction")[,,"level"])
-    #
-    # prod_forestry   <- vm_forestry_reduction[,,ac_sub]   * pm_carbon_density_ac_forestry[,getYears(timePeriods(gdx)),ac_sub]
-    # prod_secdforest <- vm_secdforest_reduction[,,ac_sub] * pm_carbon_density_ac[,getYears(timePeriods(gdx)),ac_sub]
-    # prod_primforest <- vm_primforest_reduction           * pm_carbon_density_ac[,getYears(timePeriods(gdx)),"acx"]
-    # prod_other      <- vm_other_reduction[,,ac_sub]      * pm_carbon_density_ac[,getYears(timePeriods(gdx)),ac_sub]
-
-    inflow   <- collapseNames(readGDX(gdx,"ov_prod")[,,kforestry][,,"level"]) ## mio. tDM
-    inflow   <- inflow/readGDX(gdx,"f73_volumetric_conversion") ## Convert to Volume -- mio.tDM to mio.m3
     ## Conversion factor 230kgC/m3 -- Table S2 in SI of https://doi.org/10.1088/1748-9326/7/3/034023
     ## Unit below = million KgC
-    inflow   <- inflow*230
-    inflow   <- inflow/1e3   ## Conversion to million ton C
+    inflow <- inflow * 230 ## bit lower than mean value
+    inflow <- inflow / 1e3 ## Conversion to million ton C
+
+    ## Find out which years have to be extrapolated - DONT GO TOO BACK IN TIME TO AVOID NEGATIVE VALUES from time_interpolate
+    all_years <- paste0("y", 1970:2150)
+
+    ## Find misssing year
+    missing <- setdiff(all_years, getYears(inflow))
+
+    ## Interpolate for missing years
+    inflow <- time_interpolate(
+      dataset = inflow,
+      interpolated_year = missing,
+      integrate_interpolated_years = T,
+      extrapolation_type = "linear"
+    )
+
+    ## Regional end use demand
+    prod_specific <- readGDX(gdx, "p73_forestry_demand_prod_specific")
+
+    ## Extract pulpwood demand
+    ## See Figure 5 in DOI 10.1186/s13021-015-0016-7
+    prod_specific[, , "pulpwood"] <- prod_specific[, , "industrial_roundwood"] - (prod_specific[, , "sawlogs_and_veneer_logs"] + prod_specific[, , "other_industrial_roundwood"])
+
+    ## Interpolate
+    prod_specific <- time_interpolate(
+      dataset = prod_specific,
+      interpolated_year = missing,
+      integrate_interpolated_years = T,
+      extrapolation_type = "linear"
+    )
 
 
-    ## Inflow of the carbon stock calculation is based on www.pnas.org/cgi/doi/10.1073/pnas.1904231116 equation 3
-    ## Inflow of carbon is calculated and not tDM because IPCC describes inflow in Gg C yr-1 from eq. 2.8.5
-    ## Only locally produced stuff has to be accounted (inflow is already locally produced)
+    irw <- c("sawlogs_and_veneer_logs", "other_industrial_roundwood", "pulpwood")
+    if (any(sum(dimSums(prod_specific[, , "industrial_roundwood"], dim = c(1, 3)) - dimSums(prod_specific[, , irw], dim = c(1, 3))) > 1)) message("Sum of subcategories for industrial roundwood do not match. Check script.")
 
-    calc_yrs <- getYears(inflow)
+    ## Isolate industrial roundwood
+    end_use_share <- prod_specific[, , irw]
 
+    ## Calculate share for each wood category
+    end_use_share <- end_use_share / dimSums(end_use_share, dim = c(1, 3))
+
+    ## Calculate global share (this is approximation)
+    end_use_share <- dimSums(end_use_share, dim = 1)
+
+    ## Disaggregate inflow into categories
+    inflow_disagg <- collapseNames(inflow[, , "wood"]) * end_use_share[, getYears(inflow), ] ## mio. tC
+    # dimSums(inflow_disagg, dim = c(1, 3))
+    # dimSums(inflow_disagg, dim = c(1))
+    # dimSums(inflow[, , "wood"], dim = 1)
+
+
+    ## IPCC method to calculate HWO stock
+    ## See Equation 4 in DOI 10.1186/s13021-015-0016-7
+    ## See Equation in section 2.1 page 2 in DOI http://dx.doi.org/10.1088/1748-9326/7/3/034023
+    ## See Equation 12.2 in "2019 Refinement to the 2006 IPCC Guidelines for National Greenhouse Gas Inventories"
+    ## "Chapter 12: Harvested Wood Products"
+    ## https://www.ipcc-nggip.iges.or.jp/public/2019rf/pdf/4_Volume4/19R_V4_Ch12_HarvestedWoodProducts.pdf
+
+    ## Some Definitions
     ## k= decay constant of FOD for each HWP category (HWPj)
     ## given in units yr-1(k= ln(2)/HL, where HL is half-life of the HWP pool in years (see Section 2.8.3.2).
-    ## Assumed 35 years here
 
-    k <- round(log(2)/(half_life),4)
+    ## Create half life data frame
+    half_life_df <- data.frame(
+      var = c(irw),
+      half_life = sl_oir_pw
+    )
 
-    decay_alpha <- exp(-k)
-    limiter_beta <- ((1-decay_alpha)/k)
+    k <- round(log(2) / (as.magpie(half_life_df)), 8) ## See http://dx.doi.org/10.1088/1748-9326/7/3/034023 or IPCC doc linked above
 
-    stock   <- new.magpie(cells_and_regions = getCells(inflow),years = getYears(inflow),fill = 0)
+    alpha <- exp(-k) ## See http://dx.doi.org/10.1088/1748-9326/7/3/034023 or IPCC doc linked above
 
-    ## Old Stock
-    previous_stock <- new.magpie(cells_and_regions = "GLO",years = c("y1985","y1990"),fill = c(5450,5750)) # in MtC SI https://doi.org/10.1088/1748-9326/7/3/034023
-    previous_stock <- previous_stock*(setYears(inflow[,1,]/dimSums(inflow[,1,],dim=1),NULL))
-    dimSums(previous_stock,dim=1)
+    beta <- ((1 - alpha) / k) ## See http://dx.doi.org/10.1088/1748-9326/7/3/034023 or IPCC doc linked above
 
-    ## Old Inflow
-    previous_inflow <- new.magpie(cells_and_regions = "GLO",years = c("y1985","y1990"),fill = c(290,310)) # in MtC SI https://doi.org/10.1088/1748-9326/7/3/034023
-    previous_inflow <- previous_inflow*(setYears(inflow[,1,]/dimSums(inflow[,1,],dim=1),NULL))
+    ## Create empty object
+    stock <- inflow_disagg
+    stock[stock > 0] <- 0
 
-    ## 1995 Calculations
-    stock[,"y1995",]    = (decay_alpha * setYears(previous_stock[,"y1990","wood"],NULL)) + (limiter_beta * setYears(previous_inflow[,"y1990","wood"],NULL))
+    ## Create old Stock
+    ## See Excel file linked in SI of http://dx.doi.org/10.1088/1748-9326/7/3/034023
+    previous_stock <- new.magpie(cells_and_regions = "GLO", years = c("y1970"), fill = c(4271)) # in MtC SI https://doi.org/10.1088/1748-9326/7/3/034023
+    previous_stock <- collapseNames(setYears(previous_stock
+    * (setYears(inflow_disagg[, "y1970", ]
+      / dimSums(inflow_disagg[, "y1970", ],
+          dim = c(1, 3)
+        ), NULL)), NULL))
+    # dimSums(previous_stock, dim = 1)
 
-    ## Inflow has to be in MtC/yr and Stock in MtC
-    for (i in 1:(length(calc_yrs)-1)) {
-      stock[,i+1,]    = (decay_alpha * stock[,i,]) + (limiter_beta * inflow[,i,"wood"])
+    ## Create first inflow
+    first_inflow <- new.magpie(cells_and_regions = "GLO", years = c("y1971"), fill = c(220)) # in MtC SI https://doi.org/10.1088/1748-9326/7/3/034023
+    first_inflow <- collapseNames(setYears(first_inflow * (setYears(inflow_disagg[, "y1995", ] / dimSums(inflow_disagg[, "y1995", ], dim = c(1, 3)), NULL)), NULL))
+    # dimSums(first_inflow, dim = 1)
+
+    ## Disaggregate first stock
+    stock[, "y1970", ] <- dimSums(previous_stock, dim = 3) * end_use_share[, "y1970", ]
+    # dimSums(stock, dim = c(1, 3))[, 1994:2000]
+
+    ## From this stock, some outflow will happen, due to decay
+
+    ## Create outlow
+    outflow <- inflow_disagg
+    outflow[outflow != 0] <- 0
+    # dimSums(outflow, dim = c(1, 3))
+
+    ## Create annual inflow
+    annual_inflow <- outflow
+
+    ## Create stock staying stock
+    stock_in_place <- stock
+
+    ## Create inflow to be added
+    inflow_to_add <- inflow_disagg
+    inflow_to_add[inflow_to_add != 0] <- 0
+
+    for (i in 1:(length(getYears(stock)) - 1)) { ## See http://dx.doi.org/10.1088/1748-9326/7/3/034023 for description of this algorithm
+
+      ## Stock in place
+      stock_in_place[, i, ] <- alpha * stock[, i, ]
+
+      ## How much new inflow comes
+      inflow_to_add[, i, ] <- beta * inflow_disagg[, i, ]
+
+      # Calculate outflow
+      outflow[, i, ] <- stock[, i, ] - stock_in_place[, i, ]
+
+      # Update stock for next step
+      stock[, i + 1, ] <- stock_in_place[, i, ] + inflow_to_add[, i, ]
     }
 
-    temp <- NULL
-    for(y in 1:(length(getYears(stock))-1)){
-      ## Diff is carbon in HWP - Description of EQUATION 2.8.5 in 2013 Revised Supplementary Methods and Good Practice Guidance Arising from the Kyoto Protocol
-      diff <- setYears(stock[,y+1,],NULL)-stock[,y,]
-      diff <- diff/timestep_length[,y,]
-      if(timestep_length[,y,]>5){
-        diff <- diff*timestep_length[,y,]/5
-      }
-      temp <- mbind(temp,diff)
-    }
+    # print(dimSums(stock,dim=c(1))[,1994:2008,])
+    #  dimSums(stock,dim=c(1,3))[,1994:2008,]
+    #
+    #  print(dimSums(inflow_to_add,dim=c(1),na.rm=T)[,1994:2008,])
+    #  dimSums(inflow_to_add,dim=c(1,3),na.rm=T)[,1994:2008,]
+    #  #dimSums(annual_inflow,dim=c(1,3),na.rm=T)[,1994:2008,]
+    #
+    #  print(dimSums(outflow,dim=c(1))[,1994:2008,])
+    #  dimSums(outflow,dim=c(1,3))[,1994:2008,]
+    #
+    #  print(dimSums(inflow_to_add-outflow,dim=c(1))[,1994:2008,])
+    #  dimSums(inflow_to_add-outflow,dim=c(1,3))[,1994:2008,]
+    #  # dimSums(annual_inflow-outflow,dim=c(1,3))[,1994:2008,]
 
-    missing_yr <- setdiff(getYears(stock),getYears(temp))
-    temp <- mbind(temp,setYears(temp[,tail(getYears(temp),1),]*1.05,missing_yr))
-    dimSums(temp,dim=1)
+    ## Overall wood and woodfuel category
+    overall_wood_removal <- readGDX(gdx, "ov_prod", select = list(type = "level"))[, , kforestry] ## MtDM
 
-    annual_cHWP <- temp
+    ## Convert to Volume -- mio.tDM to mio.m3
+    overall_wood_removal <- overall_wood_removal / volume
 
-    ## Find out how much is lost every year (based on half life)
-    ## The annual net carbon sink in long-lived  products is derived as the difference between the
-    ## C stocks of two subsequent years.
+    ## Conversion factor 230kgC/m3 -- Table S2 in SI of https://doi.org/10.1088/1748-9326/7/3/034023
+    ## Unit below = million KgC
+    overall_wood_removal <- overall_wood_removal * 230 ## bit lower than mean value
+    overall_wood_removal <- overall_wood_removal / 1e3 ## Conversion to million ton C
 
-    emis_cHWP <- NULL
+    ## Interpolate
+    overall_wood_removal <- time_interpolate(
+      dataset = overall_wood_removal,
+      interpolated_year = missing,
+      integrate_interpolated_years = T,
+      extrapolation_type = "linear"
+    )
 
-    for (i in 1:length(calc_yrs)) {
-      temp <- inflow[,i,"wood"] - inflow[,i,"wood"] * decay_alpha
-      emis_cHWP <- mbind(emis_cHWP,temp)
-    }
-
-    a <- mbind(setNames(annual_cHWP,"storage"),setNames(emis_cHWP,"decay"))
+    ## Bind together
+    a <- mbind(
+      setNames(overall_wood_removal[, , "wood"], "emis_wood"),
+      setNames(overall_wood_removal[, , "woodfuel"], "emis_woodfuel"),
+      setNames(-1 * dimSums(stock, dim = 3), "anthropogenic_stock"),
+      setNames(-1 * dimSums(inflow_to_add, dim = 3), "annual_inflow"),
+      setNames(dimSums(outflow, dim = 3), "annual_outflow"),
+      setNames(dimSums(-1 * inflow_to_add + outflow, dim = 3), "net_sink_HWP")
+    )
 
     if (cumulative) {
-      years <- getYears(a,as.integer = T)
-      im_years <- new.magpie("GLO",years,NULL)
-      im_years[,,] <- c(1,diff(years))
-      a[,"y1995",] <- 0
-      a <- a*im_years[,getYears(a),]
-      a <- as.magpie(apply(a,c(1,3),cumsum))
-      a <- a - setYears(a[,baseyear,],NULL)
+      years <- getYears(a, as.integer = T)
+      im_years <- new.magpie("GLO", years, NULL)
+      im_years[, , ] <- c(1, diff(years))
+      a[, "y1995", ] <- 0
+      a <- a * im_years[, getYears(a), ]
+      a <- as.magpie(apply(a, c(1, 3), cumsum))
+      a <- a - setYears(a[, baseyear, ], NULL)
     }
 
-    if (level != "cell") a <- superAggregate(a, aggr_type = "sum", level = level,na.rm = FALSE)
+    if (level != "cell") a <- superAggregate(a, aggr_type = "sum", level = level, na.rm = FALSE)
 
     ### Conversion based on unit
-    if(unit=="gas") a <- a * 44/12
-    } else {
-      a <- NULL
-      message("Disabled for magpie run without timber production.")
-    }
+    if (unit == "gas") a <- a * 44 / 12
+  } else {
+    a <- NULL
+    message("Disabled for magpie run without timber production.")
+  }
 
-  out(a,file)
+  out(a, file)
 }
