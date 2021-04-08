e851d974530cc5f11cee2764079df2f8dec01d0f
Marcos Alves <mppalves@gmail.com>
2021-04-08 18:48:55 +0200

    reportCattle

--- R/getReport.R
+++ R/getReport.R
@@ -147,6 +147,7 @@ getReport <- function(gdx,file=NULL,scenario=NULL,filter=c(1,2,7),detail=TRUE,..
                     "reportBII(gdx)",
                     "reportPriceWoodyBiomass(gdx)",
                     "reportCarbonstock(gdx)",
+                    "reportCattle(gdx)",
                     gdx=gdx))
 
   message(paste0("Total runtime:  ",format(t["elapsed"], nsmall = 2, digits = 2),"s"))
---
+++ R/reportCattle.R
@@ -0,0 +1,40 @@
+#' @title reportCattle
+#' @description reports cattle related numbers
+#'
+#' @export
+#'
+#' @param gdx GDX file
+#' @return Cattle values as magpie objetc
+#' @author Marcos Alves
+#' @examples
+#' \dontrun{
+#' x <- reportCattle(gdx)
+#' }
+#'
+reportCattle <- function(gdx) {
+
+  # read in data
+  past_ha_c <- readGDX(gdx, "ov31_past_area", format = "simplest")[, , list("past_mngt" = "cont_grazing", "type" = "level", "w" = "rainfed")]
+  past_yld_m <- readGDX(gdx, "ov_past_yld", format = "simplest")[, , list("past_mngt" = "mowing", "type" = "level", "w" = "rainfed")]
+  past_ha_m <- readGDX(gdx, "ov31_past_area", format = "simplest")[, , list("past_mngt" = "mowing", "type" = "level", "w" = "rainfed")]
+  lsu_ha <- readGDX(gdx, "ov31_lsu_ha", format = "simplest")[, , list("type" = "level")]
+  total_grazing_cattle_c <- past_ha_c * lsu_ha
+  total_mowing_cattle <- past_yld_m * past_ha_m / (8.9 * 365 / 1000) # (lsu equivalent anual consumption in tDM)
+
+  # aggregate and add global
+  total_grazing_cattle_c_reg <- gdxAggregate(gdx, total_grazing_cattle_c, to = "regglo", absolute = T)
+  total_mowing_cattle_reg <- gdxAggregate(gdx, total_mowing_cattle, to = "regglo", absolute = T)
+  lsu_ha_reg <- gdxAggregate(gdx, lsu_ha, to = "regglo", weight = past_ha_c, absolute = F)
+  past_ha_c_reg <- gdxAggregate(gdx, past_ha_c, to = "regglo", absolute = T)
+  past_ha_m_reg <- gdxAggregate(gdx, past_ha_m, to = "regglo", absolute = T)
+
+  # aggreate and rename
+  x <- NULL
+  x <- mbind(x, setNames(lsu_ha_reg, "Cattle|Continuous grazing|Density (Lsu per ha)"))
+  x <- mbind(x, setNames(total_grazing_cattle_c_reg, "Cattle|Continuous grazing|Total lsu (millions)"))
+  x <- mbind(x, setNames(total_mowing_cattle_reg, "Cattle|Mowing|Total lsu (millions)"))
+  x <- mbind(x, setNames(past_ha_c_reg, paste0("Resources|Land Cover|+|", reportingnames("past"), "|Continuous grazing", " (million ha)")))
+  x <- mbind(x, setNames(past_ha_m_reg, paste0("Resources|Land Cover|+|", reportingnames("past"), "|Mowing", " (million ha)")))
+
+  return(x)
+}
