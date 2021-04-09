3a0c9fdf9f284ed6948d99e9b84a35dde7f7edfe
Marcos Alves <mppalves@gmail.com>
2021-04-09 13:33:34 +0200

    pasture soil carbon fix

--- R/reportPastSoilCarbon.R
+++ R/reportPastSoilCarbon.R
@@ -16,10 +16,11 @@ reportPastSoilCarbon <- function(gdx) {
   # read in data
   soilc_cg_target <- readGDX(gdx, "ov31_soilc_target", format = "simplest")[, , list("type" = "level")]
   past_ha_c <- readGDX(gdx, "ov31_past_area", format = "simplest")[, , list("past_mngt" = "cont_grazing", "type" = "level", "w" = "rainfed")]
-
+  soilc_cg_target <- soilc_cg_target/past_ha_c
+  soilc_cg_target[is.nan(soilc_cg_target)] <- 0
+  soilc_cg_target[is.infinite(soilc_cg_target)] <- 0
   soilc_cg_target_reg <- gdxAggregate(gdx, soilc_cg_target, to = "regglo", weight = past_ha_c, absolute = F)
-  soilc_cg_change_reg <- soilc_cg_target_reg-soilc_cg_target_reg[,1,]
-  soilc_cg_change_reg[is.nan(soilc_cg_change_reg)] <- 0
+  soilc_cg_change_reg <- soilc_cg_target_reg - soilc_cg_target_reg[,1,]
   getYears(soilc_cg_change_reg) <- getYears(soilc_cg_target_reg)
   
   # aggreate and rename
