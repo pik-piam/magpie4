4ed7e924e84b772da182bc8af36b44279ef3465a
Debbora Leip <leip@pik-potsdam.de>
2021-04-16 11:50:54 +0200

    fixed typo in reportCostsFertilizer

--- R/reportCostsFertilizer.R
+++ R/reportCostsFertilizer.R
@@ -16,7 +16,7 @@ reportCostsFertilizer <- function(gdx){
   nr_fertilizer_costs <- CostsFertilizer(gdx, level = "regglo")
   # no phosphorus fertililzer costs in MAgPIE
   
-  nr_fertilizer_costs <- reporthelper(nr_fertilizer_costs, dim = 3.1, level_zero_name = "Costs|Fertiliizer", detail = TRUE)
+  nr_fertilizer_costs <- reporthelper(nr_fertilizer_costs, dim = 3.1, level_zero_name = "Costs|Fertilizer", detail = TRUE)
   nr_fertilizer_costs <- summationhelper(nr_fertilizer_costs)
   
   getNames(nr_fertilizer_costs) <- paste0(getNames(nr_fertilizer_costs), " (million US$05/yr)")
