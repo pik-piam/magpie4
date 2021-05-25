25c15a188a3c3afae3e99ded1317610a7e9f3155
pvjeetze <vjeetze@pik-potsdam.de>
2021-05-25 11:19:21 +0200

    updated MapLandUseForDownscaling.R

--- R/MapLandUseForDownscaling.R
+++ R/MapLandUseForDownscaling.R
@@ -1,40 +1,47 @@
 #' @title MapLandUseForDownscaling
-#' @description Modifies magpie land use output so that it can be fed into a downscaling model for generating high resolution land use maps.  
+#' @description Modifies magpie land use output so that it can be fed into a downscaling model for generating high resolution land use maps.
 #'
 #' @importFrom magclass mbind read.magpie dimSums
-#' @importFrom luscale superAggregate
 #' @importFrom gdx out
+#' @importFrom magpiesets findset
 #' @export
 #'
 #' @param file a file name the output should be written to using write.magpie
-#' @param dir output directory which contains cellular magpie output and the the luh2v2 initialisation data for primary and secondary vegetation ("cell.luh2v2initial_primsecdother_0.5_share.mz").
+#' @param dir output directory which contains cellular magpie output.
 #' @param downscaling_model The only current option is "SEALS" (Spatial Economic Allocation Landscape Simulator).
+#' @param selectyears Years to provide data for.
 #' @return Shares of different land use classes per grid sell as magpie object.
 #' @author Patrick v. Jeetze
-#' @seealso \code{\link{PrimSecdOtherLand}}
 #' @examples
-#'
 #' \dontrun{
 #' x <- MapLandUseForDownscaling(dir = ".", downscaling_model = "SEALS")
 #' }
 #'
-MapLandUseForDownscaling <- function(dir = ".", file = NULL, downscaling_model = "SEALS") {
-  
+MapLandUseForDownscaling <- function(dir = ".", file = NULL, downscaling_model = "SEALS", selectyears = c("y2015", "y2050")) {
   downscaling_model_list <- c( # List of available models for downscaling:
-                              "SEALS"
-                              )
+    "SEALS"
+  )
 
   if (!any(grepl(downscaling_model, downscaling_model_list))) {
-    warning(paste("No available model for downscaling specified. Current options for 'downscaling_model' are", 
-                  paste(gsub("(\\w+)", '"\\1"', downscaling_model_list), collapse = " ")))
+    stop(paste(
+      "No available model for downscaling specified. Current options for 'downscaling_model' are",
+      paste(gsub("(\\w+)", '"\\1"', downscaling_model_list), collapse = " ")
+    ))
   }
 
+  selectyears <- sort(findset(selectyears, noset = "original"))
+
   if (downscaling_model == "SEALS") {
-    primSecOther <- PrimSecdOtherLand(level = "grid", dir = dir, unit_gridded = "share")
-    landShr <- read.magpie(file.path(dir, "cell.land_0.5_share.mz"))
-    out_seals <- landShr[, , "other", invert = TRUE]
-    out_seals <- mbind(out_seals, primSecOther)
-    out_seals <- out_seals[, "y1985", , invert = TRUE]
+    landShr <- read.magpie(file.path(dir, "cell.land_0.5_share.mz"))[, selectyears, ]
+
+    out_seals <- mbind(
+      setNames(landShr[, , "crop"], "crop"),
+      setNames(landShr[, , "past"], "past"),
+      setNames(landShr[, , "primforest"] + landShr[, , "secdforest"] + landShr[, , "forestry"], "forest"),
+      setNames(landShr[, , "urban"], "urban"),
+      setNames(landShr[, , "other"], "other")
+    )
+
     out(out_seals, file)
   }
 }
