87c5be7b34223d665c34b3aff7b38b608322ca15
Marcos Alves <mppalves@gmail.com>
2021-06-09 18:41:15 +0200

    bug fix

--- R/reportCattle.R
+++ R/reportCattle.R
@@ -26,16 +26,17 @@ reportCattle <- function(gdx) {
   try({past_yld_m <- readGDX(gdx, "ov_past_yld", format = "simplest")[, , list("past_mngt" = "mowing", "type" = "level", "w" = "rainfed")]})
   try({past_ha_m <- readGDX(gdx, "ov31_past_area", format = "simplest")[, , list("past_mngt" = "mowing", "type" = "level", "w" = "rainfed")]})
 
-  if (!any(c(is.null(past_ha_c), is.null(past_yld_m),is.null(past_ha_m),is.null(lsu_ha)))) {
+  if (!any(c(is.null(past_ha_c), is.null(past_yld_m),is.null(past_ha_m),is.null(past_yld_c)))) {
   total_grazing_cattle_c <- past_ha_c * past_yld_c / (8.9 * 365 / 1000) # (lsu equivalent anual consumption in tDM)
   total_mowing_cattle <- past_yld_m * past_ha_m / (8.9 * 365 / 1000) # (lsu equivalent anual consumption in tDM)
   lsu_ha_m <- total_mowing_cattle/past_ha_m
   lsu_ha_m[is.nan(lsu_ha_m)] <- 0
-
+  lsu_ha_c <- total_grazing_cattle_c/past_ha_c
+  
   # aggregate and add global
   total_grazing_cattle_c_reg <- gdxAggregate(gdx, total_grazing_cattle_c, to = "regglo", absolute = T)
   total_mowing_cattle_reg <- gdxAggregate(gdx, total_mowing_cattle, to = "regglo", absolute = T)
-  lsu_ha_reg <- gdxAggregate(gdx, lsu_ha, to = "regglo", weight = past_ha_c, absolute = F)
+  lsu_ha_reg <- gdxAggregate(gdx, lsu_ha_c, to = "regglo", weight = past_ha_c, absolute = F)
   lsu_ha_m_reg <- gdxAggregate(gdx, lsu_ha_m, to = "regglo", weight = past_ha_m, absolute = F)
   past_ha_c_reg <- gdxAggregate(gdx, past_ha_c, to = "regglo", absolute = T)
   past_ha_m_reg <- gdxAggregate(gdx, past_ha_m, to = "regglo", absolute = T)
