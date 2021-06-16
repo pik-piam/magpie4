a947271e87a4d9c53f7dd09cd30902af5fc34829
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-06-16 14:17:13 +0200

    removed usage/import of mbind2 and fulldim function

--- R/land.R
+++ R/land.R
@@ -1,14 +1,17 @@
 #' @title land
 #' @description reads land out of a MAgPIE gdx file
 #'
-#' @importFrom magclass mbind2 fulldim read.magpie
+#' @importFrom magclass mbind read.magpie
 #' @export
 #'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
-#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
-#' @param types NULL or a vector of strings. If NULL, all land types are used. Options are "crop", "past", "forestry", "primforest","secdforest, "urban", "other", "primother" and "secdother"
-#' @param subcategories NULL or vector of strings. If NULL, no subcategories are returned. Meaningful options are "forestry", "secdforest" and "other"
+#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global)
+#' or any other aggregation level defined in superAggregate
+#' @param types NULL or a vector of strings. If NULL, all land types are used. Options are "crop", "past",
+#' "forestry", "primforest","secdforest, "urban", "other", "primother" and "secdother"
+#' @param subcategories NULL or vector of strings. If NULL, no subcategories are returned. Meaningful options
+#'  are "forestry", "secdforest" and "other"
 #' @param sum determines whether output should be land-type-specific (FALSE) or aggregated over all types (TRUE).
 #' @param dir for gridded outputs: magpie output directory which contains a mapping file (rds or spam) disaggregation
 #' @param spamfiledirectory deprecated. please use \code{dir} instead
@@ -21,8 +24,9 @@
 #' x <- land(gdx)
 #' }
 #'
-land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = NULL, sum = FALSE, dir = ".", spamfiledirectory = "") {
-  
+land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = NULL,
+                 sum = FALSE, dir = ".", spamfiledirectory = "") {
+
   dir <- getDirectory(dir, spamfiledirectory)
 
   if (level == "grid") {
@@ -40,9 +44,9 @@ land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = 
     if (!is.null(subcategories)) {
       if ("crop" %in% subcategories) {
         crop <- croparea(gdx, product_aggr = FALSE, level = "cell")
-        crop_nobio <- dimSums(crop[, , c("begr", "betr"), invert = TRUE])
-        crop_bio <- dimSums(crop[, , c("begr", "betr")])
-        crop <- mbind(setNames(crop_nobio, "crop.nobio"), setNames(crop_bio, "crop.bio"))
+        cropNoBio <- dimSums(crop[, , c("begr", "betr"), invert = TRUE])
+        cropBio <- dimSums(crop[, , c("begr", "betr")])
+        crop <- mbind(setNames(cropNoBio, "crop.nobio"), setNames(cropBio, "crop.bio"))
         names(dimnames(crop)) <- names(dimnames(x))
       } else {
         crop <- x[, , "crop"]
@@ -54,13 +58,16 @@ land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = 
         past <- x[, , "past"]
       }
       if ("forestry" %in% subcategories) {
-        #     if (length(unlist(strsplit(names(dimnames(forestry_check))[3],"\\."))) == 3) {
-        forestry <- add_dimension(readGDX(gdx, "ov32_land", "ov_land_fore", select = list(type = "level")), dim = 3.1, add = "land", "forestry")
-        if (suppressWarnings(!is.null(readGDX(gdx, "fcostsALL")) | names(dimnames(forestry))[[3]] == "land.type32.ac")) {
+        forestry <- add_dimension(readGDX(gdx, "ov32_land", "ov_land_fore", select = list(type = "level")),
+                                  dim = 3.1, add = "land", "forestry")
+        if (suppressWarnings(!is.null(readGDX(gdx, "fcostsALL")) |
+                             names(dimnames(forestry))[[3]] == "land.type32.ac")) {
           forestry <- dimSums(forestry, dim = 3.3)
         }
         ## Follwing check throws a warning with 7th digit rounding. Diff is 0.5 hectares. Rounding issue?
-        if (abs(sum(x[, , "forestry.total"] - dimSums(forestry, dim = 3.2))) > 2e-06) warning("Forestry: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        if (abs(sum(x[, , "forestry.total"] - dimSums(forestry, dim = 3.2))) > 2e-06) {
+          warning("Forestry: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        }
       } else {
         forestry <- x[, , "forestry"]
       }
@@ -71,8 +78,11 @@ land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = 
         primforest <- x[, , "primforest"]
       }
       if ("secdforest" %in% subcategories) {
-        secdforest <- add_dimension(readGDX(gdx, "ov35_secdforest", "ov_natveg_secdforest", select = list(type = "level")), dim = 3.1, add = "land", "secdforest")
-        if (abs(sum(x[, , "secdforest.total"] - dimSums(secdforest, dim = 3.2))) > 1e-06) warning("secdforest: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        secdforest <- add_dimension(readGDX(gdx, "ov35_secdforest", "ov_natveg_secdforest",
+                                            select = list(type = "level")), dim = 3.1, add = "land", "secdforest")
+        if (abs(sum(x[, , "secdforest.total"] - dimSums(secdforest, dim = 3.2))) > 1e-06) {
+          warning("secdforest: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        }
       } else {
         secdforest <- x[, , "secdforest"]
       }
@@ -83,12 +93,15 @@ land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = 
         urban <- x[, , "urban"]
       }
       if ("other" %in% subcategories) {
-        other <- add_dimension(readGDX(gdx, "ov35_other", "ov_natveg_other", select = list(type = "level")), dim = 3.1, add = "land", "other")
-        if (round(sum(x[, , "other.total"] - dimSums(other, dim = 3.2)), 7) != 0) warning("Other: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        other <- add_dimension(readGDX(gdx, "ov35_other", "ov_natveg_other", select = list(type = "level")),
+                               dim = 3.1, add = "land", "other")
+        if (round(sum(x[, , "other.total"] - dimSums(other, dim = 3.2)), 7) != 0) {
+          warning("Other: Total and sum of subcategory land types diverge! Check your GAMS code!")
+        }
       } else {
         other <- x[, , "other"]
       }
-      x <- mbind2(crop, past, forestry, primforest, secdforest, urban, other)
+      x <- mbind(crop, past, forestry, primforest, secdforest, urban, other)
     }
   }
   if (is.null(x)) {
@@ -103,8 +116,8 @@ land <- function(gdx, file = NULL, level = "reg", types = NULL, subcategories = 
       primsecdother <- PrimSecdOtherLand(land_share = "cell.land_0.5_share.mz", level = level, dir = dir)
       primsecdother <- primsecdother[, "y1985", , invert = TRUE]
       primsecdother <- add_dimension(primsecdother, dim = 3.2, add = "sub", "total")
-      x <- mbind2(x, primsecdother)
-    } else {}
+      x <- mbind(x, primsecdother)
+    }
     x <- x[, , types]
   }
 
