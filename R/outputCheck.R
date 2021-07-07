4c1c6bbc2c284901b24472475152c49556f9c146
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-07-07 09:52:39 +0200

    added outputCheck function which allows to check a MAgPIE-GDX for known problems

---
+++ R/outputCheck.R
@@ -0,0 +1,49 @@
+#' @title outputCheck
+#' @description Function to check a MAgPIE gdx file for known problems
+#' (e.g. non-zero dummy variables). The function will throw warnings for
+#' problem found in the outputs.
+#'
+#' @param gdx GDX file
+#' @author Jan Philipp Dietrich
+#' @examples
+#' \dontrun{
+#' outputCheck(gdx)
+#' }
+#' @export
+#'
+
+outputCheck <- function(gdx) {
+
+  .reportWarnings <- function(...) {
+    w <- c(...)
+    if (length(w) == 0) {
+      message("Output looks good, no problems found!")
+    } else {
+      for (wi in w) {
+        warning(wi, call. = FALSE)
+      }
+      s <- ifelse(length(w) > 1, "s", "")
+      message("Please check your run, ", length(w), " problem", s, " identified!")
+    }
+  }
+
+  .checkTradeManna <- function(gdx) {
+    x <- readGDX(gdx, "v21_manna_from_heaven", react = "silent")
+    if (is.null(x) || all(x == 0)) return(NULL)
+    return("v21_manna_from_heaven contains non-zero values!")
+  }
+
+  .checkFoodModelConvergence <- function(gdx) {
+    iter <- readGDX(gdx, "p15_iteration_counter", react = "silent")
+    iterMax <- readGDX(gdx, "s15_maxiter", react = "silent")
+    if (is.null(iter) || is.null(iterMax) || all(iter <= iterMax)) return(NULL)
+    violatingYears <- getYears(iter, as.integer = TRUE)[iter > iterMax]
+    return(paste0("The food demand model did not converge in ",
+                  length(violatingYears), " timesteps (",
+                  paste(violatingYears, collapse = ", "), ")!"))
+  }
+
+  .reportWarnings(.checkFoodModelConvergence(gdx),
+                  .checkTradeManna(gdx))
+
+}
