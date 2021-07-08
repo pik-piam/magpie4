8df34fc318463a0468af8e9997f800c06bdd0ade
Felicitas <beier@pik-potsdam.de>
2021-07-08 23:46:02 +0200

    bugfix in expenditureIndexFood for GHG tax = FALSE wrong product aggregation took place

--- R/expenditureIndexFood.R
+++ R/expenditureIndexFood.R
@@ -1,94 +1,122 @@
 #' @title       expenditureIndexFood
-#' @description calculates food expenditure index (baseyear = 100) corrected for ghg emission costs based on a MAgPIE gdx file
-#' 
+#' @description calculates food expenditure index (baseyear = 100)
+#'              corrected for ghg emission costs based on a MAgPIE gdx file
+#'
 #' @export
-#' 
+#'
 #' @param gdx        GDX file
 #' @param file       File the output should be written to using write.magpie
-#' @param level      Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in mapping
-#' @param products   Selection of products (either by naming products, e.g. "tece", or naming a set, e.g."kcr")
+#' @param level      Level of regional aggregation; "reg" (regional),
+#'                   "glo" (global), "regglo" (regional and global)
+#'                   or any other aggregation level defined in mapping
+#' @param products   Selection of products (either by naming products,
+#'                   e.g. "tece", or naming a set, e.g."kcr")
 #' @param baseyear   Baseyear of the price index
-#' @param basketyear Year of reference food basket (should be in the past for comparison of different runs to have identical and comparable food basket)
+#' @param basketyear Year of reference food basket
+#'                   (should be in the past for comparison of different runs to
+#'                   have identical and comparable food basket)
 #' @param round      Rounded result (TRUE or FALSE)
-#' @param ghgtax     Correction of food price expenditure for ghg emission costs (TRUE or FALSE)
-#' 
+#' @param ghgtax     Correction of food price expenditure for ghg emission costs
+#'                   (TRUE or FALSE)
+#'
 #' @return A MAgPIE object containing food price expenditure index
-#' 
+#'
 #' @author Felicitas Beier
-#' 
+#'
 #' @examples
-#'   \dontrun{
-#'     x <- expenditureIndexFood(gdx)
-#'   }
-#' 
+#' \dontrun{
+#' x <- expenditureIndexFood(gdx)
+#' }
+#'
 #' @importFrom magclass setYears getRegions dimSums getNames new.magpie
 #' @importFrom madrat toolAggregate
 #' @importFrom luscale superAggregate
 
-expenditureIndexFood <- function (gdx, file=NULL, level="reg", products="kfo", basketyear="y2010", baseyear="y2010", round=TRUE, ghgtax=TRUE) {
+expenditureIndexFood <- function(gdx, file = NULL, level = "reg",
+                                 products = "kfo",
+                                 basketyear = "y2010", baseyear = "y2010",
+                                 round = TRUE, ghgtax = TRUE) {
 
   # Read in representative food basket: per-capita kcal consumption from the food demand model
   products   <- readGDX(gdx, products)
-  foodbasket <- setYears(Kcal(gdx = gdx, level = "iso", calibrated = TRUE, after_shock = TRUE, products = "kfo", product_aggr = FALSE, per_capita = FALSE)[,basketyear,], NULL)
+  foodbasket <- setYears(Kcal(gdx = gdx, level = "iso", calibrated = TRUE,
+                              after_shock = TRUE, products = "kfo",
+                              product_aggr = FALSE, per_capita = FALSE)[, basketyear, ], NULL)
 
   # Agricultural Prices: Prices from MAgPIE after optimization (USD05PPP per kcal)
   price      <- FoodDemandModuleConsumerPrices(gdx)
-  
+
   # Food expenditure per country per product
-  food_expenditure <- (foodbasket*price*365)
+  food_expenditure <- (foodbasket * price * 365)
   # Aggregation to image-10 regions
-  mapping          <- readGDX(gdx,"i_to_iso")
-  food_expenditure <- toolAggregate(food_expenditure, rel=mapping, from="iso", to="i")
-  
+  mapping          <- readGDX(gdx, "i_to_iso")
+  food_expenditure <- toolAggregate(food_expenditure, rel = mapping,
+                                    from = "iso", to = "i")
+
   # Total food expenditure
-  food_expenditure_total <- dimSums(food_expenditure, dim=3)
+  food_expenditure_total <- dimSums(food_expenditure, dim = 3)
   # Share of single food products in total food expenditure
-  foodproduct_shr <- food_expenditure / food_expenditure_total
+  foodproduct_shr        <- food_expenditure / food_expenditure_total
 
   if (ghgtax) {
+
     # Emission costs
     .tmp_cost <- function(gdx, name, label) {
-      
-      cost <- readGDX(gdx, name, format = "first_found", select = list(type = "level"), react = "quiet")
-      if (is.null(cost))  return(NULL)
+
+      cost <- readGDX(gdx, name, format = "first_found",
+                      select = list(type = "level"), react = "quiet")
+      if (is.null(cost)) return(NULL)
       cost <- dimSums(cost, dim = 3)
       cost <- superAggregate(cost, aggr_type = "sum", level = "reg")
       dimnames(cost)[[3]] <- label
-      
+
       return(cost)
     }
-    
+
     # Costs for emission rights for pollutants and greenhouse gases (mio. USD05MER per yr)
     ghg_tax     <- .tmp_cost(gdx, "ov_emission_costs", "GHG Emissions")
-    ghg_tax     <- collapseNames(foodproduct_shr*ghg_tax)
-    
+    ghg_tax     <- collapseNames(foodproduct_shr * ghg_tax)
+
     # Corrected food expenditure
-    tmp           <- dimSums(food_expenditure[,,products] - ghg_tax[,,products], dim=3)
+    tmp           <- dimSums(food_expenditure[, , products] - ghg_tax[, , products], dim = 3)
     getNames(tmp) <- "Food Expenditure Index corrected for ghg costs"
+
   } else {
+
     # Food expenditure
-    tmp           <- food_expenditure_total
+    tmp           <- dimSums(food_expenditure[, , products], dim = 3)
     getNames(tmp) <- "Food Expenditure Index"
+
   }
 
   # Reported level
-  if (level=="reg") {
+  if (level == "reg") {
+
     food_expenditure_corrected <- tmp
-  } else if(level=="glo"){
-    food_expenditure_corrected <- dimSums(tmp, dim=1)
-  } else if (level=="regglo") {
-    food_expenditure_corrected                    <- new.magpie(c(getRegions(tmp), "GLO"), getYears(tmp), getNames(tmp))
-    food_expenditure_corrected[getRegions(tmp),,] <- tmp[getRegions(tmp),,]
-    food_expenditure_corrected["GLO",,]           <- dimSums(tmp, dim=1)
-  } else if (level!="iso") {
+
+  } else if (level == "glo") {
+
+    food_expenditure_corrected <- dimSums(tmp, dim = 1)
+
+  } else if (level == "regglo") {
+
+    food_expenditure_corrected                      <- new.magpie(cells_and_regions =  c(getRegions(tmp), "GLO"),
+                                                                  years = getYears(tmp),
+                                                                  names = getNames(tmp))
+    food_expenditure_corrected[getRegions(tmp), , ] <- tmp[getRegions(tmp), , ]
+    food_expenditure_corrected["GLO", , ]           <- dimSums(tmp, dim = 1)
+
+  } else if (level != "iso") {
     stop("iso-level reporting of Food Expenditure Index not supported")
   }
-  
+
   # Food Expenditure Index (corrected)
-  foodexpenditureindex <- food_expenditure_corrected / setYears(food_expenditure_corrected[,baseyear,], NULL)
-  
+  foodexpenditureindex <- food_expenditure_corrected /
+                          setYears(food_expenditure_corrected[, baseyear, ],
+                                   NULL)
+
   # Food Expenditure Index with baseyear=100
-  foodexpenditureindex            <- foodexpenditureindex*100
+  foodexpenditureindex            <- foodexpenditureindex * 100
   if (round) foodexpenditureindex <- round(foodexpenditureindex)
 
   out(foodexpenditureindex, file)
--- R/reportExpenditureFoodIndex.R
+++ R/reportExpenditureFoodIndex.R
@@ -1,48 +1,67 @@
 #' @title reportExpenditureFoodIndex
-#' @description reports food expenditure index and food expenditure index corrected for emission costs
-#' 
+#' @description reports food expenditure index and food expenditure index
+#'              corrected for emission costs
+#'
 #' @export
-#' 
-#' @param gdx GDX file
+#'
+#' @param gdx        GDX file
 #' @param baseyear   Baseyear of the price index
-#' @param basketyear Year of reference food basket (should be in the past for comparison of different runs to have identical and comparable food basket)
-#' 
-#' @return Food expenditure index as MAgPIE object 
+#' @param basketyear Year of reference food basket
+#'                   (should be in the past for comparison of different runs to
+#'                   have identical and comparable food basket)
+#'
+#' @return Food expenditure index as MAgPIE object
 #' @author Felicitas Beier
 #' @examples
-#' 
-#'   \dontrun{
-#'     x <- reportPriceFoodIndex(gdx)
-#'   }
-#' 
-
-reportExpenditureFoodIndex <- function(gdx, baseyear="y2010", basketyear="y2010") {
+#' \dontrun{
+#' x <- reportPriceFoodIndex(gdx)
+#' }
+#'
+reportExpenditureFoodIndex <- function(gdx, baseyear = "y2010", basketyear = "y2010") {
 
   # corrected food expenditure index all food products
-  x1 <- expenditureIndexFood(gdx, level="regglo", products="kfo", basketyear=basketyear, baseyear=baseyear, ghgtax=TRUE, round=TRUE)
-  getNames(x1) <- paste0("Prices|Food Expenditure Index corrected for ghg costs (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x1 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = TRUE, round = TRUE)
+  getNames(x1) <- paste0("Prices|Food Expenditure Index corrected for ghg costs
+                         (Index ", gsub("\\y", "", baseyear), "=100)")
+
   # plant-based food products
-  x2 <- expenditureIndexFood(gdx, level="regglo", products="kfo_pp", basketyear=basketyear, baseyear=baseyear, ghgtax=TRUE, round=TRUE)
-  getNames(x2) <- paste0("Prices|Food Expenditure Index corrected for ghg costs|Plant-based food products (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x2 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo_pp",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = TRUE, round = TRUE)
+  getNames(x2) <- paste0("Prices|Food Expenditure Index corrected for ghg
+                         costs|Plant-based food products (Index ", gsub("\\y", "", baseyear), "=100)")
+
   # livestock food products
-  x3 <- expenditureIndexFood(gdx, level="regglo", products="kfo_lp", basketyear=basketyear, baseyear=baseyear, ghgtax=TRUE, round=TRUE)
-  getNames(x3) <- paste0("Prices|Food Expenditure Index corrected for ghg costs|Livestock food products (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x3 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo_lp",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = TRUE, round = TRUE)
+  getNames(x3) <- paste0("Prices|Food Expenditure Index corrected for ghg
+                         costs|Livestock food products (Index ", gsub("\\y", "", baseyear), "=100)")
+
   # food expenditure index all food products
-  x4 <- expenditureIndexFood(gdx, level="regglo", products="kfo", basketyear=basketyear, baseyear=baseyear, ghgtax=FALSE, round=TRUE)
-  getNames(x4) <- paste0("Prices|Food Expenditure Index (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x4 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = FALSE, round = TRUE)
+  getNames(x4) <- paste0("Prices|Food Expenditure Index
+                         (Index ", gsub("\\y", "", baseyear), "=100)")
+
   # food expenditure index all food products
-  x5 <- expenditureIndexFood(gdx, level="regglo", products="kfo_pp", basketyear=basketyear, baseyear=baseyear, ghgtax=FALSE, round=TRUE)
-  getNames(x5) <- paste0("Prices|Food Expenditure Index|Plant-based food products (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x5 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo_pp",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = FALSE, round = TRUE)
+  getNames(x5) <- paste0("Prices|Food Expenditure Index|Plant-based food products
+                         (Index ", gsub("\\y", "", baseyear), "=100)")
+
   # food expenditure index all food products
-  x6 <- expenditureIndexFood(gdx, level="regglo", products="kfo_lp", basketyear=basketyear, baseyear=baseyear, ghgtax=FALSE, round=TRUE)
-  getNames(x6) <- paste0("Prices|Food Expenditure Index|Livestock food products (Index ",gsub("\\y","",baseyear),"=100)")
-  
+  x6 <- expenditureIndexFood(gdx, level = "regglo", products = "kfo_lp",
+                             basketyear = basketyear, baseyear = baseyear,
+                             ghgtax = FALSE, round = TRUE)
+  getNames(x6) <- paste0("Prices|Food Expenditure Index|Livestock food products
+                         (Index ", gsub("\\y", "", baseyear), "=100)")
+
   x <- mbind(x1, x2, x3, x4, x5, x6)
-  
+
   return(x)
 }
