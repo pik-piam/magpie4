3a52c9f000f4a9a8f666bbc8381d8b99a254a15e
Marcos Alves <mppalves@gmail.com>
2021-07-20 18:39:27 +0200

    bug fix

--- R/reportGraslandSoilCarbon.R
+++ R/reportGraslandSoilCarbon.R
@@ -35,7 +35,7 @@ reportGraslandSoilCarbon <- function(gdx, dir = ".", spamfiledirectory = "") {
   }, silent = T)
 
   if (!is.null(sc_range)) {
-    if (!is.null(range_areas)) {
+    if (!is.null(grass_areas)) {
       grass_areas <- gdxAggregate(gdx, grass_areas, to = "regglo", absolute = T)
       
       sc_range <- toolAggregate(sc_range, map_cell, from = "celliso", to = "iso")
@@ -52,10 +52,13 @@ reportGraslandSoilCarbon <- function(gdx, dir = ".", spamfiledirectory = "") {
       
       sc_base_avg <- sc_base / grass_areas[, getYears(sc_base), c("pastr", "range") ]
       sc_base_avg[is.infinite(sc_base_avg) | is.nan(sc_base_avg)] <- 0
-
+      sc_base_avg_t <- dimSums(sc_base, dim =3) / dimSums(grass_areas[, getYears(sc_base), c("pastr", "range") ], dim = 3)
+      
       x <- NULL
       x <- mbind(x, setNames(sc_base_avg, paste0("Resources|Soil Carbon|Grassland|+|",reportingnames(getNames(grass_areas, dim = 1)),"|Density (tC per ha)")))
+      x <- mbind(x, setNames(dimSums(sc_base_avg_t, dim = 3), paste0("Resources|Soil Carbon|Grassland|Density (tC per ha)")))
       x <- mbind(x, setNames(sc_base, paste0("Resources|Soil Carbon|Grassland|+|",reportingnames(getNames(grass_areas, dim = 1)),"|Total (tC)")))
+      x <- mbind(x, setNames(dimSums(sc_base, dim = 3), paste0("Resources|Soil Carbon|Grassland|Total (tC)")))
       
     } else {
       print("Disabled (dissagregation must be run first) ")
