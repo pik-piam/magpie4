0c60e306b3b421164a260cd0c6889cfd10c6b2fc
Debbora Leip <leip@pik-potsdam.de>
2021-09-29 14:24:01 +0200

    updated fertilizer cost reporting

--- R/reportCostsFertilizer.R
+++ R/reportCostsFertilizer.R
@@ -1,25 +1,27 @@
 #' @title reportCostsFertilizer
 #' @description reports MAgPIE nitrogen fertilizer costs disaggregated to crop categories
-#' 
+#'
 #' @param gdx GDX file
 #' @return magpie object with fertilizer costs
 #' @author Debbora Leip
 #' @examples
-#' 
+#'
 #'   \dontrun{
 #'     x <- reportCostsFertilizer(gdx)
 #'   }
-#'   
+#'
 #' @importFrom magpiesets reporthelper summationhelper
-#'    
-reportCostsFertilizer <- function(gdx){
-  nr_fertilizer_costs <- CostsFertilizer(gdx, level = "regglo")
+#'
+reportCostsFertilizer <- function(gdx) {
+  fertilizerCosts <- CostsFertilizer(gdx, level = "regglo")
+  totalCosts <- setNames(dimSums(fertilizerCosts, dim = 3), "Costs|Fertilizer")
   # no phosphorus fertilizer costs in MAgPIE
-  
-  nr_fertilizer_costs <- reporthelper(nr_fertilizer_costs, dim = 3.1, level_zero_name = "Costs|Fertilizer", detail = TRUE)
-  nr_fertilizer_costs <- summationhelper(nr_fertilizer_costs)
-  
-  getNames(nr_fertilizer_costs) <- paste0(getNames(nr_fertilizer_costs), " (million US$05/yr)")
-  
-  return(nr_fertilizer_costs)
+
+  fertilizerCosts <- reporthelper(fertilizerCosts, dim = 3.1, level_zero_name = "Costs|Fertilizer", detail = TRUE)
+  fertilizerCosts <- summationhelper(fertilizerCosts)
+  fertilizerCosts <- mbind(totalCosts, fertilizerCosts)
+
+  getNames(fertilizerCosts) <- paste0(getNames(fertilizerCosts), " (million US$05/yr)")
+
+  return(fertilizerCosts)
 }
