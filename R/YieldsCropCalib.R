db1b9a41fa113a4ca4097e78678cfba8cc581334
emolinab <mbacca@pik-potsdam.de>
2021-10-08 15:33:33 +0200

    +0.000001 zero areas in YieldsCropCalib and YieldsCropRaw function

--- R/YieldsCropCalib.R
+++ R/YieldsCropCalib.R
@@ -19,27 +19,29 @@
 #'
 YieldsCropCalib <- function(gdx, file = NULL, level = "cell") {
 
-  if(level %in% c("cell","glo","reg","regglo")){
+  if (level %in% c("cell", "glo", "reg", "regglo")) {
   kcr <- findset("kcr")
-  t<-readGDX(gdx, "t")
-  out <- readGDX(gdx, "i14_yields_calib")[,t,kcr]
-  
+  t <- readGDX(gdx, "t")
+  out <- readGDX(gdx, "i14_yields_calib")[, t, kcr]
+
   weight <- out
-  area<-readGDX(gdx, "fm_croparea")[,1995,]
-  # add small area for begr and betr, which is zero in fm_croparea. Otherwise yields for begr and betr are zero.
-  area[,,c("begr","betr")] <- 0.000001
-  weight[, , ]<- area
   
+  # The +0.000001 is added as a small area for crops with zero values in fm_croparea. 
+  # Otherwise yields for begr and betr are zero.
+  
+  area <- readGDX(gdx, "fm_croparea")[, 1995, ] + 0.000001
+  weight[, , ] <- area
+
   if (level != "cell") out <- gdxAggregate(gdx, out, weight = weight, to = level, absolute = FALSE) else out
-  
-  }else if (level== "grid"){
-    
+
+  } else if (level == "grid") {
+
     kcr <- findset("kcr")
-    t<-readGDX(gdx, "t")
-    out <- readGDX(gdx, "i14_yields_calib")[,t,kcr]
-    
+    t <- readGDX(gdx, "t")
+    out <- readGDX(gdx, "i14_yields_calib")[, t, kcr]
+
     out <- gdxAggregate(gdx, out, weight = NULL, to = "grid", absolute = FALSE)
-  }else{
+  } else {
     stop("Level not recognized")
   }
 
