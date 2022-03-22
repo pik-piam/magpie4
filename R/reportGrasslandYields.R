443505b5a5b7f501da5904f4bef79555bd5adc86
Marcos Alves <mppalves@gmail.com>
2022-03-22 20:20:39 +0100

    updating pasture productivity calculation

--- R/reportGrasslandYields.R
+++ R/reportGrasslandYields.R
@@ -22,21 +22,29 @@ reportGrasslandYields <- function(gdx) {
   grass_yld <- NULL
   
   try({grass_areas <- readGDX(gdx, "ov31_grass_area", format = "simplest")[, , list("type" = "level")]})
-  try({grass_yld <- readGDX(gdx, "ov31_grass_yld", format = "simplest")[, , list("type" = "level")]})
+  try({grass_yld <- readGDX(gdx, "i31_grass_yields", format = "simplest")})
+  try({tau <- readGDX(gdx, "ov_tau", format = "simplest")})
   
   if(!is.null(grass_yld)) {
-    grass_areas <- collapseNames(grass_areas) 
-    grass_yld <- collapseNames(grass_yld) 
-    past_prod <- grass_areas * grass_yld 
+    # grass_areas <- collapseNames(grass_areas)
+    # grass_yld <- collapseNames(grass_yld)
+    # past_prod <- grass_areas * grass_yld
+    # 
+    # past_prod_reg <- gdxAggregate(gdx, past_prod, to = "regglo", absolute = T)
+    # grass_areas_reg <- gdxAggregate(gdx, grass_areas, to = "regglo", absolute = T)
+    # 
+    # grass_yields <- past_prod_reg/grass_areas_reg
+    # grass_yields[is.nan(grass_yields) | is.infinite(grass_yields)] <- 0
+    # 
+    # x <- setNames(grass_yields, paste0("Productivity|Yield|+|", reportingnames(getNames(grass_yields)), " (t DM/ha)"))
     
-    past_prod_reg <- gdxAggregate(gdx, past_prod, to = "regglo", absolute = T)
-    grass_areas_reg <- gdxAggregate(gdx, grass_areas, to = "regglo", absolute = T)
+    grass_yld <- collapseNames(grass_yld)
+    tau <- gdxAggregate(gdx, tau, to = "cell", absolute = F)
+    grass_yld <- grass_yld[,getYears(tau),]
     
-    grass_yields <- past_prod_reg/grass_areas_reg
-    grass_yields[is.nan(grass_yields) | is.infinite(grass_yields)] <- 0
-    
+    grass_yld[,,"pastr"] <- tau[,,"pastr.level"] * grass_yld[,,"pastr"]
+    grass_yields <- gdxAggregate(gdx, grass_yld, weight = grass_areas, to = "regglo", absolute = F)
     x <- setNames(grass_yields, paste0("Productivity|Yield|+|", reportingnames(getNames(grass_yields)), " (t DM/ha)"))
-    
   } else {
     x <- "Disabled (No separate grassland yields)"
   }
