14b189719044bfb38ae2644bec62f1be361f85f3
Felicitas <beier@pik-potsdam.de>
2022-04-10 20:51:08 +0200

    linter improvements

--- R/reportIncome.R
+++ R/reportIncome.R
@@ -1,38 +1,39 @@
 #' @title reportIncome
 #' @description reports income
-#' 
+#'
 #' @export
-#' 
-#' @param gdx GDX file
+#'
+#' @param gdx  GDX file
 #' @param type ppp for purchase power parity, mer for market exchange rate
 #' @return Annual per capita and total income as MAgPIE object (US$2005 MER/cap/yr and million US$05 PPP/yr)
 #' @author Florian Humpenoeder, Isabelle Weindl, Felicitas Beier
 #' @examples
-#' 
-#'   \dontrun{
-#'     x <- reportIncome(gdx)
-#'   }
-#' 
+#' \dontrun{
+#' x <- reportIncome(gdx)
+#' }
+#'
+reportIncome <- function(gdx, type = "ppp") {
 
-reportIncome <- function(gdx, type="ppp") {
-  
-  #read in regional data
-  per_capita <- income(gdx, type=type, level = "regglo")
-  total      <- income(gdx, type=type, level = "regglo", per_capita=FALSE)
-  
-  #rename
-  if (type=="ppp") {
-    getNames(per_capita) <- "Income (US$05 PPP/cap/yr)"
+  # read in regional data
+  perCapita  <- income(gdx, type = type, level = "regglo")
+  total      <- income(gdx, type = type, level = "regglo", per_capita = FALSE)
+
+  # rename
+  if (type == "ppp") {
+
+    getNames(perCapita)  <- "Income (US$05 PPP/cap/yr)"
     getNames(total)      <- "Total income (million US$05 PPP/yr)"
-  } else if (type=="mer") {
-    getNames(per_capita) <- "Income (US$05 MER/cap/yr)"
+
+  } else if (type == "mer") {
+
+    getNames(perCapita)  <- "Income (US$05 MER/cap/yr)"
     getNames(total)      <- "Total income (million US$05 MER/yr)"
+
   } else {
     stop("Please specify reporting type for income units: mer or ppp")
   }
 
-  out <- mbind(per_capita,total)
+  out <- mbind(perCapita, total)
 
   return(out)
 }
-
