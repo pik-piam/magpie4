31c467ca25bee13cff38ccbfd6f47328e16aa754
mscrawford <mscrawfor@gmail.com>
2022-04-20 16:32:18 +0200

    Added  to costs.R

--- R/costs.R
+++ R/costs.R
@@ -9,7 +9,8 @@
 #' "regglo" (regional and global) or any other aggregation level defined in superAggregate
 #' @param sum total costs (TRUE) or detailed costs (FALSE)
 #' @return A MAgPIE object containing the goal function costs [million US$05]
-#' @author Jan Philipp Dietrich, Markus Bonsch, Misko Stevanovic, Florian Humpenoeder, Edna J. Molina Bacca
+#' @author Jan Philipp Dietrich, Markus Bonsch, Misko Stevanovic, Florian Humpenoeder,
+#' Edna J. Molina Bacca, Michael Crawford
 #' @importFrom gdx readGDX out
 #' @importFrom magclass mbind dimSums collapseNames
 #' @importFrom luscale superAggregate
@@ -19,8 +20,8 @@
 #' }
 #'
 costs <- function(gdx, file = NULL, level = "reg", sum = TRUE) {
-  tmp_cost <- function(gdx, name, label) {
-    cost <- readGDX(gdx, name, format = "first_found", select = list(type = "level"), react = "quiet")
+  tmpCost <- function(gdx, name, label) {
+    cost <- readGDX(gdx, name, format = "first_found", select = list(type = "level"), react = "silent")
     if (is.null(cost)) {
       return(NULL)
     }
@@ -31,47 +32,52 @@ costs <- function(gdx, file = NULL, level = "reg", sum = TRUE) {
   }
 
   x <- list(
-    tmp_cost(gdx, "ov_cost_landcon", "Land Conversion"),
-    tmp_cost(gdx, "ov_cost_transp", "Transport"),
-    tmp_cost(gdx, "ov_nr_inorg_fert_costs", "N Fertilizer"),
-    tmp_cost(gdx, "ov_tech_cost", "TC"),
-    tmp_cost(gdx, "ov_p_fert_costs", "P Fertilizer"),
-    tmp_cost(gdx, "ov_emission_costs", "GHG Emissions"),
-    tmp_cost(gdx, "ov_reward_cdr_aff", "Reward for Afforestation") * -1,
-    tmp_cost(gdx, "ov_maccs_costs", "MACCS"),
-    tmp_cost(gdx, "ov_cost_AEI", "AEI"),
-    tmp_cost(gdx, "ov_cost_trade", "Trade"),
-    tmp_cost(gdx, "ov_cost_fore", "Forestry"),
-    tmp_cost(gdx, "ov_cost_timber", "Timber production"),
-    tmp_cost(gdx, "ov_cost_hvarea_natveg", "Timber harvest natveg"),
-    tmp_cost(gdx, "ov_cost_bioen", "Bioenergy"),
-    tmp_cost(gdx, c("ov_cost_processing", "ov_processing_costs"), "Processing"),
-    tmp_cost(gdx, "ov_costs_overrate_cropdiff", "Punishment overrated cropland difference"),
-    tmp_cost(gdx, "ov_bioenergy_utility", "Reward for producing bioenergy"),
-    tmp_cost(gdx, "ov_processing_substitution_cost", "Substitution processing"),
-    tmp_cost(gdx, "ov_costs_additional_mon", "Punishment cost for additionally transported monogastric livst_egg"),
-    tmp_cost(gdx, "ov_cost_land_transition", "Land transition matrix"),
-    tmp_cost(gdx, "ov_peatland_cost", "Peatland"),
-    tmp_cost(gdx, "ov_peatland_emis_cost", "Peatland GHG emisssions"),
-    tmp_cost(gdx, "ov_cost_bv_loss", "Biodiversity")
+    tmpCost(gdx, "ov_cost_landcon", "Land Conversion"),
+    tmpCost(gdx, "ov_cost_transp", "Transport"),
+    tmpCost(gdx, "ov_nr_inorg_fert_costs", "N Fertilizer"),
+    tmpCost(gdx, "ov_tech_cost", "TC"),
+    tmpCost(gdx, "ov_p_fert_costs", "P Fertilizer"),
+    tmpCost(gdx, "ov_emission_costs", "GHG Emissions"),
+    tmpCost(gdx, "ov_reward_cdr_aff", "Reward for Afforestation") * -1,
+    tmpCost(gdx, "ov_maccs_costs", "MACCS"),
+    tmpCost(gdx, "ov_cost_AEI", "AEI"),
+    tmpCost(gdx, "ov_cost_trade", "Trade"),
+    tmpCost(gdx, "ov_cost_fore", "Forestry"),
+    tmpCost(gdx, "ov_cost_timber", "Timber production"),
+    tmpCost(gdx, "ov_cost_hvarea_natveg", "Timber harvest natveg"),
+    tmpCost(gdx, "ov_cost_bioen", "Bioenergy"),
+    tmpCost(gdx, c("ov_cost_processing", "ov_processing_costs"), "Processing"),
+    tmpCost(gdx, "ov_costs_overrate_cropdiff", "Punishment overrated cropland difference"),
+    tmpCost(gdx, "ov_bioenergy_utility", "Reward for producing bioenergy"),
+    tmpCost(gdx, "ov_processing_substitution_cost", "Substitution processing"),
+    tmpCost(gdx, "ov_costs_additional_mon", "Punishment cost for additionally transported monogastric livst_egg"),
+    tmpCost(gdx, "ov_cost_land_transition", "Land transition matrix"),
+    tmpCost(gdx, "ov_peatland_cost", "Peatland"),
+    tmpCost(gdx, "ov_peatland_emis_cost", "Peatland GHG emisssions"),
+    tmpCost(gdx, "ov_cost_bv_loss", "Biodiversity"),
+    tmpCost(gdx, "ov_cost_urban", "Punishment urban deviation")
   )
 
   if (suppressWarnings(is.null(readGDX(gdx, "ov_cost_inv")))) {
-    input_costs <- tmp_cost(gdx, "ov_cost_prod", "Input Factors")
+    inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors")
   } else {
-    input_costs <- tmp_cost(gdx, "ov_cost_prod", "Input Factors") + tmp_cost(gdx, "ov_cost_inv", "Input Factors")
+    inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors") + tmpCost(gdx, "ov_cost_inv", "Input Factors")
   }
 
-  x[[length(x) + 1]] <- input_costs
+  x[[length(x) + 1]] <- inputCosts
 
   x <- mbind(x)
 
   #if (!as.numeric(readGDX(gdx, "s73_timber_demand_switch", "sm_timber_demand_switch"))) x[, , "Timber production"] <- 0
 
   # check
-  if(any(abs(readGDX(gdx,"ov11_cost_reg",select = list(type="level")) - dimSums(x, dim = 3)) > 1e-6)) warning("Differences between total cost and cost categories detected. A newly added cost item in the MAgPIE cost module might be missing in the costs R function.")
-  
-  
+  if (any(abs(readGDX(gdx, "ov11_cost_reg", select = list(type = "level")) - dimSums(x, dim = 3)) > 1e-6)) {
+    warning("Differences between total cost and cost categories detected.
+            A newly added cost item in the MAgPIE cost module might be missing
+            in the costs R function.")
+  }
+
+
   if (sum) {
     x <- dimSums(x, dim = 3)
   }
