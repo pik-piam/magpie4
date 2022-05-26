b14c17858144636f20ad52c94350d903fda21c3a
pvjeetze <vjeetze@pik-potsdam.de>
2022-05-26 14:06:26 +0200

    updated protecteArea and reportProtectedArea

--- R/protectedArea.R
+++ R/protectedArea.R
@@ -1,6 +1,6 @@
 #' @title protectedArea
 #' @description reads protectedArea out of a MAgPIE gdx file
-#' 
+#'
 #' @export
 #'
 #' @param gdx GDX file
@@ -8,53 +8,67 @@
 #' @param level Level of regional aggregation; "cell", "grid, "reg" (regional), "glo" (global), "regglo" (regional and global) or any secdforest aggregation level defined in superAggregate
 #' @param sum sum over land pools (default = FALSE)
 #' @param dir for gridded outputs: magpie output directory which contains a mapping file (rds or spam) disaggregation
-#' @details protected areas in primforest, secdforest and other land 
+#' @details protected areas in primforest, secdforest and other land
 #' @return protected area in Mha
-#' @author Florian Humpenoeder
+#' @author Florian Humpenoeder, Patrick v. Jeetze
 #' @importFrom gdx readGDX out
 #' @importFrom magclass clean_magpie dimSums collapseNames setYears write.magpie
 #' @importFrom luscale superAggregate
 #' @examples
-#' 
-#'   \dontrun{
-#'     x <- protectedArea(gdx)
-#'   }
-#' 
-
-protectedArea <- function(gdx, file=NULL, level="cell", sum=FALSE, dir="."){
-  
-  map_file                   <- Sys.glob(file.path(dir, "clustermap_*.rds"))
+#' \dontrun{
+#' x <- protectedArea(gdx)
+#' }
+#'
+protectedArea <- function(gdx, file = NULL, level = "cell", sum = FALSE, dir = ".") {
+  map_file <- Sys.glob(file.path(dir, "clustermap_*.rds"))
   mapping <- readRDS(map_file)
-  
-  #read in protected areas
-  a <- readGDX(gdx,"p35_save_natveg",react="silent")
+
+  # read in protected areas
+
+  a <- readGDX(gdx, "pm_land_conservation", react = "silent")
+  # sum protection and restoration area
+  a <- dimSums(a, dim = 3.2)
+
   if (is.null(a)) {
-    primforest <- setNames(readGDX(gdx,"p35_save_primforest",react="silent"),"primforest")
-    secdforest <- setNames(readGDX(gdx,"p35_save_secdforest",react="silent"),"secdforest")
-    other <- setNames(readGDX(gdx,"p35_save_other",react="silent"),"other")
-    a <- mbind(primforest,secdforest,other)
+    a <- readGDX(gdx, "p35_save_natveg", react = "silent")
+    if (!is.null(a)) {
+      a <- mbind(a, new.magpie(getCells(a), NULL, c("crop", "past", "forestry", "urban"), fill = 0))
     }
-  
+  }
+
+  if (is.null(a)) {
+    primforest <- setNames(readGDX(gdx, "p35_save_primforest", react = "silent"), "primforest")
+    secdforest <- setNames(readGDX(gdx, "p35_save_secdforest", react = "silent"), "secdforest")
+    other <- setNames(readGDX(gdx, "p35_save_other", react = "silent"), "other")
+    a <- mbind(
+      primforest,
+      secdforest,
+      other,
+      new.magpie(getCells(a), NULL, c("crop", "past", "forestry", "urban"), fill = 0)
+    )
+  }
+
   names(dimnames(a))[1] <- "j"
-  
-  #sum
-  if (sum) a <- dimSums(a,dim=3.1)
-  
-  #aggregate over regions
-  if (level != "cell" & level != "grid") a <- superAggregate(a, aggr_type = "sum", level = level,na.rm = FALSE)
-  #disaggregate to grid level 
+
+  # sum
+  if (sum) a <- dimSums(a, dim = 3.1)
+
+  # aggregate over regions
+  if (level != "cell" & level != "grid") {
+    a <- superAggregate(a, aggr_type = "sum", level = level, na.rm = FALSE)
+  } # disaggregate to grid level
   else if (level == "grid") {
-    #protected area as share of respective land type
-    b <- land(gdx,level="cell")[,,getNames(a)]
-    shr <- a/b
+    # protected area as share of respective land type
+    b <- land(gdx, level = "cell")[, , getNames(a)]
+    shr <- a / b
     shr[is.nan(shr)] <- 0
     shr[shr > 1] <- 1
-    #downscale share from cluster to grid level
-    shr <- toolAggregate(shr,mapping,to="cell")
-    #multiply with grid level land type area
-    x <- land(gdx,level="grid",dir=dir)[,,getNames(a)]
+    # downscale share from cluster to grid level
+    shr <- toolAggregate(shr, mapping, to = "cell")
+    # multiply with grid level land type area
+    x <- land(gdx, level = "grid", dir = dir)[, , getNames(a)]
     a <- shr * x
   }
-  
-  out(a,file)
+
+  out(a, file)
 }
--- R/reportProtectedArea.R
+++ R/reportProtectedArea.R
@@ -1,32 +1,34 @@
 #' @title reportProtectedArea
 #' @description reports protected areas
-#' 
+#'
 #' @export
-#' 
+#'
 #' @param gdx GDX file
 #' @return protected area in Mha
 #' @author Florian Humpenoeder
 #' @examples
-#' 
-#'   \dontrun{
-#'     x <- reportProtectedArea(gdx)
-#'   }
-#' 
-
+#' \dontrun{
+#' x <- reportProtectedArea(gdx)
+#' }
+#'
 reportProtectedArea <- function(gdx) {
-  
-  #read in regional data
-  a <- protectedArea(gdx,level = "reg")
-  
-  #add global
-  a <- mbind(a, setItems(dimSums(a,dim=1), dim = 1, "GLO"))
-  
-  #aggreate and rename
+
+  # read in regional data
+  a <- protectedArea(gdx, level = "reg")
+
+  # add global
+  a <- mbind(a, setItems(dimSums(a, dim = 1), dim = 1, "GLO"))
+
+  # aggreate and rename
   x <- NULL
-  x <- mbind(x,setNames(a[,,"primforest"],paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("primforest"),"|Protected (million ha)")))
-  x <- mbind(x,setNames(a[,,"secdforest"],paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("secdforest"),"|Protected (million ha)")))
-  x <- mbind(x,setNames(a[,,"other"],paste0("Resources|Land Cover|", reportingnames(getNames(a[,,"other"],dim=1)),"|Protected (million ha)")))
-  
+  x <- mbind(x, setNames(a[, , "crop"], paste0("Resources|Land Cover|+|", reportingnames("crop"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "past"], paste0("Resources|Land Cover|+|", reportingnames("past"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "forestry"], paste0("Resources|Land Cover|Forest|+|", reportingnames(getNames(a[, , "forestry"], dim = 1)), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "primforest"], paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("primforest"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "secdforest"], paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("secdforest"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "urban"], paste0("Resources|Land Cover|+|", reportingnames(getNames(a[, , "urban"], dim = 1)), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "other"], paste0("Resources|Land Cover|+|", reportingnames(getNames(a[, , "other"], dim = 1)), "|Protected (million ha)")))
+
+
   return(x)
 }
-
