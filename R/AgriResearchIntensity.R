8a60835485e5d4b7e67753fb5a6f70d98e2a61f1
David Chen <davidch@lt916.pik-potsdam.de>
2022-06-03 15:53:40 +0200

    bugfix AgriResearchIntensity

--- R/AgriResearchIntensity.R
+++ R/AgriResearchIntensity.R
@@ -7,7 +7,6 @@
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param level aggregation level, reg, glo or regglo, cell or grid
-#' @param debug debug mode TRUE makes some consistency checks between estimates for different resolutions.
 #' @author David M Chen
 
 #' @examples
@@ -15,14 +14,14 @@
 #' x <- AgriResearchIntensity(gdx)
 #' }
 #'
-AgriResearchIntensity <- function(gdx, file = NULL, level = "reg",  debug = FALSE) {
+AgriResearchIntensity <- function(gdx, file = NULL, level = "reg") {
 
   costs <- CostTC(gdx, level = level)
 
   gdp <- income(gdx, level = level, per_capita = FALSE,
                      type = "mer")
 
-  out <- costs/gdp *100
+  agInt <- costs / gdp *100
 
-  return(out, file)
+  out(agInt, file)
 }
