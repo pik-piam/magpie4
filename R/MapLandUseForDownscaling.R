307aff3e9d774bede16048d1fd66fcff1acac644
pvjeetze <vjeetze@pik-potsdam.de>
2022-08-17 20:57:29 +0200

    added cluster option to MapLandUseForDownscaling

--- R/MapLandUseForDownscaling.R
+++ R/MapLandUseForDownscaling.R
@@ -6,10 +6,12 @@
 #' @importFrom magpiesets findset
 #' @export
 #'
+#' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param dir output directory which contains cellular magpie output.
 #' @param downscaling_model The only current option is "SEALS" (Spatial Economic Allocation Landscape Simulator).
 #' @param selectyears Years to provide data for.
+#' @param grid Whether gridded output should be used. Otherwise output will be returned at cluster level.
 #' @return Shares of different land use classes per grid sell as magpie object.
 #' @author Patrick v. Jeetze
 #' @examples
@@ -17,7 +19,7 @@
 #' x <- MapLandUseForDownscaling(dir = ".", downscaling_model = "SEALS")
 #' }
 #'
-MapLandUseForDownscaling <- function(dir = ".", file = NULL, downscaling_model = "SEALS", selectyears = c("y2015", "y2050")) {
+MapLandUseForDownscaling <- function(gdx, dir = ".", file = NULL, downscaling_model = "SEALS", selectyears = c("y2015", "y2050"), grid = TRUE) {
   downscaling_model_list <- c( # List of available models for downscaling:
     "SEALS"
   )
@@ -32,7 +34,12 @@ MapLandUseForDownscaling <- function(dir = ".", file = NULL, downscaling_model =
   selectyears <- sort(findset(selectyears, noset = "original"))
 
   if (downscaling_model == "SEALS") {
-    landShr <- read.magpie(file.path(dir, "cell.land_0.5_share.mz"))[, selectyears, ]
+    if (grid == TRUE) {
+      landShr <- read.magpie(file.path(dir, "cell.land_0.5_share.mz"))[, selectyears, ]
+    } else {
+      landUse <- land(gdx, level = "cell")
+      landShr <- landUse / dimSums(landUse, dim = 3)
+    }
 
     out_seals <- mbind(
       setNames(landShr[, , "crop"], "crop"),
