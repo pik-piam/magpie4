9a7acc632bb178d7595dc4ea03a60ae69420f438
David Chen <davidch@lt916.pik-potsdam.de>
2022-10-12 17:45:18 +0200

    update reportValueMaterialDemand to report total

--- R/reportValueMaterialDemand.R
+++ R/reportValueMaterialDemand.R
@@ -11,17 +11,20 @@
 #' x <- reportValueMaterialDemand(gdx)
 #' }
 #'
-#'
+#' @importFrom magpiesets findset
+
 reportValueMaterialDemand <- function(gdx) {
 
-    out <-  ValueMaterialDemand(gdx,level="regglo")
+    out <-  ValueMaterialDemand(gdx, level = "regglo")
     out <- dimSums(out, dim = 3.2)
 
-    getNames(out,dim=1) <- paste0("Value|Bioeconomy Demand|",
-                                  reportingnames(getNames(out,dim=1)))
+   getNames(out, dim = 1) <- paste0("Value|Bioeconomy Demand|+|",
+                                  reportingnames(getNames(out, dim = 1)))
 
-   out <-  summationhelper(out)
-   getNames(out) <- paste(getNames(out),"(million US$05/yr)",sep=" ")
+   total <- dimSums(out, dim = 3)
+   getNames(total) <-  "Value|Bioeconomy Demand"
+  out <- mbind(total, out)
+   getNames(out) <- paste(getNames(out), "(million US$05/yr)", sep = " ")
 
   return(out)
 }
