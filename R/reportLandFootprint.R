6e3e0aefffbabed13a2919434356ad5bfd176e27
Vartika Singh <51452385+vartika271987@users.noreply.github.com>
2022-10-21 15:38:05 +0530

    Land footprint indicator function added

--- R/reportLandFootprint.R
+++ R/reportLandFootprint.R
@@ -1,11 +1,12 @@
 #' @title reportLandFootprint
-#' @description reports land footprint of food production for aggregated per-capita kcal intake including exogenous scenarios
+#' @description reports land footprint of food production for the population in a region
+#' accounting for traded food assuming perfect substitution between domestic production
+#' and imports
 
 #' @import magpiesets
 #' @export
 #'
 #' @param gdx GDX file
-#' @param detail if detail=F, the subcategories of groups are not reported (e.g. "soybean" within "oilcrops")
 #' @param level spatial aggregation: "reg", "glo", "regglo", "iso"
 #' @return land use for food production per capita (million hectares per-capita)
 #' @author Vartika Singh
@@ -16,15 +17,27 @@
 #'   }
 #'
 
-reportLandFootprint <- function(gdx){
+reportLandFootprint <- function(gdx, level = "regglo") {
 
-population <- population(gdx, level = "regglo")
+population <- population(gdx, level = level)
 
 #reading land information
-land <- land(gdx, level = "regglo", types = NULL, subcategories = c("forestry"), sum = FALSE)
+land <- land(gdx, level = level, types = NULL, subcategories = c("forestry"), sum = FALSE)
+
+#Value of trade of crops in million tonnes
+out <- trade(gdx, level = level, products = "kcr", type = "net-exports")
+
+#land needed to produce the traded commodities will be traded quantity (m tonnes)
+# divided by yield (m tonnes/ha)
+yield <- yields(gdx, level = "regglo", products = "kcr", product_aggr = FALSE,
+                  attributes = "dm", water_aggr = TRUE)
+
+landTrade <- out / yield
+landTradetotal <- dimSums(landTrade, dim = 3, na.rm = TRUE)
+
 
 #total land for food production
-totalLand <- land[, ,"crop.total"] + land[, ,"past.total"]
+totalLand <- land[, , "crop.total"] + land[, , "past.total"] + landTradetotal
 
 landFootprint <- totalLand / population
 
