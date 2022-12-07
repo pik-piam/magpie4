3928b693ddd9f3b21979df52fd28e3300a4e8d0b
Debbora Leip <leip@pik-potsdam.de>
2022-12-07 15:38:09 +0100

    updated aggregation method of wage index

--- R/wageDevelopment.R
+++ R/wageDevelopment.R
@@ -6,7 +6,7 @@
 #'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
-#' @param level Level of regional aggregation ("reg", "glo", "regglo")
+#' @param level Level of regional aggregation ("iso", "reg", "glo", "regglo")
 #' @return MAgPIE object containing indicator on wage development
 #' @author Debbora Leip
 #' @importFrom luscale superAggregate
@@ -17,13 +17,22 @@
 #'
 wageDevelopment <- function(gdx, file = NULL, level = "regglo") {
 
-  x <- readGDX(gdx, "pm_hourly_costs", react = "silent")[, , "scenario", drop = TRUE]
+  x <- readGDX(gdx, "p36_hourly_costs_iso", react = "silent")[, , "scenario", drop = TRUE]
 
   if (!is.null(x)) {
     x <- collapseDim(x / x[, 2020, ], dim = 2.2)
-    pop <- population(gdx, level = "reg")
+    pop <- population(gdx, level = "iso")
     pop[, , ] <- pop[, 2020, ]
-    x <- superAggregate(x, aggr_type = "weighted_mean", weight = pop, level = level)
+    x <- x[, getYears(pop), ]
+    if (level != "iso") {
+      map <- readGDX(gdx, "i_to_iso")
+      x <- toolAggregate(x, rel = map, weight = pop, from = "iso", to = "i", dim = 1)
+      if (level != "reg") {
+        pop <- population(gdx, level = "reg")
+        pop[, , ] <- pop[, 2020, ]
+        x <- superAggregate(x, aggr_type = "weighted_mean", weight = pop, level = level)
+      }
+    }
   }
 
   out(x, file)
