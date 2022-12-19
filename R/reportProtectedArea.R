49f0a59c7e1df5769dc624050e76b41d6c97cd17
pvjeetze <vjeetze@pik-potsdam.de>
2022-12-19 17:02:27 +0100

    land sum bugfix

--- R/reportProtectedArea.R
+++ R/reportProtectedArea.R
@@ -18,13 +18,13 @@ reportProtectedArea <- function(gdx) {
 
   # aggreate and rename
   x <- NULL
-  x <- mbind(x, setNames(a[, , "crop"], paste0("Resources|Land Cover|+|", reportingnames("crop"), "|Protected (million ha)")))
-  x <- mbind(x, setNames(a[, , "past"], paste0("Resources|Land Cover|+|", reportingnames("past"), "|Protected (million ha)")))
-  x <- mbind(x, setNames(a[, , "forestry"], paste0("Resources|Land Cover|Forest|+|", reportingnames(getNames(a[, , "forestry"], dim = 1)), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "crop"], paste0("Resources|Land Cover|", reportingnames("crop"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "past"], paste0("Resources|Land Cover|", reportingnames("past"), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "forestry"], paste0("Resources|Land Cover|Forest|", reportingnames(getNames(a[, , "forestry"], dim = 1)), "|Protected (million ha)")))
   x <- mbind(x, setNames(a[, , "primforest"], paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("primforest"), "|Protected (million ha)")))
   x <- mbind(x, setNames(a[, , "secdforest"], paste0("Resources|Land Cover|Forest|Natural Forest|", reportingnames("secdforest"), "|Protected (million ha)")))
-  x <- mbind(x, setNames(a[, , "urban"], paste0("Resources|Land Cover|+|", reportingnames(getNames(a[, , "urban"], dim = 1)), "|Protected (million ha)")))
-  x <- mbind(x, setNames(a[, , "other"], paste0("Resources|Land Cover|+|", reportingnames(getNames(a[, , "other"], dim = 1)), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "urban"], paste0("Resources|Land Cover|", reportingnames(getNames(a[, , "urban"], dim = 1)), "|Protected (million ha)")))
+  x <- mbind(x, setNames(a[, , "other"], paste0("Resources|Land Cover|", reportingnames(getNames(a[, , "other"], dim = 1)), "|Protected (million ha)")))
 
 
   return(x)
