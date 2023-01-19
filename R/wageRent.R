ace35ed7ce1975622ffe39480cfabf28d1e18653
Debbora Leip <leip@pik-potsdam.de>
2023-01-19 18:43:08 +0100

    adjusted wageRent function to work with productivity increasing wage changes

--- R/wageRent.R
+++ R/wageRent.R
@@ -15,13 +15,16 @@
 #'
 wageRent <- function(gdx, file = NULL, level = "regglo") {
 
-  empl <- agEmployment(gdx = gdx, detail = FALSE, level = "reg")
-
+  # get wage difference, but correcting baseline wages for productivity gain (as that
+  # part is an actual higher cost paied for labor)
   wage <- readGDX(gdx, "pm_hourly_costs")
+  productivityGain <- readGDX(gdx, "pm_productivity_gain_from_wages")
+  wage[, , "baseline"] <- wage[, , "baseline"] * productivityGain
   wageDiff <- wage[, , "scenario", drop = TRUE] - wage[, , "baseline", drop = TRUE]
 
+  # calculate wage rent as employment * wage difference
+  empl <- agEmployment(gdx = gdx, detail = FALSE, level = "reg")
   wageRent <- setNames(empl * wageDiff, "wage_rent")
-
   wageRent <- superAggregate(wageRent, aggr_type = "sum", level = level)
 
   return(wageRent)
