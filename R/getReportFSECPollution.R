3dfe6eec615a5aedb0e2c421948c7c989b4199da
Michael Crawford <mscrawfor@gmail.com>
2023-01-25 16:00:55 +0100

    Added disaggregated nutrient surplus datasets

--- R/getReportFSECPollution.R
+++ R/getReportFSECPollution.R
@@ -11,8 +11,6 @@
 #' calling function.
 #' @return A list of MAgPIE objects containing the reports
 #' @author Michael Crawford
-#' @importFrom madrat toolConditionalReplace
-#' @importFrom utils download.file
 #' @examples
 #'
 #'   \dontrun{
@@ -46,88 +44,53 @@ getReportFSECPollution <- function(magpieOutputDir, reportOutputDir = NULL, scen
     }
 
     # -----------------------------------------------------------------------------------------------------------------
-    # Total nutrient surplus incl. natural vegetation (Mt N)
+    # Nutrient surplus from different land-use types
 
     message("getReportFSECPollution: Calculating total nutrient surplus")
 
-    gdx_path <- file.path(magpieOutputDir, "fulldata.gdx")
+    gdxPath <- file.path(magpieOutputDir, "fulldata.gdx")
 
-    nbCropland        <- reportNitrogenBudgetCropland(gdx_path, grid = TRUE, dir = magpieOutputDir, include_emissions = TRUE)
-    nbPasture         <- reportNitrogenBudgetPasture(gdx_path,  grid = TRUE, dir = magpieOutputDir, include_emissions = TRUE)
-    nbManureExcretion <- reportGridManureExcretion(gdx_path, dir = magpieOutputDir)
-    nbNonAgLand       <- reportNitrogenBudgetNonagland(gdx_path, grid = TRUE, dir = magpieOutputDir)
+    # Cropland
+    croplandBudget  <- reportNitrogenBudgetCropland(gdxPath,
+                                                    grid = TRUE, dir = magpieOutputDir, include_emissions = TRUE)
+    croplandSurplus <- croplandBudget[, , "Nutrient Surplus"]
+    croplandSurplus <- .formatReport(croplandSurplus, "Nutrient surplus from cropland")
+    .saveNetCDFReport(croplandSurplus, file = "nutrientSurplus_cropland", comment = "unit: Mt N")
 
-    # Combined nutrientSurplus, incl. natural vegetation
-    nutrientSurplus <- mbind(nbCropland[, , "Nutrient Surplus"],
-                             nbPasture[, , "Nutrient Surplus"],
-                             nbManureExcretion[, , "Manure|Manure In Confinements|+|Losses"],
-                             nbNonAgLand[, , "Nutrient Surplus"])
+    # Pasture
+    pastureBudget  <- reportNitrogenBudgetPasture(gdxPath,
+                                                  grid = TRUE, dir = magpieOutputDir, include_emissions = TRUE)
+    pastureSurplus <- pastureBudget[, , "Nutrient Surplus"]
+    pastureSurplus <- .formatReport(pastureSurplus, "Nutrient surplus from pasture")
+    .saveNetCDFReport(pastureSurplus, file = "nutrientSurplus_pasture", comment = "unit: Mt N")
 
-    nutrientSurplus <- dimSums(nutrientSurplus, dim = 3)
+    # Manure excretion
+    manureBudget  <- reportGridManureExcretion(gdxPath,
+                                               dir = magpieOutputDir)
+    manureSurplus <- manureBudget[, , "Manure|Manure In Confinements|+|Losses"]
+    manureSurplus <- .formatReport(manureSurplus, "Nutrient surplus from manure losses in confinements")
+    .saveNetCDFReport(manureSurplus, file = "nutrientSurplus_manure", comment = "unit: Mt N")
 
-    # Save formatted report
-    nutrientSurplus <- .formatReport(nutrientSurplus, "Nutrient surplus incl natural vegetation")
-    .saveNetCDFReport(nutrientSurplus,
-                      file = "nutrientSurplus_total",
-                      comment = "unit: Mt N")
+    # Non-agricultural land
+    nonAgLandBudget <- reportNitrogenBudgetNonagland(gdxPath,
+                                                     grid = TRUE, dir = magpieOutputDir)
+    nonAgLandSurplus <- nonAgLandBudget[, , "Nutrient Surplus"]
+    nonAgLandSurplus <- .formatReport(nonAgLandSurplus, "Nutrient surplus from non-agricultural land")
+    .saveNetCDFReport(nonAgLandSurplus, file = "nutrientSurplus_nonAgLand", comment = "unit: Mt N")
 
-    # -----------------------------------------------------------------------------------------------------------------
-    # Nutrient surplus intensity, incl. natural vegetation (kg N / ha)
-
-    message("getReportFSECPollution: Calculating nutrient surplus per total area")
-
-    # Total land
-    gridLand  <- reportGridLand(gdx_path, dir = magpieOutputDir)
-    totalLand <- dimSums(gridLand, dim = 3)
-
-    # Calculate intensity of nutrient surplus
-    nutrientSurplus_perTotalArea <- (nutrientSurplus / totalLand) * 1000 # Mt X / Mha to kg X / ha
-
-    # Five cells have 0 "totalLand", which leads to INFs
-    nutrientSurplus_perTotalArea <- toolConditionalReplace(x = nutrientSurplus_perTotalArea,
-                                                           conditions = "!is.finite()",
-                                                           replaceby = 0)
-
-    # Save formatted report
-    nutrientSurplus_perTotalArea <- .formatReport(nutrientSurplus_perTotalArea, "Nutrient surplus intensity, incl natural vegetation")
-    .saveNetCDFReport(nutrientSurplus_perTotalArea,
-                      file = "nutrientSurplus_intensity",
-                      comment = "unit: kg N / ha")
-
-
-    # -----------------------------------------------------------------------------------------------------------------
-    # Nutrient surplus exposure
-
-    message("getReportFSECPollution: Calculating population-weighted nutrient surplus exposure")
-
-    # Read population datasets
-    popFile <- file.path(magpieOutputDir, "../../input/FSEC_populationScenarios", "FSEC_populationScenarios_v2_22-08-22.mz")
-    pop <- read.magpie(popFile)
-
-    config <- gms::loadConfig(file.path(magpieOutputDir, "config.yml"))
-    pop <- pop[, , config$gms$c09_pop_scenario]
-    getNames(pop) <- "value"
-
-    # Ensure alignment of years
-    yearsPresent <- Reduce(f = intersect, x = Map(getYears, list(nutrientSurplus_perTotalArea, pop)))
-    pop <- pop[, yearsPresent, ]
-
-    # Round off projections' fractions of people and use persons rather than millions persons
-    pop <- round(pop * 1E6)
-
-    nutrientSurplus_populationExposure <- nutrientSurplus_perTotalArea * pop
-
-    nutrientSurplus_populationExposure <- .formatReport(nutrientSurplus_populationExposure, "Nutrient surplus exposure")
-    .saveNetCDFReport(nutrientSurplus_populationExposure,
-                      file = "nutrientSurplus_populationExposure",
-                      comment = "unit: (kg N / ha) * person")
-
+    # Calculate total nutrient surplus
+    total <- mbind(croplandSurplus, pastureSurplus, manureSurplus, nonAgLandSurplus)
+    total <- dimSums(total, dim = 3)
+    total <- .formatReport(total, "Nutrient surplus from land and manure management")
+    .saveNetCDFReport(total, file = "nutrientSurplus_total", comment = "unit: Mt N")
 
     # -----------------------------------------------------------------------------------------------------------------
     # Return
 
-    return(list("nutrientSurplus_total"               = nutrientSurplus,
-                "nutrientSurplus_perTotalArea"        = nutrientSurplus_perTotalArea,
-                "nutrientSurplus_populationExposure"  = nutrientSurplus_populationExposure))
+    return(list("nutrientSurplus_cropland"  = croplandSurplus,
+                "nutrientSurplus_pasture"   = pastureSurplus,
+                "nutrientSurplus_manure"    = manureSurplus,
+                "nutrientSurplus_nonAgLand" = nonAgLandSurplus,
+                "nutrientSurplus_total"     = total))
 
 }
