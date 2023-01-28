66da8ed269e28780c18535c5dd7ea4af149cbb97
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2023-01-28 10:40:56 +0100

    take care for cases module is not activated

--- R/taxRevenueRotations.R
+++ R/taxRevenueRotations.R
@@ -22,47 +22,56 @@
 
 taxRevenueRotations <- function(gdx, file = NULL, level = "regglo", dir = ".", penalty="onlyTaxRevenue") {
 
-  # for testing
   vm_rotation_penalty <- collapseNames(readGDX(gdx, "ov_rotation_penalty")[,,"level"])
-  i30_rotation_incentives <- readGDX(gdx, "i30_rotation_incentives")[,getYears(vm_rotation_penalty),]
 
-  # for reproduction
-  f30_rotation_incentives <- readGDX(gdx, "f30_rotation_incentives")
-  i30_rotation_incentives <- readGDX(gdx, "i30_rotation_incentives")[,getYears(vm_rotation_penalty),]
-  v30_penalty <- collapseNames(readGDX(gdx, "ov30_penalty")[,,"level"])
-  v30_penalty_max_irrig <- collapseNames(readGDX(gdx,"ov30_penalty_max_irrig")[,,"level"])
-  rotamax_red30 <- readGDX(gdx,"rotamax_red30")
+  if (suppressWarnings(is.null(readGDX(gdx, "i30_rotation_incentives")))) {
+    cat("rotation module not activated, so tax Revenues set to zero")
+    if(sum(vm_rotation_penalty)>0) {stop("rotation penalty should be zero if module is not activated")}
+    tax=vm_rotation_penalty
+  } else {
 
-  full_penalty= (
-    dimSums(v30_penalty * i30_rotation_incentives,dim="rota30") +
-      dimSums(v30_penalty_max_irrig * i30_rotation_incentives[,,rotamax_red30], dim="rotamax30")
-  )
+    # for testing
 
-  internalized_services =
-    (
-      dimSums(v30_penalty * f30_rotation_incentives[,,"default"],dim="rota30") +
-        dimSums(v30_penalty_max_irrig * f30_rotation_incentives[,,"default"][,,readGDX(gdx,"rotamax_red30")], dim="rotamax30")
+    i30_rotation_incentives <- readGDX(gdx, "i30_rotation_incentives")[,getYears(vm_rotation_penalty),]
+
+    # for reproduction
+    f30_rotation_incentives <- readGDX(gdx, "f30_rotation_incentives")
+    i30_rotation_incentives <- readGDX(gdx, "i30_rotation_incentives")[,getYears(vm_rotation_penalty),]
+    v30_penalty <- collapseNames(readGDX(gdx, "ov30_penalty")[,,"level"])
+    v30_penalty_max_irrig <- collapseNames(readGDX(gdx,"ov30_penalty_max_irrig")[,,"level"])
+    rotamax_red30 <- readGDX(gdx,"rotamax_red30")
+
+    full_penalty= (
+      dimSums(v30_penalty * i30_rotation_incentives,dim="rota30") +
+        dimSums(v30_penalty_max_irrig * i30_rotation_incentives[,,rotamax_red30], dim="rotamax30")
     )
 
-  tax = full_penalty-internalized_services
+    internalized_services =
+      (
+        dimSums(v30_penalty * f30_rotation_incentives[,,"default"],dim="rota30") +
+          dimSums(v30_penalty_max_irrig * f30_rotation_incentives[,,"default"][,,readGDX(gdx,"rotamax_red30")], dim="rotamax30")
+      )
 
-  test <- FALSE
-  if (test==TRUE){
-    testvalue1 = sum((vm_rotation_penalty))
-    testvalue2 = sum((
-      dimSums(v30_penalty * i30_rotation_incentives,dim="rota30") +
-      dimSums(v30_penalty_max_irrig * i30_rotation_incentives[,,readGDX(gdx,"rotamax_red30")], dim="rotamax30")
-    ))
-    if (abs(testvalue1-testvalue2)>0.1) {stop("test not passed")}
-    if (sum(tax[,"y2000"])!=0) {stop("Tax should be zero in 2000")}
+    tax = full_penalty-internalized_services
+
+    test <- FALSE
+    if (test==TRUE){
+      testvalue1 = sum((vm_rotation_penalty))
+      testvalue2 = sum((
+        dimSums(v30_penalty * i30_rotation_incentives,dim="rota30") +
+        dimSums(v30_penalty_max_irrig * i30_rotation_incentives[,,readGDX(gdx,"rotamax_red30")], dim="rotamax30")
+      ))
+      if (abs(testvalue1-testvalue2)>0.1) {stop("test not passed")}
+      if (sum(tax[,"y2000"])!=0) {stop("Tax should be zero in 2000")}
+    }
+
+    if (penalty=="FullPenalty") {
+      tax <- full_penalty
+    } else if (penalty=="OnlyInternalizedService") {
+      tax <- internalized_services
+    }
+
   }
-
-  if (penalty=="FullPenalty") {
-    tax <- full_penalty
-  } else if (penalty=="OnlyInternalizedService") {
-    tax <- internalized_services
-  }
-
   out <- gdxAggregate(gdx = gdx,x = tax, to = level,
                       weight = "land", types = "crop", absolute = TRUE,
                       dir = dir)
