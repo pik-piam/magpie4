b760eaea8e91f824fbea458e30a32ad19c9a930c
Debbora Leip <leip@pik-potsdam.de>
2023-01-29 10:48:56 +0100

    replaced superAggregate by gdxAggregate in wageRent

--- R/wageRent.R
+++ R/wageRent.R
@@ -6,7 +6,6 @@
 #' @param file a file name the output should be written to using write.magpie
 #' @param level aggregation level, reg, glo or regglo
 #' @author Debbora Leip
-#' @importFrom luscale superAggregate
 
 #' @examples
 #' \dontrun{
@@ -30,7 +29,7 @@ wageRent <- function(gdx, file = NULL, level = "regglo") {
 
   # calculate wage rent as employment * weekly hours * weeks in year* wage difference
   wageRent <- setNames(totalHours * wageDiff, "wage_rent")
-  wageRent <- superAggregate(wageRent, aggr_type = "sum", level = level)
+  wageRent <- gdxAggregate(gdx, wageRent, to = level, absolte = TRUE)
 
   return(wageRent)
 }
