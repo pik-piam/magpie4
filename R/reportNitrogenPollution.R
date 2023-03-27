5b6064a9ece4e7dde9a52044d17de6bce71b027d
Michael Crawford <mscrawfor@gmail.com>
2023-03-27 20:00:32 +0200

    Added added land and AWS to reportNitrogenPollution

--- R/reportNitrogenPollution.R
+++ R/reportNitrogenPollution.R
@@ -22,20 +22,29 @@ reportNitrogenPollution <- function(gdx, dir = ".") {
   awms <- dimSums(readGDX(gdx,"ov_manure_confinement")[,,"level"][,,"nr"],dim=3)-dimSums(readGDX(gdx,"ov_manure_recycling")[,,"level"][,,"nr"],dim=3)
   consumption <- dimSums(demand(gdx,attributes="nr",product_aggr=TRUE)[,,c("food","other_util","bioenergy","waste")],dim=3)
   nonagland <- dimSums(NitrogenBudgetNonagland(gdx, level = "reg", dir = dir)[,,"surplus"],dim=3)
-  combined = mbind(
+
+  combined <- mbind(
     setNames(cropland, "Resources|Nitrogen|Pollution|Surplus|+|Cropland (Mt Nr/yr)"),
     setNames(pasture, "Resources|Nitrogen|Pollution|Surplus|+|Pasture (Mt Nr/yr)"),
     setNames(awms, "Resources|Nitrogen|Pollution|Surplus|+|Animal Waste Management (Mt Nr/yr)"),
     setNames(nonagland, "Resources|Nitrogen|Pollution|Surplus|+|Non-agricultural land (Mt Nr/yr)"),
     setNames(consumption, "Resources|Nitrogen|Pollution|Surplus|+|End of life losses (Mt Nr/yr)")
   )
-  combined = mbind(
+  combined <- mbind(
     setNames(dimSums(combined,dim=3),"Resources|Nitrogen|Pollution|Surplus (Mt Nr/yr)"),
     combined
   )
-  combined = mbind(
+
+  landAndAWMS <- dimSums(mbind(cropland, pasture, awms, nonagland), dim = 3)
+  combined <- mbind(
+    setNames(landAndAWMS, "Resources|Nitrogen|Nutrient surplus from land and manure management (Mt Nr/yr)"),
+    combined
+  )
+
+  combined <- mbind(
     combined,
     setCells(dimSums(combined,dim=1),"GLO")
   )
+
   return(combined)
 }
