efa72c6c6534d941e2c42f04ab438291acc04526
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-05-17 17:15:26 +0200

    added addGeometry

---
+++ R/addGeometry.R
@@ -0,0 +1,33 @@
+#' @title addGeometry
+#'
+#' @description Enriches land use data on cluster resolution geometry information as required for conversion
+#' by magclass::as.SpatVector
+#'
+#' @param x Landuse data on cluster/cell resolution as a magclass object
+#' @param clustermap A dataframe mapping with columns cluster, cell, and country
+#' @return A magclass object enriched with geometry information
+#' @author Jan Philipp Dietrich
+#' @examples
+#' \dontrun{
+#' landUse <- magpie4::land("fulldata.gdx", level = "cell")
+#' clustermap <- readRDS(Sys.glob("clustermap_*.rds"))
+#' landUseEnriched <- magpie4::addGeometry(landUse, clustermap)
+#' attr(landUseEnriched, "geometry")
+#' attr(landUseEnriched, "crs")
+#' }
+#'
+#' @export
+
+addGeometry <- function(x, clustermap) {
+  id <- as.data.frame(magclass::getItems(x, dim = 1, split = TRUE, full = TRUE))
+  id[[2]] <- as.integer(id[[2]])
+  cells <- magclass::getItems(x, dim = 1)
+  clusterMagclass <- magclass::new.magpie(cells, names = "clusterId", fill = id[[2]])
+  clusterMagclass <- madrat::toolAggregate(clusterMagclass, clustermap, from = "cluster", to = "cell")
+  clusterPolygons <- terra::as.polygons(magclass::as.SpatRaster(clusterMagclass))
+  terra::crs(clusterPolygons) <- "+proj=longlat +datum=WGS84 +no_defs"
+  m <- magclass::as.magpie(clusterPolygons)
+  attr(x, "geometry") <- attr(m, "geometry")
+  attr(x, "crs") <- attr(m, "crs")
+  return(x)
+}
--- R/clusterOutputToTerraVector.R
+++ R/clusterOutputToTerraVector.R
@@ -20,23 +20,7 @@ clusterOutputToTerraVector <- function(x, clustermap) {
   if (!requireNamespace("terra", quietly = TRUE)) {
     stop("terra is required for clusterOutputToTerraVector, please run `install.packages('terra')`")
   }
-
   stopifnot(identical(names(dimnames(x)), c("j.region", "t", "land")))
-
-  # extract spatial IDs
-  id <- as.data.frame(magclass::getItems(x, dim = 1, split = TRUE, full = TRUE))
-  id[[2]] <- as.integer(id[[2]])
-
-  # generate polygons
-  cells <- magclass::getItems(x, dim = 1)
-  clusterMagclass <- magclass::new.magpie(cells, names = "clusterId", fill = id[[2]])
-  clusterMagclass <- madrat::toolAggregate(clusterMagclass, clustermap, from = "cluster", to = "cell")
-  clusterPolygons <- terra::as.polygons(magclass::as.SpatRaster(clusterMagclass))
-  terra::crs(clusterPolygons) <- "+proj=longlat +datum=WGS84 +no_defs"
-
-  # fill in data
-  data  <- cbind(id, as.data.frame(magclass::wrap(as.array(x), list(1, 2:3), sep = "..")))
-  clusterPolygons <- terra::merge(clusterPolygons, data, by.x = "clusterId", by.y = "region")
-
-  return(clusterPolygons)
+  x <- addGeometry(x, clustermap)
+  return(magclass::as.SpatVector(x))
 }
