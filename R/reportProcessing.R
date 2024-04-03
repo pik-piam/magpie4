882263e64de591377d538ce7f98741e902f5237e
Debbora Leip <leip@pik-potsdam.de>
2024-04-03 10:44:13 +0200

    fix for summation issue in processing raw material

--- R/reportProcessing.R
+++ R/reportProcessing.R
@@ -39,7 +39,7 @@ reportProcessing <- function(gdx, detail = TRUE, indicator = "primary_to_process
 
     getNames(out) <- sub(getNames(out), pattern = "\\.", replacement = "|")
     getNames(out) <- paste(getNames(out), "(Mt DM/yr)", sep = " ")
-    getNames(out) <- sub(getNames(out), pattern = "Processing|+", replacement = "Processing", fixed = TRUE)
+    getNames(out) <- sub(getNames(out), pattern = "Processing|+", replacement = "Processing|++", fixed = TRUE)
 
     return(out)
 
@@ -47,14 +47,24 @@ reportProcessing <- function(gdx, detail = TRUE, indicator = "primary_to_process
 
     x <-  processing(gdx, level = "regglo", indicator = "secondary_from_primary")
     x <- dimOrder(x, c(2, 1))
+    getNames(x, dim = 1) <- reportingnames(getNames(x, dim = 1))
     getNames(x, dim = 2) <- reportingnames(getNames(x, dim = 2))
+    getNames(x, dim = 1) <- paste0("Processing|Raw material|Processed into ", getNames(x, dim = 1))
+    x <- add_columns(x, addnm = "Processing|Raw material|Processed into Secondary products", dim = 3.1, fill = 0)
+    x[, , "Processing|Raw material|Processed into Secondary products"] <- dimSums(x, dim = 3.1)
+    x <- add_columns(x, addnm = "dummy", dim = 3.2, fill = 0)
+    x[, , "dummy"] <- dimSums(x, dim = 3.2)
 
-
-    x <- reporthelper(x = x, level_zero_name = paste0("Processing|Raw material"), detail = detail, dim = 3.1)
     getNames(x) <- sub(getNames(x), pattern = "\\.", replacement = "|")
+    x <- summationhelper(x)
+    getNames(x) <- sub(getNames(x), pattern = "\\|\\+\\|dummy", replacement = "")
     getNames(x) <- paste(getNames(x), "(Mt DM/yr)", sep = " ")
 
-    out <- summationhelper(x)
+    out <- x[, , where(x != 0)$true$data]
+
+    if (detail == FALSE) {
+      out <- out[, , "Secondary products", pmatch = TRUE]
+    }
 
     return(out)
   }
