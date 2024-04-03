aa31d6aeaf0a22b255ebfa88d9042bfd65338882
caviddhen <david.chen@pik.potsdam.de>
2024-04-03 18:44:05 +0200

    update producer price index reporting for agmip

--- R/reportProducerPriceIndex.R
+++ R/reportProducerPriceIndex.R
@@ -5,7 +5,10 @@
 #' 
 #' @param gdx GDX file
 #' @return Producer price index as MAgPIE object Unit: see names
-#' @author Isabelle Weindl
+#' @author Isabelle Weindl, David M CHen
+#' @param prod_groups whether to return only product groups
+
+#' @import magpiesets
 #' @examples
 #' 
 #'   \dontrun{
@@ -13,21 +16,40 @@
 #'   }
 #' 
 
-reportProducerPriceIndex <- function(gdx){
+reportProducerPriceIndex <- function(gdx, prod_groups = FALSE){
   
+  if (prod_groups) {
   #read in data
-  primary <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2010", type="producer")
-  crops <- priceIndex(gdx,level="regglo", products="kcr", baseyear = "y2010", type="producer")
-  livestock <- priceIndex(gdx,level="regglo", products="kli", baseyear = "y2010", type="producer")
-  bioenergy <- priceIndex(gdx,level="regglo", products=c("begr","betr"), baseyear = "y2010", type="producer")
+  primary <- priceIndex(gdx,level="regglo", products="kfo", baseyear = "y2020", type="producer")
+  crops <- priceIndex(gdx,level="regglo", products="kcr", baseyear = "y2020", type="producer")
+  livestock <- priceIndex(gdx,level="regglo", products="kli", baseyear = "y2020", type="producer")
+  bioenergy <- priceIndex(gdx,level="regglo", products=c("begr","betr"), baseyear = "y2020", type="producer")
   
   #rename
-  getNames(primary) <- "Prices|Producer Price Index|Primary food products (Index 2010=100)"
-  getNames(crops) <- paste0("Prices|Producer Price Index|",reportingnames("kcr")," (Index 2010=100)",sep="")
-  getNames(livestock) <- paste0("Prices|Producer Price Index|",reportingnames("kli")," (Index 2010=100)",sep="")
-  getNames(bioenergy) <- "Prices|Producer Price Index|Bioenergy (Index 2010=100)"
+  getNames(primary) <- "Prices|Producer Price Index|Primary food products (Index 2020=100)"
+  getNames(crops) <- paste0("Prices|Producer Price Index|",reportingnames("kcr")," (Index 2020=100)",sep="")
+  getNames(livestock) <- paste0("Prices|Producer Price Index|",reportingnames("kli")," (Index 2020=100)",sep="")
+  getNames(bioenergy) <- "Prices|Producer Price Index|Bioenergy (Index 2020=100)"
   
-  out <- mbind(primary,crops,livestock,bioenergy)
+  out <- mbind(primary,crops,livestock,bioenergy) 
+  
+  } else {
+  pr <- priceIndex(gdx, level="regglo", products="kall", product_aggr = FALSE, 
+                   baseyear = "y2020", type="producer")
+   
+    getNames(pr, dim = 1) <- paste0("Prices|Producer Price Index|", reportingnames(getNames(pr, dim = 1)), 
+                                    " (Index 2020=100)")
+  crops <- priceIndex(gdx,level="regglo", products="kcr", baseyear = "y2020", type="producer")
+  getNames(crops) <- paste0("Prices|Producer Price Index|",reportingnames("kcr")," (Index 2020=100)",sep="")
+  livestock <- priceIndex(gdx,level="regglo", products="kli", baseyear = "y2020", type="producer")
+  getNames(livestock) <- paste0("Prices|Producer Price Index|",reportingnames("kli")," (Index 2020=100)",sep="")
+  all <-  priceIndex(gdx,level="regglo", products="kall", baseyear = "y2020", type="producer")
+  getNames(all) <- paste0("Prices|Producer Price Index|",reportingnames("kall")," (Index 2020=100)",sep="")
+
+out <- mbind(pr, crops)
+out <- mbind(out, livestock)
+out <- mbind(out, all)
+  }
   
   return(out)
 }
