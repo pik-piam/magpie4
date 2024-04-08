b93b734f200c2b818d9acf2c5dc0e6c937561c6d
Pascal Sauer <pascal.sauer@pik-potsdam.de>
2024-04-08 11:16:07 +0200

    conditional tau

--- R/reportTau.R
+++ R/reportTau.R
@@ -13,11 +13,12 @@
 reportTau <- function(gdx) {
   pt <- NULL
   tau <- readGDX(gdx, "ov_tau", format = "first_found")[, , "level"]
-  if (any(grepl("pastr", getItems(tau, dim = 3)))) {
-      pt <- tau(gdx = gdx, level = "regglo", type = "pastr")
-      if (!is.null(pt)) {
-        getNames(pt) <- "Productivity|Landuse Intensity Indicator Tau managed pastures (Index)"
-      }
+  grassArea <- readGDX(gdx, "ov31_grass_area", format = "first_found", react = "silent")[, , "pastr.level"]
+  if (any(grepl("pastr", getItems(tau, dim = 3))) && !is.null(grassArea)) {
+    pt <- tau(gdx = gdx, level = "regglo", type = "pastr")
+    if (!is.null(pt)) {
+      getNames(pt) <- "Productivity|Landuse Intensity Indicator Tau managed pastures (Index)"
+    }
   }
   cr <- tau(gdx = gdx, level = "regglo", type = "crop")
   getNames(cr) <- "Productivity|Landuse Intensity Indicator Tau (Index)"
