14db2d93947280e158c8b857a7f363f351b4d579
florianh <humpenoeder@pik-potsdam.de>
2024-06-10 17:41:47 +0200

    bugfix duplicate entries

--- R/reportCroparea.R
+++ R/reportCroparea.R
@@ -29,14 +29,6 @@ reportCroparea <- function(gdx, detail = FALSE) {
                       level_zero_name = ReportingnameCroparea, detail = detail)
   getNames(out) <- paste(gsub("\\.", "|", getNames(out)), "(million ha)", sep = " ")
   out <- summationhelper(out, sep = "+")
-  getNames(fallowland) <- paste0(
-    "Resources|Land Cover|Cropland|+|",
-    reportingnames(getNames(fallowland)), " (million ha)")
-  getNames(treecover) <- paste0(
-    "Resources|Land Cover|Cropland|+|",
-    reportingnames(getNames(treecover)), " (million ha)")
-  x <- mbind(x, fallowland, treecover, out)
-
   out <- croparea(gdx, level = "regglo", products = "kcr",
                   product_aggr = FALSE, water_aggr = FALSE)
   total <- dimSums(out, dim = 3.1)
