fb3aab3cc794931a47dca04838d605f18283d6a2
florianh <humpenoeder@pik-potsdam.de>
2024-10-16 09:16:13 +0200

    update emission reporting reforestation / afforestation

--- R/reportEmissions.R
+++ R/reportEmissions.R
@@ -191,6 +191,7 @@ reportEmissions <- function(gdx, storageWood = TRUE) {
   }
 
   # nolint start
+  s32_aff_plantation <- readGDX(gdx,"s32_aff_plantation")
   emissionsReport <- with(yearlyCO2, mbind(
     emissionsReport,
 
@@ -218,8 +219,11 @@ reportEmissions <- function(gdx, storageWood = TRUE) {
 
     # Regrowth
     setNames(dimSums(regrowth, dim = 3),          "Emissions|CO2|Land|Land-use Change|+|Regrowth (Mt CO2/yr)"),
-    setNames(regrowth[, , "forestry_aff"],        "Emissions|CO2|Land|Land-use Change|Regrowth|+|CO2-price AR (Mt CO2/yr)"),
-    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land|Land-use Change|Regrowth|+|NPI_NDC AR (Mt CO2/yr)"),
+    setNames(regrowth[, , "forestry_aff"],
+             paste0("Emissions|CO2|Land|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Reforestation" else "Afforestation"," (Mt CO2/yr)")),
+    setNames(new.magpie(getRegions(regrowth), getYears(regrowth), NULL, fill = 0,sets = getSets(regrowth)),
+             paste0("Emissions|CO2|Land|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Afforestation" else "Reforestation"," (Mt CO2/yr)")),
+    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land|Land-use Change|Regrowth|+|NPI_NDC Reforestation (Mt CO2/yr)"),
     setNames(regrowth[, , "forestry_plant"],      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Timber Plantations (Mt CO2/yr)"),
     setNames(regrowth[, , "crop_treecover"],      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Cropland Tree Cover (Mt CO2/yr)"),
     setNames(regrowth[, , "secdforest"],          "Emissions|CO2|Land|Land-use Change|Regrowth|+|Secondary Forest (Mt CO2/yr)"),
@@ -316,8 +320,11 @@ reportEmissions <- function(gdx, storageWood = TRUE) {
 
     # Regrowth
     setNames(dimSums(regrowth, dim = 3),          "Emissions|CO2|Land RAW|Land-use Change|+|Regrowth (Mt CO2/yr)"),
-    setNames(regrowth[, , "forestry_aff"],        "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|CO2-price AR (Mt CO2/yr)"),
-    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|NPI_NDC AR (Mt CO2/yr)"),
+    setNames(regrowth[, , "forestry_aff"],
+             paste0("Emissions|CO2|Land RAW|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Reforestation" else "Afforestation"," (Mt CO2/yr)")),
+    setNames(new.magpie(getRegions(regrowth), getYears(regrowth), NULL, fill = 0,sets = getSets(regrowth)),
+             paste0("Emissions|CO2|Land RAW|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Afforestation" else "Reforestation"," (Mt CO2/yr)")),
+    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|NPI_NDC Reforestation (Mt CO2/yr)"),
     setNames(regrowth[, , "forestry_plant"],      "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|Timber Plantations (Mt CO2/yr)"),
     setNames(regrowth[, , "crop_treecover"],      "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|Cropland Tree Cover (Mt CO2/yr)"),
     setNames(regrowth[, , "secdforest"],          "Emissions|CO2|Land RAW|Land-use Change|Regrowth|+|Secondary Forest (Mt CO2/yr)"),
@@ -413,8 +420,11 @@ reportEmissions <- function(gdx, storageWood = TRUE) {
 
     # Regrowth
     setNames(dimSums(regrowth, dim = 3),          "Emissions|CO2|Land|Cumulative|Land-use Change|+|Regrowth (Gt CO2)"),
-    setNames(regrowth[, , "forestry_aff"],        "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|CO2-price AR (Gt CO2)"),
-    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|NPI_NDC AR (Gt CO2)"),
+    setNames(regrowth[, , "forestry_aff"],
+             paste0("Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Reforestation" else "Afforestation"," (Mt CO2/yr)")),
+    setNames(new.magpie(getRegions(regrowth), getYears(regrowth), NULL, fill = 0,sets = getSets(regrowth)),
+             paste0("Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|CO2-price ",if (s32_aff_plantation==0) "Afforestation" else "Reforestation"," (Mt CO2/yr)")),
+    setNames(regrowth[, , "forestry_ndc"],        "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|NPI_NDC Reforestation (Gt CO2)"),
     setNames(regrowth[, , "forestry_plant"],      "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|Timber Plantations (Gt CO2)"),
     setNames(regrowth[, , "crop_treecover"],      "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|Cropland Tree Cover (Gt CO2)"),
     setNames(regrowth[, , "secdforest"],          "Emissions|CO2|Land|Cumulative|Land-use Change|Regrowth|+|Secondary Forest (Gt CO2)"),
