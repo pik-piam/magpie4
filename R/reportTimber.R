ce84a7959d5980c8e09073e3ac656ed4991619ec
florianh <humpenoeder@pik-potsdam.de>
2024-11-15 10:28:05 +0100

    update timber reporting

--- R/reportTimber.R
+++ R/reportTimber.R
@@ -22,15 +22,15 @@ reportTimber<-function(gdx){
     a <- mbind(a,setNames(dimSums(a,dim = 3.2),paste0(getNames(dimSums(a,dim = 3.2)),".+|Roundwood")))
 
     dem <- collapseNames(a[,,"Demand"])
-    getNames(dem) <- paste0("Timber|Volumetric|Demand|",getNames(dem))
+    getNames(dem) <- paste0("Timber|Volumetric|Demand|+|Roundwood|",getNames(dem))
     getNames(dem) <- paste0(getNames(dem)," (Mm3/yr)")
 
     prod <- collapseNames(a[,,"Production"])
-    getNames(prod) <- paste0("Timber|Volumetric|Production|",getNames(prod))
+    getNames(prod) <- paste0("Timber|Volumetric|Production|+|Roundwood|",getNames(prod))
     getNames(prod) <- paste0(getNames(prod)," (Mm3/yr)")
 
     heaven <- collapseNames(a[,,"Heaven"])
-    getNames(heaven) <- paste0("Timber|Volumetric|Infeasible|",getNames(heaven))
+    getNames(heaven) <- paste0("Timber|Volumetric|Infeasible|+|Roundwood|",getNames(heaven))
     getNames(heaven) <- paste0(getNames(heaven)," (Mm3/yr)")
 
     out <- mbind(dem, prod, heaven)
