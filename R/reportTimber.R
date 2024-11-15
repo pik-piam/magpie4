837ad81783c7fe1af227c42cd95edae485c42518
florianh <humpenoeder@pik-potsdam.de>
2024-11-15 11:11:55 +0100

    bugfix

--- R/reportTimber.R
+++ R/reportTimber.R
@@ -19,19 +19,21 @@ reportTimber<-function(gdx){
   if(suppressWarnings(!is.null(readGDX(gdx,"fcostsALL")))){
     a <- Timber(gdx,level = "regglo")
     getNames(a,dim=2) <- reportingnames(getNames(a,dim=2))
-    a <- mbind(a,setNames(dimSums(a,dim = 3.2),paste0(getNames(dimSums(a,dim = 3.2)),".+|Roundwood")))
 
     dem <- collapseNames(a[,,"Demand"])
-    getNames(dem) <- paste0("Timber|Volumetric|Demand|+|Roundwood|",getNames(dem))
+    getNames(dem) <- paste0("Timber|Volumetric|Demand|Roundwood|+|",getNames(dem))
     getNames(dem) <- paste0(getNames(dem)," (Mm3/yr)")
+    dem <- mbind(setNames(dimSums(dem,dim = 3),"Timber|Volumetric|Demand|+|Roundwood"),dem)
 
     prod <- collapseNames(a[,,"Production"])
     getNames(prod) <- paste0("Timber|Volumetric|Production|+|Roundwood|",getNames(prod))
     getNames(prod) <- paste0(getNames(prod)," (Mm3/yr)")
+    prod <- mbind(setNames(dimSums(prod,dim = 3),"Timber|Volumetric|Production|+|Roundwood"),prod)
 
     heaven <- collapseNames(a[,,"Heaven"])
     getNames(heaven) <- paste0("Timber|Volumetric|Infeasible|+|Roundwood|",getNames(heaven))
     getNames(heaven) <- paste0(getNames(heaven)," (Mm3/yr)")
+    heaven <- mbind(setNames(dimSums(heaven,dim = 3),"Timber|Volumetric|Infeasible|+|Roundwood"),heaven)
 
     out <- mbind(dem, prod, heaven)
   } else {message("Disabled (no timber) ", appendLF = FALSE)}
