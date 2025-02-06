6b60cf9ecc02461a70eb36b26e4cd30e7ca91db9
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2025-02-06 18:38:05 +0100

    infinite values in yields and priceIndex are now being replaced with NAs

--- R/priceIndex.R
+++ R/priceIndex.R
@@ -5,13 +5,19 @@
 #'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
-#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
+#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or
+#' any other aggregation level defined in superAggregate
 #' @param products Selection of products (either by naming products, e.g. "tece", or naming a set,e.g."kcr")
-#' @param index "lasp" (Laspeyres-Index: baseyear weighting), "paas" (Paasche-Index: current weighting), "fish" (Fisher-Index: geometric mean of "lasp" and "paas")
-#' @param chain Chain Index: if true, the base period for each time period is the immediately preceding time period. Can be combined with all of the above indices
+#' @param index "lasp" (Laspeyres-Index: baseyear weighting), "paas" (Paasche-Index: current weighting),
+#' "fish" (Fisher-Index: geometric mean of "lasp" and "paas")
+#' @param chain Chain Index: if true, the base period for each time period is the immediately preceding time period.
+#' Can be combined with all of the above indices
 #' @param baseyear baseyear of the price index
 #' @param round shall the results be rounded?
-#' @param type For whom are the prices important? "producer" are the prices that farmer face, as they also produce intermediate products (seed, feed). "consumer" are the prices for the end consumer faces (supermarket, bioenergy plant). Currently, the only difference is the basket composition (ideally, also prices should differ between regions)
+#' @param type For whom are the prices important? "producer" are the prices that farmer face, as they also produce
+#' intermediate products (seed, feed). "consumer" are the prices for the end consumer faces
+#' (supermarket, bioenergy plant).
+#' Currently, the only difference is the basket composition (ideally, also prices should differ between regions)
 #' @param product_aggr aggregate over products or not (boolean)
 #' @return A MAgPIE object containing price indices for consumers or producers (depending on type)
 #' @author Jan Philipp Dietrich, Florian Humpenoeder, Benjamin Bodirsky
@@ -23,86 +29,151 @@
 #'
 #' @importFrom magclass as.magpie
 
-priceIndex <- function(gdx, file = NULL, level = "reg", products = "kall", index = "lasp", chain = FALSE,
-                       baseyear = "y2005", round = TRUE, type = "consumer", product_aggr = TRUE) {
-  if (chain==FALSE) {
-    if (index == "lasp" | index == "paas") {
-      if(type=="producer") {
-        q_t <- readGDX(gdx,"ov_supply",select = list(type="level"),react = "warning")
-        p_t <- prices(gdx,type="producer")
-      } else if (type =="consumer") {
-        q_t <- dimSums(demand(gdx)[,,c("food","other_util","bioenergy")],dim=3.1)
-        p_t <- prices(gdx,type="consumer",products=products)
-      } else {stop("invalid type")}
-      if (!all(products%in%readGDX(gdx,"kall"))) products<-readGDX(gdx, products)
-      if (suppressWarnings(is.null(readGDX(gdx,"fcostsALL")))) {
-        # products <- products[-which(products=="wood")]
-        # products <- products[-which(products=="woodfuel")]
-        if ("wood" %in% products)     products <- products[-which(products=="wood")]
-        if ("woodfuel" %in% products) products <- products[-which(products=="woodfuel")]
-        if ("constr_wood" %in% products) products <- products[-which(products=="constr_wood")]
+priceIndex <- function(gdx,
+                       file = NULL,
+                       level = "reg",
+                       products = "kall",
+                       index = "lasp",
+                       chain = FALSE,
+                       baseyear = "y2005",
+                       round = TRUE,
+                       type = "consumer",
+                       product_aggr = TRUE) { # nolint:object_name_linter
+  if (chain == FALSE) {
+    if (index == "lasp" || index == "paas") {
+      if (type == "producer") {
+        qT <- readGDX(gdx,
+                      "ov_supply",
+                      select = list(type = "level"),
+                      react = "warning")
+        pT <- prices(gdx, type = "producer")
+      } else if (type == "consumer") {
+        qT <- dimSums(demand(gdx)[, , c("food", "other_util", "bioenergy")], dim =
+                        3.1)
+        pT <- prices(gdx, type = "consumer", products = products)
+      } else {
+        stop("invalid type")
+      }
+      if (!all(products %in% readGDX(gdx, "kall")))
+        products <- readGDX(gdx, products)
+      if (suppressWarnings(is.null(readGDX(gdx, "fcostsALL")))) {
+        if ("wood" %in% products)
+          products <- products[-which(products == "wood")]
+        if ("woodfuel" %in% products)
+          products <- products[-which(products == "woodfuel")]
+        if ("constr_wood" %in% products)
+          products <- products[-which(products == "constr_wood")]
       }
 
-      p_t <- p_t[, , products]
-      q_t <- q_t[, , products]
+      pT <- pT[, , products]
+      qT <- qT[, , products]
       # check if the baseyear is contained in the gdx
-      if(!baseyear %in% getYears(q_t)) {
-        miss_year <- baseyear
-        q_t <- time_interpolate(q_t, baseyear, integrate_interpolated_years=TRUE)
-        p_t <- time_interpolate(p_t, baseyear, integrate_interpolated_years=TRUE)
+      if (!baseyear %in% getYears(qT)) {
+        missYear <- baseyear
+        qT <- time_interpolate(qT, baseyear, integrate_interpolated_years =
+                                 TRUE)
+        pT <- time_interpolate(pT, baseyear, integrate_interpolated_years =
+                                 TRUE)
       }
-      q_0 <- q_t
-      p_0 <- p_t
-      for (year in getYears(q_0)) {
-        q_0[, year, ] <- setYears(q_t[, baseyear,],year)
-        p_0[, year, ] <- setYears(p_t[, baseyear,],year)
+      q0 <- qT
+      p0 <- pT
+      for (year in getYears(q0)) {
+        q0[, year, ] <- setYears(qT[, baseyear, ], year)
+        p0[, year, ] <- setYears(pT[, baseyear, ], year)
       }
 
       if (index == "lasp") {
         if (product_aggr) {
-          dividend <- dimSums(p_t * q_0,dim=3,na.rm=TRUE)
-          divisor <- dimSums(p_0 * q_0,dim=3,na.rm=TRUE)
+          dividend <- dimSums(pT * q0, dim = 3, na.rm = TRUE)
+          divisor <- dimSums(p0 * q0, dim = 3, na.rm = TRUE)
         } else {
-          dividend <- (p_t * q_0)
-          divisor <- (p_0 * q_0)
+          dividend <- (pT * q0)
+          divisor <- (p0 * q0)
         }
       } else if (index == "paas") {
         if (product_aggr) {
-          dividend <- dimSums(p_t * q_t,dim=3,na.rm=TRUE)
-          divisor  <- dimSums(p_0 * q_t,dim=3,na.rm=TRUE)
+          dividend <- dimSums(pT * qT, dim = 3, na.rm = TRUE)
+          divisor  <- dimSums(p0 * qT, dim = 3, na.rm = TRUE)
         } else {
-          dividend <- (p_t * q_t)
-          divisor  <- (p_0 * q_t)
+          dividend <- (pT * qT)
+          divisor  <- (p0 * qT)
         }
       }
       #aggregate
-      dividend <- gdxAggregate(gdx = gdx, x=dividend, absolute = TRUE, to=level)
-      divisor <- gdxAggregate(gdx = gdx, x=divisor, absolute = TRUE, to=level)
-      px <- dividend/divisor
+      dividend <- gdxAggregate(
+        gdx = gdx,
+        x = dividend,
+        absolute = TRUE,
+        to = level
+      )
+      divisor <- gdxAggregate(
+        gdx = gdx,
+        x = divisor,
+        absolute = TRUE,
+        to = level
+      )
+      px <- dividend / divisor
 
     } else if (index == "fish") {
-      lasp <- priceIndex(gdx, level = level, products = products,
-                         chain=chain, baseyear=baseyear,
-                         index = "lasp",round=round)/100
-      paas <- priceIndex(gdx, level = level, products = products,
-                         chain=chain, baseyear=baseyear,
-                         index = "paas",round=round)/100
+      lasp <- priceIndex(
+        gdx,
+        level = level,
+        products = products,
+        chain = chain,
+        baseyear = baseyear,
+        index = "lasp",
+        round = round
+      ) / 100
+      paas <- priceIndex(
+        gdx,
+        level = level,
+        products = products,
+        chain = chain,
+        baseyear = baseyear,
+        index = "paas",
+        round = round
+      ) / 100
       px <- sqrt(lasp * paas)
-    } else stop(paste("index ", "\"", index, "\"", " does not exist. Please specify a correct index!", sep = ""))
-  } else if (chain==TRUE) {
-    px <- prices(gdx,level=level,product_aggr = TRUE); px[,,] <- NA; px[,1,] <- 1;
-    for (y in 2:dim(px)[2]) {
-      px[,y,] <- priceIndex(gdx, level = level, products = products,
-                            chain=FALSE, baseyear=y-1, index = index,round=FALSE,product_aggr = product_aggr)[,y,]/100
-      px[,y,] <- px[,y,]*setYears(px[,y-1,],NULL)
+    } else {
+      stop(
+        paste(
+          "index ",
+          "\"",
+          index,
+          "\"",
+          " does not exist. Please specify a correct index!",
+          sep = ""
+        )
+      )
     }
-    tmp<-px
-    tmp[,,]<-setYears(px[,baseyear,],NULL)
-    px<-px/tmp
+  } else if (chain == TRUE) {
+    px <- prices(gdx, level = level, product_aggr = TRUE)
+    px[, , ] <- NA
+    px[, 1, ] <- 1
+
+    for (y in 2:dim(px)[2]) {
+      px[, y, ] <- priceIndex(
+        gdx,
+        level = level,
+        products = products,
+        chain = FALSE,
+        baseyear = y - 1,
+        index = index,
+        round = FALSE,
+        product_aggr = product_aggr
+      )[, y, ] / 100
+      px[, y, ] <- px[, y, ] * setYears(px[, y - 1, ], NULL)
+    }
+    tmp <- px
+    tmp[, , ] <- setYears(px[, baseyear, ], NULL)
+    px <- px / tmp
   }
   px[is.infinite(px)] <- NA
-  px<-px*100
-  if (round) {px <- round(px)}
-  if(exists("miss_year")) px <- px[,getYears(px)!=miss_year,]
-  out(px,file)
+  px <- px * 100
+  if (round) {
+    px <- round(px)
+  }
+  if (exists("missYear"))
+    px <- px[, getYears(px) != missYear, ]
+  out(px, file)
 }
--- R/yields.R
+++ R/yields.R
@@ -5,10 +5,13 @@
 #'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
-#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
-#' @param products Selection of products (either by naming products, e.g. "tece", or naming a set,e.g."kcr"), also including "pasture"
+#' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global)
+#' or any other aggregation level defined in superAggregate
+#' @param products Selection of products (either by naming products, e.g. "tece", or naming a set,e.g."kcr"),
+#' also including "pasture"
 #' @param product_aggr aggregate over products or not (boolean)
-#' @param attributes dry matter: Mt/ha ("dm"), gross energy: PJ/ha ("ge"), reactive nitrogen: Mt/ha ("nr"), phosphor: Mt/ha ("p"), potash: Mt/ha ("k"), wet matter: Mt/ha ("wm"). Can also be a vector.
+#' @param attributes dry matter: Mt/ha ("dm"), gross energy: PJ/ha ("ge"), reactive nitrogen: Mt/ha ("nr"),
+#' phosphor: Mt/ha ("p"), potash: Mt/ha ("k"), wet matter: Mt/ha ("wm"). Can also be a vector.
 #' @param water_aggr aggregate irrigated and non-irriagted production or not (boolean).
 #' @return crop yield as MAgPIE object (unit depends on attributes)
 #' @author Florian Humpenoeder
@@ -20,44 +23,75 @@
 #'   }
 #'
 
-yields <- function(gdx,file=NULL,level="reg",products="kcr",product_aggr=FALSE,attributes="dm",water_aggr=TRUE) {
-
-  if(level=="cell"){
-
-    if (!all(products%in%findset("kcr"))){
-      products<-readGDX(gdx,products)
+yields <- function(gdx,
+                   file = NULL,
+                   level = "reg",
+                   products = "kcr",
+                   product_aggr = FALSE, # nolint:object_name_linter
+                   attributes = "dm",
+                   water_aggr = TRUE) { # nolint:object_name_linter
+  if (level == "cell") {
+    if (!all(products %in% findset("kcr"))) {
+      products <- readGDX(gdx, products)
     }
-    x=readGDX(gdx,"ov_yld")[,,"level"]
-    x= x[,,products]
-    if(water_aggr) {
-      weight=croparea(gdx=gdx,level=level,products=products,product_aggr=FALSE,water_aggr=FALSE)
-      weight[,,"rainfed"]=weight[,,"rainfed"]+10^-10
-      x=dimSums(weight*x,dim="w")/dimSums(weight,dim="w")
+    x <- readGDX(gdx, "ov_yld")[, , "level"]
+    x <- x[, , products]
+    if (water_aggr) {
+      weight <- croparea(
+        gdx = gdx,
+        level = level,
+        products = products,
+        product_aggr = FALSE,
+        water_aggr = FALSE
+      )
+      weight[, , "rainfed"] <- weight[, , "rainfed"] + 10 ^ -10
+      x <- dimSums(weight * x, dim = "w") / dimSums(weight, dim = "w")
     }
-    if(product_aggr) {
-      weight=croparea(gdx=gdx,level=level,products=products,product_aggr=FALSE,water_aggr=water_aggr)
-      weight=weight+10^-10
-      x=dimSums(weight*x,dim="kcr")/dimSums(weight,dim="kcr")
+    if (product_aggr) {
+      weight <- croparea(
+        gdx = gdx,
+        level = level,
+        products = products,
+        product_aggr = FALSE,
+        water_aggr = water_aggr
+      )
+      weight <- weight + 10 ^ -10
+      x <- dimSums(weight * x, dim = "kcr") / dimSums(weight, dim = "kcr")
     }
 
   } else {
-    prod<-production(gdx,level=level,products=products,product_aggr=product_aggr,attributes=attributes,water_aggr=water_aggr)
-    if(is.null(prod)) {
-      warning("Yields cannot be calculated as production function returned NULL! NULL is returned!")
+    prod <- production(
+      gdx,
+      level = level,
+      products = products,
+      product_aggr = product_aggr,
+      attributes = attributes,
+      water_aggr = water_aggr
+    )
+    if (is.null(prod)) {
+      warning(
+        "Yields cannot be calculated as production function returned NULL! NULL is returned!"
+      )
       return(NULL)
     }
-    if(products=="pasture"){
-      area <- setNames(land(gdx, level=level, types="past"), "pasture")
+    if (products == "pasture") {
+      area <- setNames(land(gdx, level = level, types = "past"), "pasture")
     } else {
-      area <- croparea(gdx,level=level,products=products,product_aggr=product_aggr,water_aggr=water_aggr)
+      area <- croparea(
+        gdx,
+        level = level,
+        products = products,
+        product_aggr = product_aggr,
+        water_aggr = water_aggr
+      )
     }
-    if(is.null(area)) {
+    if (is.null(area)) {
       warning("Yields cannot be calculated as croparea function returned NULL! NULL is returned!")
       return(NULL)
     }
-    x <- prod/area
+    x <- prod / area
     x[is.nan(x) | is.infinite(x)] <- NA
   }
 
-  out(x,file)
+  out(x, file)
 }
