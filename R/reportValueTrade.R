3359d32c9f192d06ffd816a3e960b30d58683a97
florianh <humpenoeder@pik-potsdam.de>
2025-03-05 12:46:58 +0100

    bugfix unit

--- R/reportValueTrade.R
+++ R/reportValueTrade.R
@@ -1,28 +1,28 @@
 #' @title reportValueTrade
 #' @description reports trade value
-#' 
-#' @importFrom magpiesets reporthelper 
+#'
+#' @importFrom magpiesets reporthelper
 #' @export
-#' 
+#'
 #' @param gdx GDX file
 #' @param detail if true, provides estimates for all commodities, otherwhise aggregates some groups
 #' @return trade value as MAgPIE object Unit: see names
 #' @author Florian Humpenoeder
 #' @examples
-#' 
+#'
 #'   \dontrun{
 #'     x <- reportValueTrade(gdx)
 #'   }
-#' 
+#'
 
 reportValueTrade<-function(gdx,detail=FALSE){
-  
+
   x <- NULL
-  
+
   # net-exports
   out<-tradeValue(gdx, level="regglo", type="net-exports", glo_weight="free_trade")
   out<-reporthelper(x=out,dim = 3.1,level_zero_name = "Trade Value|Net-Exports",detail = FALSE)
-  getNames(out) <- paste(getNames(out),"(million US$2017yr)",sep=" ")
+  getNames(out) <- paste(getNames(out),"(million US$2017/yr)",sep=" ")
   x <- mbind(x,out)
 
   # gross-exports
@@ -30,7 +30,7 @@ reportValueTrade<-function(gdx,detail=FALSE){
   out<-reporthelper(x=out,dim = 3.1,level_zero_name = "Trade Value|Exports",detail = detail)
   getNames(out) <- paste(getNames(out),"(million US$2017/yr)",sep=" ")
   x <- mbind(x,out)
-  
+
   # gross-imports
   out<-suppressMessages(tradeValue(gdx, level="regglo", type="imports", glo_weight="free_trade"))
   out<-reporthelper(x=out,dim = 3.1,level_zero_name = "Trade Value|Imports",detail = detail)
