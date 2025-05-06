b6963e86255432c47f6730089b14fb6e2f3fe2b6
florianh <humpenoeder@pik-potsdam.de>
2025-05-06 14:21:51 +0200

    bugfix reportLanduse

--- R/reportLandUse.R
+++ R/reportLandUse.R
@@ -22,7 +22,9 @@ reportLandUse <- function(gdx) {
   a <- a[,,"other",invert=TRUE]
   b <- OtherLand(gdx)
   a <- mbind(b,a)
-  secdforest <- try(madrat::toolAggregate(readGDX(gdx, "ov35_secdforest", select = list(type="level")),readGDX(gdx, "ac_to_bii_class_secd"),from = "ac", to = "bii_class_secd", dim = 3.1), silent = TRUE)
+  secdforest <- dimSums(try(madrat::toolAggregate(readGDX(gdx, "ov35_secdforest", select = list(type="level")),
+                                                  readGDX(gdx, "ac_to_bii_class_secd"),from = "ac", to = "bii_class_secd", dim = 3.1), silent = TRUE), dim=1.2)
+  secdforest <- mbind(secdforest, setItems(dimSums(secdforest,dim=1), dim = 1, "GLO"))
 
   #add global
   a <- mbind(a, setItems(dimSums(a,dim=1), dim = 1, "GLO"))
