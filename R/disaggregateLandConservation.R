a083131dca635b13b47ccae23ebd0226751372dc
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-05-15 11:19:12 +0200

    args, formatting

--- R/disaggregateLandConservation.R
+++ R/disaggregateLandConservation.R
@@ -1,13 +1,10 @@
-disaggregateLandConservation <- function(gdx, cfg, map_file, wdpa_hr_file, consv_prio_hr_file) {
+disaggregateLandConservation <- function(gdx, cfg, mapping, wdpaHr, conservationPrioHr) {
   landConsvLr <- gdx2::readGDX(gdx, "pm_land_conservation", react = "silent")
   landConsvLr <- dimSums(landConsvLr, dim = 3.2)
-  wdpaHr <- read.magpie(wdpa_hr_file)
-  map <- readRDS(map_file)
 
   # create full time series
-  landConsvHr <- new.magpie(map[, "cell"], getYears(landConsvLr), getItems(wdpaHr, dim = 3.2),
-    fill = 0, sets = c("x.y.iso", "year", "data")
-  )
+  landConsvHr <- new.magpie(mapping[, "cell"], getYears(landConsvLr), getItems(wdpaHr, dim = 3.2),
+                            fill = 0, sets = c("x.y.iso", "year", "data"))
 
   consvIso <- gdx2::readGDX(gdx, "policy_countries22")
   consvIso <- consvIso[consvIso %in% getItems(wdpaHr, dim = 1.3)]
@@ -35,13 +32,11 @@ disaggregateLandConservation <- function(gdx, cfg, map_file, wdpa_hr_file, consv
   consvNoselect <- cfg$gms$c22_protect_scenario_noselect
 
   if (!all(c(consvSelect, consvNoselect) %in% "none")) {
-    if (file.exists(consv_prio_hr_file)) {
-      consvPrioAll <- read.magpie(consv_prio_hr_file)
-      consvPrioHr <- new.magpie(
-        cells_and_regions = map[, "cell"],
-        names = getNames(consvPrioAll, dim = 2), fill = 0,
-        sets = c("x.y.iso", "year", "data")
-      )
+    if (!is.null(conservationPrioHr)) {
+      consvPrioAll <- conservationPrioHr
+      consvPrioHr <- new.magpie(cells_and_regions = mapping[, "cell"],
+                                names = getNames(consvPrioAll, dim = 2), fill = 0,
+                                sets = c("x.y.iso", "year", "data"))
 
       if (consvNoselect != "none") {
         consvPrioHr <- collapseDim(consvPrioAll[, , consvNoselect], dim = 3.1)
@@ -58,11 +53,6 @@ disaggregateLandConservation <- function(gdx, cfg, map_file, wdpa_hr_file, consv
 
       # add conservation priority areas
       landConsvHr <- (landConsvHr + consvPrioHr)
-    } else {
-      warning(paste(
-        "Future land conservation used in MAgPIE run but high resolution",
-        "conservation priority data for disaggregation not found."
-      ))
     }
   }
   # Due to internal constraints and compensation (e.g. NDC forest conservation)
@@ -71,20 +61,16 @@ disaggregateLandConservation <- function(gdx, cfg, map_file, wdpa_hr_file, consv
   # land restoration is switched off). Therefore a scaling is applied here separately
   # for grassland and natural vegetation
   natveg <- c("primforest", "secdforest", "other")
-  consvSumLr <- mbind(
-    landConsvLr[, , "past"],
-    setNames(dimSums(landConsvLr[, , natveg], dim = 3), "natveg")
-  )
+  consvSumLr <- mbind(landConsvLr[, , "past"],
+                      setNames(dimSums(landConsvLr[, , natveg], dim = 3), "natveg"))
   consvSumHrAgg <- mbind(
-    toolAggregate(landConsvHr[, , "past"], map, from = "cell", to = "cluster"),
+    toolAggregate(landConsvHr[, , "past"], mapping, from = "cell", to = "cluster"),
     toolAggregate(setNames(dimSums(landConsvHr[, , natveg], dim = 3), "natveg"),
-      map,
-      from = "cell", to = "cluster"
-    )
+                  mapping, from = "cell", to = "cluster")
   )
   consvScaling <- consvSumLr / consvSumHrAgg
   consvScaling[is.na(consvScaling) | is.infinite(consvScaling)] <- 1
-  consvScaling <- toolAggregate(consvScaling, map, from = "cluster", to = "cell")
+  consvScaling <- toolAggregate(consvScaling, mapping, from = "cluster", to = "cell")
   landConsvHr[, , "past"] <- consvScaling[, , "past"] * landConsvHr[, , "past"]
   landConsvHr[, , natveg] <- consvScaling[, , "natveg"] * landConsvHr[, , natveg]
   return(landConsvHr)
