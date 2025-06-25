267c144f3ea36a363ec5abc0e197f42bec625614
emolinab <mbacca@pik-potsdam.de>
2025-06-25 16:39:52 +0200

    first update of cost functions

--- R/costs.R
+++ R/costs.R
@@ -1,8 +1,6 @@
 #' @title costs
 #' @description reads costs entering the objective function from a MAgPIE gdx file
-#'
 #' @export
-#'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or
@@ -32,19 +30,23 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
     return(cost)
   }
 
+  # The fAn factor is used to change the costs calculation based on either "annuity" (as it enters the ) costs function
+  # where it is equal to 1, or as "investments" which turns into the inverse of the annuatization function used in GAMS
   fAn <- 1
 
   if (type == "investment") {
 
     intRate <- readGDX(gdx, "pm_interest")[, readGDX(gdx, "t"), ]
     t <- getYears(intRate, as.integer = TRUE)
-    tStep <- t - c(1990, t[seq_len(length(t))[1:(length(t) - 1)]])
-    tSm <- intRate
+    tStep <- t - c(1990, t[seq_len(length(t))[1:(length(t) - 1)]]) # calculates the time step length
+    tSm <- intRate # creates a magpie object with the same format as intRate to store time step length per region
 
+    # Replaces each year with the time step length
     for (y in seq_len(length(getYears(tSm)))) {
       tSm[, y, ] <- tStep[y]
     }
 
+    # calculates the conversion factor from "annuity" to "investment"
     fAn <- (1 + intRate) / (intRate) / tSm
 
   }
@@ -74,27 +76,29 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
     tmpCost(gdx, "ov_water_cost",   "Irrigation water"),
     tmpCost(gdx, "ov_cost_packaging",   "Wholesale Costs"),
     tmpCost(gdx, "ov_cost_scm",   "Costs for soil carbon management on cropland"),
-    tmpCost(gdx, "ov_tech_cost", "TC")* fAn
+    tmpCost(gdx, "ov_tech_cost", "TC") * fAn
   )
 
   # Input factors
-  if (suppressWarnings(!is.null(readGDX(gdx, "ov_cost_prod")))) {
-    if (suppressWarnings(is.null(readGDX(gdx, "p38_capital_mobile")))) {
+  if (suppressWarnings(!is.null(readGDX(gdx, "ov_cost_prod")))) { # backwards compatibility: no separation per product
+    if (suppressWarnings(is.null(readGDX(gdx, "p38_capital_mobile")))) { # if it is not the sticky realization
       inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors")
 
-    } else {
+    } else { # if it is sticky
       if (type == "annuity") {
         inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors") +
-          tmpCost(gdx, "ov_cost_inv", "Input Factors")
-      } else if (type == "investment") {
-        inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors") +
+          tmpCost(gdx, "ov_cost_inv", "Input Factors") # Including sticky investments with costs as in the optimization
+      } else  {
+        inputCosts <- tmpCost(gdx, "ov_cost_prod", "Input Factors") + # no investment costs
+          # investments (as calculated by MAgPIE) divided by time step length to have a yearly average investment
+          # done this way because the conversion factor between investments and annuity is different due to depreciation
           (tmpCost(gdx, "ov38_investment_immobile", "Input Factors") +
              tmpCost(gdx, "ov38_investment_mobile", "Input Factors")) / tSm
       }
 
     }
-  } else {
-    if (suppressWarnings(is.null(readGDX(gdx, "p38_capital_mobile")))) {
+  } else { # current version where input costs for kres, past, livst, and fish have same format as inputs for crops
+    if (suppressWarnings(is.null(readGDX(gdx, "p38_capital_mobile")))) { # if it is not the sticky realization
       inputCosts <- tmpCost(gdx, "ov_cost_prod_crop", "Input Factors") +
         tmpCost(gdx, "ov_cost_prod_kres", "Input Factors") +
         tmpCost(gdx, "ov_cost_prod_past", "Input Factors") +
@@ -102,20 +106,22 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
         tmpCost(gdx, "ov_cost_prod_fish", "Input Factors")
 
     } else {
-      if (type == "annuity") {
+      if (type == "annuity") { # Including sticky investments with costs as in the optimization
         inputCosts <- tmpCost(gdx, "ov_cost_prod_crop", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_kres", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_past", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_livst", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_fish", "Input Factors")
 
-      } else if (type == "investment") {
+      } else { # Including sticky investments with costs as "investments"
         inputCosts <- tmpCost(gdx, "ov_cost_prod_kres", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_past", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_livst", "Input Factors") +
           tmpCost(gdx, "ov_cost_prod_fish", "Input Factors") +
           setNames(readGDX(gdx, "ov_cost_prod_crop", format = "first_found",
-                           select = list(type = "level"), react = "quiet")[, , "labor"], "Input Factors") +
+                           select = list(type = "level"), react = "quiet")[, , "labor"], "Input Factors") + # labor
+          # investments (as calculated by MAgPIE) divided by time step length to have a yearly average investment
+          # done this way because the conversion factor between investments and annuity is different due to depreciation
           (tmpCost(gdx, "ov38_investment_immobile", "Input Factors") +
              tmpCost(gdx, "ov38_investment_mobile", "Input Factors")) / tSm
 
@@ -136,42 +142,48 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
   }
 
   # Forestry
-  if (suppressWarnings(is.null(readGDX(gdx, "ov32_cost_establishment")))) {
+  if (suppressWarnings(is.null(readGDX(gdx, "ov32_cost_establishment")))) { # backwards compatibility
     forestry <- tmpCost(gdx, "ov_cost_fore", "Forestry")
 
-  } else {
+  } else { # current dynamic realization
     forestry <- tmpCost(gdx, "ov_cost_fore", "Forestry") - tmpCost(gdx, "ov32_cost_establishment", "Forestry") +
-      tmpCost(gdx, "ov32_cost_establishment", "Forestry") * fAn
+      tmpCost(gdx, "ov32_cost_establishment", "Forestry") * fAn # to turn into "investment" or "annuity"
   }
 
   # CroplandTree
-  if (suppressWarnings(is.null(readGDX(gdx, "ov29_cost_treecover_est")))) {
+  if (suppressWarnings(is.null(readGDX(gdx, "ov29_cost_treecover_est")))) { # backwards compatibility
     croplandTree <- tmpCost(gdx, "ov_cost_cropland", "CroplandTree")
 
   } else {
-    croplandTree <- tmpCost(gdx, "ov_cost_cropland", "CroplandTree") - tmpCost(gdx, "ov29_cost_treecover_est", "CroplandTree") +
-      tmpCost(gdx, "ov29_cost_treecover_est", "CroplandTree") * fAn
+    croplandTree <- tmpCost(gdx, "ov_cost_cropland", "CroplandTree") -
+      tmpCost(gdx, "ov29_cost_treecover_est", "CroplandTree") +
+      tmpCost(gdx, "ov29_cost_treecover_est", "CroplandTree") * fAn # from "investment" or "annuity"
   }
 
   # GHG emissions
 
+  # separates investments from operational costs
   emisCostOneoff <- readGDX(gdx, "ov56_emission_cost", select = list(type = "level"), react = "silent")
   if (!is.null(emisCostOneoff)) {
     emisOneoff <- readGDX(gdx, "emis_oneoff")
     emisCostOneoff <- emisCostOneoff[, , emisOneoff]
     emisCostOneoff <- dimSums(emisCostOneoff, dim = 3)
   } else {
-    costsCellOneoff <- dimSums(superAggregate(collapseNames(
-      readGDX(gdx, "ov56_emission_costs_cell_oneoff")[, , "level"]), aggr_type = "sum", level = "reg"), dim = 3)
+    costsCellOneoff <- dimSums(superAggregate(
+                                              collapseNames(
+                                                            readGDX(gdx, "ov56_emission_costs_cell_oneoff")[, ,
+                                                                                                            "level"]),
+                                              aggr_type = "sum", level = "reg"), dim = 3)
     costsRegOneoff <- if (is.null(getNames(
-      readGDX(gdx, "ov56_emission_costs_reg_oneoff")))) 0 else
-        dimSums(readGDX(gdx, "ov56_emission_costs_reg_oneoff")[, , "level"], dim = 3)
+                                           readGDX(gdx, "ov56_emission_costs_reg_oneoff")))) 0 else
+      dimSums(readGDX(gdx, "ov56_emission_costs_reg_oneoff")[, , "level"], dim = 3)
     emisCostOneoff <- costsCellOneoff + costsRegOneoff
   }
 
-
+  # from "investment" or "annuity"
   emissions <- tmpCost(gdx, "ov_emission_costs", "GHG Emissions") - emisCostOneoff + emisCostOneoff * fAn
 
+  # adds the special cases to the overall list of costs
   x[[length(x) + 1]] <- inputCosts
   x[[length(x) + 1]] <- peatland
   x[[length(x) + 1]] <- forestry
@@ -185,7 +197,7 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
   }
 
   if (type == "annuity") {
-    # check
+    # check to make sure annuity costs (optimization) from MAgPIE are the same as the ones included hear
     if (any(abs(readGDX(gdx, "ov11_cost_reg", select = list(type = "level")) - dimSums(x, dim = 3)) > 1e-6)) {
       warning("Differences between total cost and cost categories detected.
             A newly added cost item in the MAgPIE cost module might be missing
@@ -196,8 +208,8 @@ costs <- function(gdx, file = NULL, level = "reg", type = "annuity", sum = TRUE)
 
   }
 
-  # aggregate
-  if (level == "regglo" || level == "glo"|| level == "reg") {
+  # aggregate to regional aggregation level
+  if (level == "regglo" || level == "glo" || level == "reg") {
     x <- gdxAggregate(gdx, x, to = level, absolute = TRUE)
   } else {
     stop("Level not supported")
