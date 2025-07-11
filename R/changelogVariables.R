9d433fd523c53547dbc97af8d0e6213322cfa54b
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-07-11 13:37:13 +0200

    remove units again

--- R/changelogVariables.R
+++ R/changelogVariables.R
@@ -5,14 +5,14 @@
 #' @author Pascal Sauer
 #' @export
 changelogVariables <- function() {
-  return(c(lucEmisRaw = "Emissions|CO2|Land RAW|+|Land-use Change (Mt CO2/yr)",
-           tau = "Productivity|Landuse Intensity Indicator Tau (Index)",
-           cropland = "Resources|Land Cover|+|Cropland (million ha)",
-           irrigated = "Resources|Land Cover|Cropland|Area actually irrigated (million ha)",
-           pasture = "Resources|Land Cover|+|Pastures and Rangelands (million ha)",
-           forest = "Resources|Land Cover|+|Forest (million ha)",
-           other = "Resources|Land Cover|+|Other Land (million ha)",
-           production = "Production (Mt DM/yr)",
-           costs = "Costs (million US$2017/yr)",
-           foodExp = "Household Expenditure|Food|Expenditure (US$2017/capita)"))
+  return(c(lucEmisRaw = "Emissions|CO2|Land RAW|+|Land-use Change",
+           tau = "Productivity|Landuse Intensity Indicator Tau",
+           cropland = "Resources|Land Cover|+|Cropland",
+           irrigated = "Resources|Land Cover|Cropland|Area actually irrigated",
+           pasture = "Resources|Land Cover|+|Pastures and Rangelands",
+           forest = "Resources|Land Cover|+|Forest",
+           other = "Resources|Land Cover|+|Other Land",
+           production = "Production",
+           costs = "Costs",
+           foodExp = "Household Expenditure|Food|Expenditure"))
 }
