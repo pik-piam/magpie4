495572b412ad8649af75a1f454c6772276739a00
bodirsky <bodirsky@pik-potsdam.de>
2025-10-27 15:51:27 +0100

    reverting my changes to croparea.R

--- R/NitrogenBudget.R
+++ R/NitrogenBudget.R
@@ -147,7 +147,7 @@ NitrogenBudget <- memoise(function(gdx, include_emissions = FALSE, # nolint
       }
 
       snupe <- readGDX(gdx, "ov50_nr_eff", "ov_nr_eff", format = "first_found")[, , "level"]
-      fert <- toolFertilizerDistribution(iterMax = 200, maxSnupe = 0.85, threshold = threshold,
+      fert <- toolFertilizerDistribution(iterMax = 100, maxSnupe = 0.85, threshold = threshold,
                                          mapping = mapping, from = "j", to = "i", fertilizer = fertilizer,
                                          snupe = snupe, withdrawals = withdrawals, organicinputs = organicinputs,
                                          progress = progress)
@@ -168,6 +168,9 @@ NitrogenBudget <- memoise(function(gdx, include_emissions = FALSE, # nolint
         "surplus"
       )
     )
+    # round disaggregation artefacts out
+    out <- round(out,14)
+
 
     if (any(out[, , "surplus"] < 0)) {
       balanceflow <- out[, , "surplus"]
--- R/croparea.R
+++ R/croparea.R
@@ -24,11 +24,27 @@
 #' @importFrom magclass setCells
 
 croparea <- memoise(function(gdx, file = NULL, level = "reg", products = "kcr",
-                     product_aggr = TRUE, water_aggr = TRUE) {
+                             product_aggr = TRUE, water_aggr = TRUE) {
 
+  if (level %in% c("grid", "iso")) {
+    y <- read.magpie(file.path(dirname(normalizePath(gdx)), "cell.land_0.5.mz"))
+    x <- read.magpie(file.path(dirname(normalizePath(gdx)), "cell.croparea_0.5_share.mz"))
 
-  x <- readGDX(gdx, "ov_area", format = "first_found",
+    if (length(getCells(x)) == "59199") {
+      mapfile <- system.file("extdata", "mapping_grid_iso.rds", package = "magpie4")
+      map_grid_iso <- readRDS(mapfile)
+      y <- setCells(y, map_grid_iso$grid)
+      x <- setCells(x, map_grid_iso$grid)
+    }
+    y <- y[, "y1985", , invert = TRUE] # 1985 is currently the year before simulation start. has to be updated later
+    y <- dimSums(y, dim = 3)
+    x[is.na(x)] <- 0
+    x <- x * y
+    if (level == "iso") x <- gdxAggregate(gdx, x, to = "iso")
+  } else {
+    x <- readGDX(gdx, "ov_area", format = "first_found",
                  select = list(type = "level"))
+  }
 
   if (is.null(x)) {
     warning("Crop area cannot be calculated as area data could not be found in GDX file! NULL is returned!")
