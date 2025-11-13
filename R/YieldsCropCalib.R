839bed56e0e2c6db30420926fc523dd45d2ac8a3
Kristine Karstens <karstens@pik-potsdam.de>
2025-11-13 19:24:38 +0100

    update before and after calibration yield reporting to be working with gsadapt

--- R/YieldsCropCalib.R
+++ R/YieldsCropCalib.R
@@ -6,6 +6,7 @@
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
 #' @param level Level of regional aggregation
+#' @param tau if TRUE add effect of TAU to the yield
 #' @return A MAgPIE object containing values of potential yields after the calibration routines
 #' @author Edna Molina Bacca
 #' @importFrom magpiesets findset
@@ -15,29 +16,34 @@
 #' x <- YieldsCropCalib(gdx)
 #' }
 #'
-YieldsCropCalib <- function(gdx, file = NULL, level = "cell") {
+YieldsCropCalib <- function(gdx, file = NULL, level = "cell", tau = FALSE) {
 
-  kcr <- findset("kcr")
-  t <- readGDX(gdx, "t")
-
-  if (level %in% c("cell", "glo", "reg", "regglo")) {
+  kcr <- readGDX(gdx, "kcr")
+  t   <- readGDX(gdx, "t")
   out <- readGDX(gdx, "i14_yields_calib")[, t, kcr]
 
-  weight <- out
+  if(tau) {
+    gsadaptRatio <- suppressWarnings(readGDX(gdx, "p14_yields_gsadapt_ratio_cummulative"))
+    if(is.null(gsadaptRatio)) gsadaptRatio <- 1
+    tau1995 <- readGDX(gdx, "fm_tau1995")
+    vmTau   <- readGDX(gdx, "ov_tau", select = list(type="level"))[, , "crop"]
+    out     <- collapseDim(out / gsadaptRatio * vmTau / tau1995)
+  }
 
-  # The +0.000001 is added as a small area for crops with zero values in fm_croparea.
-  # Otherwise yields for begr and betr are zero.
+  if (level %in% c("cell", "glo", "reg", "regglo")) {
 
-  area <- readGDX(gdx, "fm_croparea")[, 1995, ] + 0.000001
-  weight[, , ] <- area
+    # The +0.000001 is added as a small area for crops with zero values in fm_croparea.
+    # Otherwise yields for begr and betr are zero.
+    weight <- out
+    area <- readGDX(gdx, "fm_croparea")[, 1995, ] + 0.000001
+    weight[, , ] <- area
 
-  if (level != "cell") out <- gdxAggregate(gdx, out, weight = weight, to = level, absolute = FALSE) else out
+    if (level != "cell") out <- gdxAggregate(gdx, out, weight = weight, to = level, absolute = FALSE) else out
 
   } else if (level == "grid") {
 
-    out <- readGDX(gdx, "i14_yields_calib")[, t, kcr]
-
     out <- gdxAggregate(gdx, out, weight = NULL, to = "grid", absolute = FALSE)
+
   } else {
     stop("Level not recognized")
   }
