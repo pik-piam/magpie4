8769fb82ef101e0c589c9c5f17c4fd5f175c6442
pvjeetze <vjeetze@pik-potsdam.de>
2025-11-28 15:08:16 +0100

    adjusted tau function to accomodate different aggregation levels of vm_tau

--- R/tau.R
+++ R/tau.R
@@ -21,8 +21,12 @@
 tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4, prev_year = "y1985", type = "crop") { # nolint
 
   x <- readGDX(gdx, "ov_tau", format = "first_found")[, , "level"]
+  if (getSets(x)[1] == "j") {
+    tauLevel <- "cell"
+  } else {
+    tauLevel <- "reg"
+  }
   if (dim(x)[3] > 1) {
-
     ### If only "crop" Tau is desired
     if (type == "crop") {
       x <- x[, , "crop.level"]
@@ -37,8 +41,10 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
           warning("No Information on initial value for tau found in the gdx file! NULL is returned!")
           return(NULL)
         }
-        cell <- readGDX(gdx, "cell", react = "silent")
-        tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        if (tauLevel == "cell") {
+          cell <- readGDX(gdx, "cell", react = "silent")
+          tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        }
         x <- mbind(setYears(tau1995, prev_year), x)
       }
 
@@ -48,8 +54,8 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
         x <- toolAggregate(x, supreg)
       }
 
-      if (level != "cell") {
-        cr <- croparea(gdx, level = "cell", water_aggr = TRUE)
+      if (level != tauLevel) {
+        cr <- croparea(gdx, level = tauLevel, water_aggr = TRUE)
         if (is.null(cr)) {
           warning("tau cannot be aggregated as croparea function returned NULL! NULL is returned!")
           return(NULL)
@@ -59,7 +65,6 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
         }
         x <- superAggregate(x, aggr_type = "weighted_mean", level = level, weight = cr)
       }
-
     }
 
     ### if only "pastr" Tau is desired
@@ -76,8 +81,10 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
           warning("No Information on initial value for tau found in the gdx file! NULL is returned!")
           return(NULL)
         }
-        cell <- readGDX(gdx, "cell", react = "silent")
-        tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        if (tauLevel == "cell") {
+          cell <- readGDX(gdx, "cell", react = "silent")
+          tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        }
         x <- mbind(setYears(tau1995, prev_year), x)
       }
 
@@ -87,7 +94,7 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
         x <- toolAggregate(x, supreg)
       }
 
-      if (level != "cell") {
+      if (level != tauLevel) {
         pt <- NULL
         pt <- readGDX(gdx, "ov31_grass_area", format = "first_found", react = "silent")[, , "pastr.level"]
         if (is.null(pt)) {
@@ -101,7 +108,7 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
       }
     }
 
-### For both "crop" and "pastr" Tau (running exo Tau with "grasslands_apr22" realiz of `31_past` module)
+    ### For both "crop" and "pastr" Tau (running exo Tau with "grasslands_apr22" realiz of `31_past` module)
     if (type == "both") {
       if (is.null(x)) {
         warning("No Information on tau in the gdx file! NULL is returned!")
@@ -113,8 +120,10 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
           warning("No Information on initial value for tau found in the gdx file! NULL is returned!")
           return(NULL)
         }
-        cell <- readGDX(gdx, "cell", react = "silent")
-        tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        if (tauLevel == "cell") {
+          cell <- readGDX(gdx, "cell", react = "silent")
+          tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+        }
         x <- mbind(setYears(tau1995, prev_year), x)
       }
 
@@ -124,8 +133,8 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
         x <- toolAggregate(x, supreg)
       }
 
-      if (level != "cell") {
-        cr <- croparea(gdx, level = "cell", water_aggr = TRUE)
+      if (level != tauLevel) {
+        cr <- croparea(gdx, level = tauLevel, water_aggr = TRUE)
         if (is.null(cr)) {
           warning("tau cannot be aggregated as croparea function returned NULL! NULL is returned!")
           return(NULL)
@@ -138,7 +147,7 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
       x <- collapseNames(x) # Drop `.level` from dim 3 names
     }
 
-  # account for default realization of `31_past` module with only "crop" Tau (no "pastr" Tau) where dim(x)[3] == 1
+    # account for default realization of `31_past` module with only "crop" Tau (no "pastr" Tau) where dim(x)[3] == 1
   } else {
     getNames(x) <- NULL
     if (is.null(x)) {
@@ -151,8 +160,10 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
         warning("No Information on initial value for tau found in the gdx file! NULL is returned!")
         return(NULL)
       }
-      cell <- readGDX(gdx, "cell", react = "silent")
-      tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+      if (tauLevel == "cell") {
+        cell <- readGDX(gdx, "cell", react = "silent")
+        tau1995 <- toolAggregate(tau1995, cell, from = "i", to = "j")
+      }
       x <- mbind(setYears(tau1995, prev_year), x)
     }
 
@@ -162,8 +173,8 @@ tau <- function(gdx, file = NULL, level = "reg", start_value = FALSE, digits = 4
       x <- toolAggregate(x, supreg)
     }
 
-    if (level != "cell") {
-      cr <- croparea(gdx, level = "cell", water_aggr = TRUE)
+    if (level != tauLevel) {
+      cr <- croparea(gdx, level = tauLevel, water_aggr = TRUE)
       if (is.null(cr)) {
         warning("tau cannot be aggregated as croparea function returned NULL! NULL is returned!")
         return(NULL)
