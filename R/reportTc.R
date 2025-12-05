fa7675faa90873ee33adf03ba3cc0076aec28541
Patrick Rein <patrick.rein@pik-potsdam.de>
2025-12-05 16:07:02 +0100

    Minor stylistic changes to reportTc

--- R/reportTc.R
+++ R/reportTc.R
@@ -1,28 +1,27 @@
 #' @title reportTc
 #' @description reports Tc
-#' 
+#'
 #' @export
-#' 
+#'
 #' @param gdx GDX file
 #' @return tc values as MAgPIE object (%/yr)
 #' @author Florian Humpenoeder
 #' @examples
-#' 
+#'
 #'   \dontrun{
 #'     x <- reportTc(gdx)
 #'   }
-#' 
+#'
 
 reportTc <- function(gdx) {
-  pt = NULL
+  pt <- NULL
   tau <- readGDX(gdx, "ov_tau", format = "first_found")[, , "level"]
-  if(any(grepl("pastr",getItems(tau, dim = 3)))) {
-    pt = tc(gdx = gdx,level="regglo", type = "pastr")
+  if (any(grepl("pastr", getItems(tau, dim = 3)))) {
+    pt = tc(gdx = gdx, level = "regglo", type = "pastr")
     getNames(pt) <- "Productivity|Yield-increasing technological change managed pastures (%/yr)"
   }
-  cr = tc(gdx = gdx,level="regglo", type = "crop")
+  cr <- tc(gdx = gdx, level = "regglo", type = "crop")
   getNames(cr) <- "Productivity|Yield-increasing technological change crops (%/yr)"
-  out <- mbind(pt,cr)
+  out <- mbind(pt, cr)
   return(out)
 }
-
