992ffbf4ff48cc5ddcf737b841c26491516b0240
Patrick Rein <patrick.rein@pik-potsdam.de>
2025-12-19 14:37:29 +0100

    Re-enables parallel tests to make individual tests doable

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,24 +3,34 @@ gdxUrl <- "https://rse.pik-potsdam.de/data/example/magpie-fulldata/magpie-defaul
 fixturesDir <- "tmp_fixtures"
 gdxPath <- file.path(fixturesDir, "magpie-default-fulldata.gdx")
 
-# Create fixtures directory if it doesn't exist
-if (!dir.exists(fixturesDir)) {
-  dir.create(fixturesDir, recursive = TRUE)
-}
+# Only one of the setup scripts should do this.
+setupLock <- filelock::lock("setup.lock")
 
-# Download GDX file if it doesn't exist or is older than 36 hours
-shouldDownload <- FALSE
-if (!file.exists(gdxPath)) {
-  shouldDownload <- TRUE
-} else {
-  fileInfo <- file.info(gdxPath)
-  fileAge <- difftime(Sys.time(), fileInfo$mtime, units = "hours")
-  if (fileAge > 36) {
-    shouldDownload <- TRUE
+tryCatch(
+  {
+    # Create fixtures directory if it doesn't exist
+    if (!dir.exists(fixturesDir)) {
+      dir.create(fixturesDir, recursive = TRUE)
+    }
+
+    # Download GDX file if it doesn't exist or is older than 36 hours
+    shouldDownload <- FALSE
+    if (!file.exists(gdxPath)) {
+      shouldDownload <- TRUE
+    } else {
+      fileInfo <- file.info(gdxPath)
+      fileAge <- difftime(Sys.time(), fileInfo$mtime, units = "hours")
+      if (fileAge > 36) {
+        shouldDownload <- TRUE
+      }
+    }
+
+    if (shouldDownload) {
+      withr::local_options(timeout = 10 * 60) # 10 Minutes timeout
+      download.file(gdxUrl, gdxPath, mode = "wb", quiet = TRUE)
+    }
+  },
+  finally = {
+    filelock::unlock(setupLock)
   }
-}
-
-if (shouldDownload) {
-  withr::local_options(timeout = 10 * 60) # 10 Minutes timeout
-  download.file(gdxUrl, gdxPath, mode = "wb", quiet = TRUE)
-}
+)
