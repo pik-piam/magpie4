78e151a0a2107c86a671bf7edd98526c5b7673dc
Patrick Rein <patrick.rein@pik-potsdam.de>
2025-12-19 14:54:52 +0100

    separates running report without warnings and checking content of report

--- tests/testthat/test-reports-3.R
+++ tests/testthat/test-reports-3.R
@@ -1,129 +1,129 @@
 test_that("reportPriceFoodIndex works", {
-  expectReportSucceeds(reportPriceFoodIndex, baseyear = 'y2010')
-  expectReportSucceeds(reportPriceFoodIndex, baseyear = 'y2020')
+  expectValidReport(expectReportSucceeds(reportPriceFoodIndex, baseyear = 'y2010'))
+  expectValidReport(expectReportSucceeds(reportPriceFoodIndex, baseyear = 'y2020'))
 })
 
 
 test_that("reportProducerPriceIndex works", {
-  expectReportSucceeds(reportProducerPriceIndex)
+  expectValidReport(expectReportSucceeds(reportProducerPriceIndex))
 })
 
 
 test_that("reportExpenditureFoodIndex works", {
-  expectReportSucceeds(reportExpenditureFoodIndex)
+  expectValidReport(expectReportSucceeds(reportExpenditureFoodIndex))
 })
 
 
 test_that("reportPriceAgriculture works", {
-  expectReportSucceeds(reportPriceAgriculture)
+  expectValidReport(expectReportSucceeds(reportPriceAgriculture))
 })
 
 
 test_that("reportPriceBioenergy works", {
-  expectReportSucceeds(reportPriceBioenergy)
+  expectValidReport(expectReportSucceeds(reportPriceBioenergy))
 })
 
 
 test_that("reportPriceLand works", {
-  expectReportSucceeds(reportPriceLand)
+  expectValidReport(expectReportSucceeds(reportPriceLand))
 })
 
 
 test_that("reportPriceWater works", {
-  expectReportSucceeds(reportPriceWater)
+  expectValidReport(expectReportSucceeds(reportPriceWater))
 })
 
 
 test_that("reportValueTrade works", {
-  expectReportSucceeds(reportValueTrade)
+  expectValidReport(expectReportSucceeds(reportValueTrade))
 })
 
 
 test_that("reportProcessing works", {
-  expectReportSucceeds(reportProcessing, indicator = 'primary_to_process')
-  expectReportSucceeds(reportProcessing, indicator = 'secondary_from_primary')
+  expectValidReport(expectReportSucceeds(reportProcessing, indicator = 'primary_to_process'))
+  expectValidReport(expectReportSucceeds(reportProcessing, indicator = 'secondary_from_primary'))
 
 })
 
 
 test_that("reportAEI works", {
-  expectReportSucceeds(reportAEI)
+  expectValidReport(expectReportSucceeds(reportAEI))
 })
 
 
 test_that("reportWaterUsage works", {
-  expectReportSucceeds(reportWaterUsage, detail = FALSE)
-  expectReportSucceeds(reportWaterUsage, detail = TRUE)
+  expectValidReport(expectReportSucceeds(reportWaterUsage, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportWaterUsage, detail = TRUE))
 })
 
 
 test_that("reportWaterAvailability works", {
-  expectReportSucceeds(reportWaterAvailability)
+  expectValidReport(expectReportSucceeds(reportWaterAvailability))
 })
 
 
 test_that("reportAAI works", {
-  expectReportSucceeds(reportAAI)
+  expectValidReport(expectReportSucceeds(reportAAI))
 })
 
 
 test_that("reportSOM works", {
-  expectReportSucceeds(reportSOM)
+  expectValidReport(expectReportSucceeds(reportSOM))
 })
 
 
 test_that("reportGrowingStock works", {
-  expectReportSucceeds(reportGrowingStock, indicator = 'relative')
-  expectReportSucceeds(reportGrowingStock, indicator = 'absolute')
+  expectValidReport(expectReportSucceeds(reportGrowingStock, indicator = 'relative'))
+  expectValidReport(expectReportSucceeds(reportGrowingStock, indicator = 'absolute'))
 })
 
 
 
 test_that("reportSDG1 works", {
-  expectReportSucceeds(reportSDG1)
+  expectValidReport(expectReportSucceeds(reportSDG1))
 })
 
 
 test_that("reportSDG2 works", {
-  expectReportSucceeds(reportSDG2)
+  expectValidReport(expectReportSucceeds(reportSDG2))
 })
 
 
 test_that("reportSDG3 works", {
-  expectReportSucceeds(reportSDG3)
+  expectValidReport(expectReportSucceeds(reportSDG3))
 })
 
 
 test_that("reportSDG6 works", {
-  expectReportSucceeds(reportSDG6)
+  expectValidReport(expectReportSucceeds(reportSDG6))
 })
 
 
 test_that("reportSDG12 works", {
-  expectReportSucceeds(reportSDG12)
+  expectValidReport(expectReportSucceeds(reportSDG12))
 })
 
 
 test_that("reportSDG15 works", {
-  expectReportSucceeds(reportSDG15)
+  expectValidReport(expectReportSucceeds(reportSDG15))
 })
 
 
 test_that("reportPBwater works", {
-  expectReportSucceeds(reportPBwater, level = 'regglo')
+  expectValidReport(expectReportSucceeds(reportPBwater, level = 'regglo'))
 })
 
 
 test_that("reportPBland works", {
-  expectReportSucceeds(reportPBland, level = 'regglo')
+  expectValidReport(expectReportSucceeds(reportPBland, level = 'regglo'))
 })
 
 
 test_that("reportPBbiosphere works", {
-  expectReportSucceeds(reportPBbiosphere, level = 'regglo')
+  expectValidReport(expectReportSucceeds(reportPBbiosphere, level = 'regglo'))
 })
 
 
 test_that("reportPBnitrogen works", {
-  expectReportSucceeds(reportPBnitrogen, level = 'regglo')
+  expectValidReport(expectReportSucceeds(reportPBnitrogen, level = 'regglo'))
 })
