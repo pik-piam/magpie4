0613fc3b15ec6178a818a3816999c082124e9f92
Patrick Rein <patrick.rein@pik-potsdam.de>
2026-01-05 14:25:29 +0100

    Re-organizes individual report tests into alphabetically sorted files

---
+++ tests/testthat/test-reports-M-P.R
@@ -0,0 +1,144 @@
+test_that("reportManure works", {
+  expectValidReport(expectReportSucceeds(reportManure))
+})
+
+
+test_that("reportNetForestChange works", {
+  expectValidReport(expectReportSucceeds(reportNetForestChange))
+})
+
+
+test_that("reportNitrogenBudgetCropland works", {
+  expectValidReport(expectReportSucceeds(reportNitrogenBudgetCropland))
+})
+
+
+test_that("reportNitrogenBudgetPasture works", {
+  expectValidReport(expectReportSucceeds(reportNitrogenBudgetPasture))
+})
+
+
+test_that("reportNitrogenEfficiencies works", {
+  expectValidReport(expectReportSucceeds(reportNitrogenEfficiencies))
+})
+
+
+test_that("reportNitrogenPollution works", {
+  expectValidReport(expectReportSucceeds(reportNitrogenPollution))
+})
+
+
+test_that("reportOutputPerWorker works", {
+  expectValidReport(expectReportSucceeds(reportOutputPerWorker))
+})
+
+
+test_that("reportPBbiosphere works", {
+  expectValidReport(expectReportSucceeds(reportPBbiosphere, level = "regglo"))
+})
+
+
+test_that("reportPBland works", {
+  expectValidReport(expectReportSucceeds(reportPBland, level = "regglo"))
+})
+
+
+test_that("reportPBnitrogen works", {
+  expectValidReport(expectReportSucceeds(reportPBnitrogen, level = "regglo"))
+})
+
+
+test_that("reportPBwater works", {
+  expectValidReport(expectReportSucceeds(reportPBwater, level = "regglo"))
+})
+
+
+test_that("reportPeatland works", {
+  expectValidReport(expectReportSucceeds(reportPeatland))
+})
+
+
+test_that("reportPlantationEstablishment works", {
+  expectValidReport(expectReportSucceeds(reportPlantationEstablishment))
+})
+
+
+test_that("reportPopulation works", {
+  expectValidReport(expectReportSucceeds(reportPopulation))
+})
+
+
+test_that("reportPriceAgriculture works", {
+  expectValidReport(expectReportSucceeds(reportPriceAgriculture))
+})
+
+
+test_that("reportPriceBioenergy works", {
+  expectValidReport(expectReportSucceeds(reportPriceBioenergy))
+})
+
+
+test_that("reportPriceElasticities works", {
+  expectValidReport(expectReportSucceeds(reportPriceElasticities))
+})
+
+
+test_that("reportPriceFoodIndex works", {
+  expectValidReport(expectReportSucceeds(reportPriceFoodIndex, baseyear = "y2010"))
+  expectValidReport(expectReportSucceeds(reportPriceFoodIndex, baseyear = "y2020"))
+})
+
+
+test_that("reportPriceGHG works", {
+  expectValidReport(expectReportSucceeds(reportPriceGHG))
+})
+
+
+test_that("reportPriceLand works", {
+  expectValidReport(expectReportSucceeds(reportPriceLand))
+})
+
+
+test_that("reportPriceShock works", {
+  expectEmptyReport(expectReportSucceeds(reportPriceShock))
+})
+
+
+test_that("reportPriceWater works", {
+  expectValidReport(expectReportSucceeds(reportPriceWater))
+})
+
+
+test_that("reportPriceWoodyBiomass works", {
+  expectValidReport(expectReportSucceeds(reportPriceWoodyBiomass))
+})
+
+
+test_that("reportProcessing works", {
+  expectValidReport(expectReportSucceeds(reportProcessing, indicator = "primary_to_process"))
+  expectValidReport(expectReportSucceeds(reportProcessing, indicator = "secondary_from_primary"))
+
+})
+
+
+test_that("reportProducerPriceIndex works", {
+  expectValidReport(expectReportSucceeds(reportProducerPriceIndex))
+})
+
+
+test_that("reportProduction works", {
+  expectValidReport(expectReportSucceeds(reportProduction, level = "regglo", detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportProduction, level = "regglo", detail = TRUE))
+})
+
+
+test_that("reportProductionBioenergy works", {
+  expectValidReport(expectReportSucceeds(reportProductionBioenergy, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportProductionBioenergy, detail = TRUE))
+})
+
+
+test_that("reportProtein works", {
+  expectValidReport(expectReportSucceeds(reportProtein, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportProtein, detail = TRUE))
+})
---
+++ tests/testthat/test-reports-R-Y.R
@@ -0,0 +1,137 @@
+test_that("reportRelativeHourlyLaborCosts works", {
+  expectValidReport(expectReportSucceeds(reportRelativeHourlyLaborCosts))
+})
+
+
+test_that("reportRotationLength works", {
+  expectValidReport(expectReportSucceeds(reportRotationLength))
+})
+
+
+test_that("reportRuralDemandShares works", {
+  expectEmptyReport(expectReportSucceeds(reportRuralDemandShares, type = "tradOnly"))
+})
+
+
+test_that("reportSDG1 works", {
+  expectValidReport(expectReportSucceeds(reportSDG1))
+})
+
+
+test_that("reportSDG12 works", {
+  expectValidReport(expectReportSucceeds(reportSDG12))
+})
+
+
+test_that("reportSDG15 works", {
+  expectValidReport(expectReportSucceeds(reportSDG15))
+})
+
+
+test_that("reportSDG2 works", {
+  expectValidReport(expectReportSucceeds(reportSDG2))
+})
+
+
+test_that("reportSDG3 works", {
+  expectValidReport(expectReportSucceeds(reportSDG3))
+})
+
+
+test_that("reportSDG6 works", {
+  expectValidReport(expectReportSucceeds(reportSDG6))
+})
+
+
+test_that("reportSOM works", {
+  expectValidReport(expectReportSucceeds(reportSOM))
+})
+
+
+test_that("reportTau works", {
+  expectValidReport(expectReportSucceeds(reportTau))
+})
+
+
+test_that("reportTc works", {
+  expectValidReport(expectReportSucceeds(reportTc, level = "regglo"))
+})
+
+
+test_that("reportTimber works", {
+  expectValidReport(expectReportSucceeds(reportTimber))
+})
+
+
+test_that("reportTotalHoursWorked works", {
+  expectValidReport(expectReportSucceeds(reportTotalHoursWorked))
+})
+
+
+test_that("reportTrade works", {
+  expectValidReport(expectReportSucceeds(reportTrade, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportTrade, detail = TRUE))
+})
+
+
+test_that("reportValueMaterialDemand works", {
+  expectValidReport(expectReportSucceeds(reportValueMaterialDemand))
+})
+
+
+test_that("reportValueTrade works", {
+  expectValidReport(expectReportSucceeds(reportValueTrade))
+})
+
+
+test_that("reportVegfruitShare works", {
+  expectValidReport(expectReportSucceeds(reportVegfruitShare))
+})
+
+
+test_that("reportWageDevelopment works", {
+  expectValidReport(expectReportSucceeds(reportWageDevelopment,  baseYear = 2000))
+  expectValidReport(expectReportSucceeds(reportWageDevelopment,  baseYear = 2010))
+  expectValidReport(expectReportSucceeds(reportWageDevelopment,  baseYear = 2020))
+})
+
+
+test_that("reportWaterAvailability works", {
+  expectValidReport(expectReportSucceeds(reportWaterAvailability))
+})
+
+
+test_that("reportWaterIndicators works", {
+  expectValidReport(expectReportSucceeds(reportWaterIndicators))
+})
+
+
+test_that("reportWaterUsage works", {
+  expectValidReport(expectReportSucceeds(reportWaterUsage, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportWaterUsage, detail = TRUE))
+})
+
+
+test_that("reportWorkingAgePopulation works", {
+  expectValidReport(expectReportSucceeds(reportWorkingAgePopulation))
+})
+
+
+test_that("reportYields works", {
+  expectValidReport(expectReportSucceeds(reportYields, detail = FALSE, physical = TRUE))
+  expectValidReport(expectReportSucceeds(reportYields, detail = FALSE, physical = FALSE))
+  expectValidReport(expectReportSucceeds(reportYields, detail = TRUE, physical = TRUE))
+  expectValidReport(expectReportSucceeds(reportYields, detail = TRUE, physical = FALSE))
+})
+
+
+test_that("reportYieldsCropCalib works", {
+  expectValidReport(expectReportSucceeds(reportYieldsCropCalib, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportYieldsCropCalib, detail = TRUE))
+})
+
+
+test_that("reportYieldsCropRaw works", {
+  expectValidReport(expectReportSucceeds(reportYieldsCropRaw, detail = FALSE))
+  expectValidReport(expectReportSucceeds(reportYieldsCropRaw, detail = TRUE))
+})
