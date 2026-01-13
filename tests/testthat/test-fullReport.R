30e95ff40619b8b1c018e902028b0b6906418e6d
Patrick R <hi@patrickrein.de>
2026-01-13 11:52:32 +0100

    Activates tests with old full data for the two getReport tests

--- tests/testthat/test-fullReport.R
+++ tests/testthat/test-fullReport.R
@@ -17,3 +17,23 @@ test_that("a full report can be generated without errors or warnings", {
 
   expectValidReport(report)
 })
+
+test_that("a full report for an older fulldata.gdx can be generated without errors or warnings", {
+  skip_on_cran()
+  run_only_if_full_tests_requested()
+
+  gdxPath <- fullDataGdxPath("magpie-old-default")
+  skip_if_not(file.exists(gdxPath))
+
+  # Run getReport and check for error messages
+  report <- NULL
+  expect_no_warning(
+    output <- capture_messages(report <- getReport(gdxPath))
+  )
+
+  # No report function throws an error
+  expect_no_match(!!paste0(output, collapse = ""),
+                  "ERROR .*")
+
+  expectValidReport(report)
+})
