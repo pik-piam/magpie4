971cff9ce08114423bf3367cd58fc03920c3a916
Patrick R <hi@patrickrein.de>
2026-01-27 13:43:07 +0100

    Activates flexible aggregation for CostCapitalStocks

--- R/reportCostCapitalStocks.R
+++ R/reportCostCapitalStocks.R
@@ -2,6 +2,8 @@
 #' @description reports MAgPIE capital stocks
 #'
 #' @param gdx GDX file
+#' @param level An aggregation level for the spatial dimension. Can be any level
+#' available via superAggregateX.
 #' @return Magpie object associated with overall costs and value of production
 #' @author Edna J. Molina Bacca
 #' @examples
@@ -16,10 +18,10 @@
 #' Capital Stocks\|Arable farm capital | million US$2017 | Capital stocks used in cropland (sticky cost implementation)
 #' @md
 #' @export
-reportCostCapitalStocks <- function(gdx) {
+reportCostCapitalStocks <- function(gdx, level = "regglo") {
 
   # Capital stocks used in croland per region
-  x <- try(CostCapital(gdx, level = "regglo", type = "stocks"), silent = TRUE)
+  x <- try(CostCapital(gdx, level = level, type = "stocks"), silent = TRUE)
 
   if ("try-error" %in% class(x)) {
     message("Info only available for sticky cost implementation")
