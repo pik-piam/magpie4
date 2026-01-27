c4c8b22bce96208c1c18cfd648af6601366ba4eb
Patrick Rein <patrick.rein@pik-potsdam.de>
2026-01-27 11:06:31 +0100

    Reworks tryList and tryReport to use named lists as results and moves output generation to tryList

--- R/tryReport.R
+++ R/tryReport.R
@@ -1,16 +1,56 @@
+#' @title reportCondition
+#' @description Create a named list representing the outcome of a report
+#' @param resultType Character string for the result type
+#' @param message Character string with the result message
+#' @param report The report name that was run
+#' @param elapsed The elapsed time for the report in seconds
+#' @param result The result magpie object or NULL
+#' @return A named list with report result information
+#' @keywords internal
+reportResult <- function(resultType, message, report, elapsed, result = NULL) {
+  elapsedString <- paste0(" ", format(elapsed, nsmall = 2, digits = 2), "s")
+  list(
+    type = resultType,
+    message = message,
+    report = report,
+    elapsed = elapsedString,
+    result = result
+  )
+}
+
+reportSuccess <- function(report, elapsed, result) {
+  reportResult("success", "success", report, elapsed, result)
+}
+
+reportError <- function(report, elapsed, errorObject) {
+  reportResult("error", paste0("ERROR ", errorObject), report, elapsed)
+}
+
+reportValidationError <- function(report, elapsed, reason) {
+  msg <- paste0("VALIDATION ERROR - ", reason)
+  reportResult("validationError", msg, report, elapsed)
+}
+
+reportWarning <- function(report, elapsed, reason) {
+  reportResult("warning", reason, report, elapsed)
+}
+
 #' @title tryReport
 #' @description Internal support function to run a reporting in a try environment
 #' and properly report problems if something goes wrong without stopping the
-#' further processing in case of an error
+#' further processing in case of an error.
 #'
 #' @param report report function to be run
 #' @param width  max number of characters per line
 #' @param gdx gdx file to report from
-#' @param level spatial level (either "regglo" for region+global, "iso" for country-level, or the file of a mapping file)
-#' @param n number of parent generations to go back when catching the environment
-#' the report should get evaluated in
+#' @param level spatial level (either "regglo" for region+global, "iso" for country-level,
+#' or the file of a mapping file).
+#' @param env environment to evaluate the report in
+#' @return A named list with information on the outcome of the report (success, error, validationError, warning)
 #' @author Jan Philipp Dietrich
 #' @importFrom gdx2 readGDX
+#' @seealso \code{\link{reportCondition}}, \code{\link{reportSuccess}}, \code{\link{reportError}},
+#' \code{\link{reportValidationError}}, \code{\link{reportWarning}}
 
 tryReport <- function(report, width, gdx, level = "regglo", env = NULL) {
   additionalRegs <- NULL
@@ -31,36 +71,30 @@ tryReport <- function(report, width, gdx, level = "regglo", env = NULL) {
   }
 
   years <- readGDX(gdx, "t")
-  message("   ", format(report, width = width), appendLF = FALSE)
   t <- system.time(x <- try(eval(parse(text = paste0("suppressMessages(", report, ")")), env),
                             silent = TRUE))
-  t <- paste0(" ", format(t["elapsed"], nsmall = 2, digits = 2), "s")
-  if (is(x, "try-error")) {
-    message("ERROR", x, t)
-    x <- NULL
+  elapsed <- t["elapsed"]
+
+  cond <- if (is(x, "try-error")) {
+    reportError(report, elapsed, "execution failed", x)
   } else if (is.null(x)) {
-    message("no return value", t)
-    x <- NULL
+    reportWarning(report, elapsed, "no return value")
   } else if (is.character(x)) {
-    message(x, t)
-    x <- NULL
+    reportWarning(report, elapsed, x)
   } else if (!is.magpie(x)) {
-    message("ERROR - no magpie object", t)
-    x <- NULL
+    reportValidationError(report, elapsed, "no magpie object")
   } else if (!setequal(getYears(x), years)) {
-    message("ERROR - wrong years", t)
-    x <- NULL
+    reportValidationError(report, elapsed, "wrong years")
   } else if (!setequal(getItems(x, dim = 1), regs) &&
                (is.null(additionalRegs) || !setequal(getItems(x, dim = 1), additionalRegs))) {
     # The condition above should be removed as soon as all sub-reports
     # respect the report level (see comment above).
-    message("ERROR - wrong regions", t)
-    x <- NULL
+    reportValidationError(report, elapsed, "wrong regions")
   } else if (any(grepl(".", getNames(x), fixed = TRUE))) {
-    message("ERROR - data names contain dots (.)", t)
-    x <- NULL
+    reportValidationError(report, elapsed, "data names contain dots (.)")
   } else {
-    message("success", t)
+    reportSuccess(report, elapsed, x)
   }
-  return(x)
+
+  return(cond)
 }
