6a11ab5443bee5b544b221e573d5bf5e75dede79
Patrick R <hi@patrickrein.de>
2026-02-02 13:53:16 +0100

    Activates flexible aggregation for Peatland

--- R/PeatlandArea.R
+++ R/PeatlandArea.R
@@ -5,7 +5,9 @@
 #'
 #' @param gdx GDX file
 #' @param file a file name the output should be written to using write.magpie
-#' @param level Level of regional aggregation; "cell", "reg" (regional), "glo" (global), "regglo" (regional and global) or any aggregation level defined in superAggregate. In addition "climate" for the 3 climate regions tropical, temperate and boreal is available.
+#' @param level Level of regional aggregation; "cell", "reg" (regional), "glo" (global),
+#' "regglo" (regional and global) or any aggregation level defined in superAggregateX.
+#' In addition "climate" for the 3 climate regions tropical, temperate and boreal is available.
 #' @param sum sum over land types TRUE (default) or FALSE
 #' @details Intact, degraded and rewettet peatland area
 #' @return Peatland area in Mha
@@ -18,40 +20,45 @@
 #'     x <- PeatlandArea(gdx)
 #'   }
 
-PeatlandArea <- function(gdx, file=NULL, level="cell", sum=TRUE){
+PeatlandArea <- function(gdx, file = NULL, level = "cell", sum = TRUE) {
 
-  a <- readGDX(gdx,"ov58_peatland","ov58_peatland_man",select=list(type="level"),react = "silent")
-  if(!is.null(a)) {
-    if(names(dimnames(a))[[3]] == "man58.land58") {
-      if(sum) {
-        a <- dimSums(a,dim=c(3.2))
-        a[,,"degrad"] <- a[,,"degrad"] + collapseNames(a[,,"unused"])
-        a <- a[,,"unused",invert=TRUE]
-        b <- readGDX(gdx,"ov58_peatland_intact",select=list(type="level"))
+  a <- readGDX(gdx, "ov58_peatland", "ov58_peatland_man", select = list(type = "level"), react = "silent")
+  if (!is.null(a)) {
+    if (names(dimnames(a))[[3]] == "man58.land58") {
+      if (sum) {
+        a <- dimSums(a, dim = c(3.2))
+        a[, , "degrad"] <- a[, , "degrad"] + collapseNames(a[, , "unused"])
+        a <- a[, , "unused", invert = TRUE]
+        b <- readGDX(gdx, "ov58_peatland_intact", select = list(type = "level"))
         getNames(b) <- "intact"
-        a <- mbind(a,b)
+        a <- mbind(a, b)
       } else {
-        b <- readGDX(gdx,"ov58_peatland_intact",select=list(type="level"))
+        b <- readGDX(gdx, "ov58_peatland_intact", select = list(type = "level"))
         getNames(b) <- "intact.intact"
-        a <- mbind(a,b)
+        a <- mbind(a, b)
       }
     } else if (names(dimnames(a))[[3]] == "land58") {
-      if(sum) {
-        b <- new.magpie(getCells(a),getYears(a),c("degrad","intact","rewet"))
-        b[,,"degrad"] <- dimSums(a[,,c("crop","past","forestry","unused","peatExtract")],dim=3)
-        b[,,"intact"] <- dimSums(a[,,c("intact")],dim=3)
-        b[,,"rewet"] <- dimSums(a[,,c("rewetted")],dim=3)
+      if (sum) {
+        b <- new.magpie(getCells(a), getYears(a), c("degrad", "intact", "rewet"))
+        b[, , "degrad"] <- dimSums(a[, , c("crop", "past", "forestry", "unused", "peatExtract")], dim =
+                                     3)
+        b[, , "intact"] <- dimSums(a[, , c("intact")], dim = 3)
+        b[, , "rewet"] <- dimSums(a[, , c("rewetted")], dim = 3)
         a <- b
-      } else{
+      } else {
         a <- a
       }
     }
 
-    if(level == "climate") {
-      map_cell_clim <- readGDX(gdx,"p58_mapping_cell_climate")
-      a <- dimSums(map_cell_clim*a,dim=1)
-    } else if (level != "cell") a <- superAggregate(a, aggr_type = "sum", level = level,na.rm = FALSE)
-  } else a <- NULL
+    if (level == "climate") {
+      mappingCellClimate <- readGDX(gdx, "p58_mapping_cell_climate")
+      a <- dimSums(mappingCellClimate * a, dim = 1)
+    } else if (level != "cell") {
+      a <- superAggregateX(a, aggr_type = "sum", level = level)
+    }
+  } else {
+    a <- NULL
+  }
 
-  out(a,file)
+  out(a, file)
 }
--- R/reportPeatland.R
+++ R/reportPeatland.R
@@ -1,17 +1,16 @@
 #' @title reportPeatland
 #' @description reports peatland area
-#' 
+#'
 #' @export
-#' 
+#'
 #' @param gdx GDX file
 #' @return peatland area as magclass object (million ha)
 #' @author Florian Humpenoeder
 #' @examples
-#' 
+#'
 #'   \dontrun{
 #'     x <- reportPeatland(gdx)
 #'   }
-#' 
 #'
 #' @section Peatland variables:
 #' Name | Unit | Meta
@@ -21,20 +20,17 @@
 #' Resources\|Peatland\|+\|Degraded | million ha | Area of drained peatlands
 #' Resources\|Peatland\|+\|Rewetted | million ha | Area of rewetted peatlands
 #' @md
+reportPeatland <- function(gdx, level = "regglo") {
 
-
-reportPeatland <- function(gdx) {
-  
   x <- NULL
-  
-  a <- PeatlandArea(gdx,level="regglo")
-  if(!is.null(a)) {
-    x <- mbind(x,setNames(dimSums(a,dim=3),"Resources|Peatland (million ha)"))
-    x <- mbind(x,setNames(a[,,"intact"],paste0("Resources|Peatland|+|", reportingnames(getNames(a[,,"intact"],dim=1))," (million ha)")))
-    x <- mbind(x,setNames(a[,,"degrad"],paste0("Resources|Peatland|+|", reportingnames(getNames(a[,,"degrad"],dim=1))," (million ha)"))) 
-    x <- mbind(x,setNames(a[,,"rewet"],paste0("Resources|Peatland|+|", reportingnames(getNames(a[,,"rewet"],dim=1))," (million ha)")))
+
+  a <- PeatlandArea(gdx, level = level)
+  if (!is.null(a)) {
+    x <- mbind(x, setNames(dimSums(a, dim = 3), "Resources|Peatland (million ha)"))
+    x <- mbind(x, setNames(a[, , "intact"], paste0("Resources|Peatland|+|", reportingnames(getNames(a[, , "intact"], dim = 1)), " (million ha)")))
+    x <- mbind(x, setNames(a[, , "degrad"], paste0("Resources|Peatland|+|", reportingnames(getNames(a[, , "degrad"], dim = 1)), " (million ha)"))) 
+    x <- mbind(x, setNames(a[, , "rewet"], paste0("Resources|Peatland|+|", reportingnames(getNames(a[, , "rewet"], dim = 1)), " (million ha)")))
   }
-  
+
   return(x)
 }
-
