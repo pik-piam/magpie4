0cd3b566700f0613a9c7a9d5eef7d5f9d42d2f0f
Patrick R <hi@patrickrein.de>
2026-02-02 11:34:46 +0100

    Reformating of Kcal.R

--- R/Kcal.R
+++ R/Kcal.R
@@ -32,86 +32,102 @@
 #'
 
 Kcal <- function(gdx,
-                 file=NULL,
-                 level="reg",
-                 products="kfo",
-                 product_aggr=TRUE,
-                 after_shock=TRUE,
-                 calibrated=TRUE,
-                 magpie_input=FALSE,
-                 attributes="kcal",
-                 per_capita=TRUE){
+                 file = NULL,
+                 level = "reg",
+                 products = "kfo",
+                 product_aggr = TRUE,
+                 after_shock = TRUE,
+                 calibrated = TRUE,
+                 magpie_input = FALSE,
+                 attributes = "kcal",
+                 per_capita = TRUE) {
 
-  if(magpie_input==TRUE & after_shock==FALSE) {stop("magpie_input and after_shock==FALSE ")}
+  if (magpie_input == TRUE && after_shock == FALSE) {
+    stop("magpie_input and after_shock==FALSE ")
+  }
 
   # retrieve the right data
-  if (calibrated==FALSE){
-
-    if (after_shock==TRUE){
-      out<-readGDX(gdx=gdx,"p15_kcal_regr")
-    } else if (after_shock==FALSE){
-      out<-readGDX(gdx=gdx,"o15_kcal_regr_initial")
-    } else {stop("after_shock has to be binary")}
-
+  if (calibrated == FALSE) {
+    if (after_shock == TRUE) {
+      out <- readGDX(gdx = gdx, "p15_kcal_regr")
+    } else if (after_shock == FALSE) {
+      out <- readGDX(gdx = gdx, "o15_kcal_regr_initial")
+    } else {
+      stop("after_shock has to be binary")
+    }
   } else {
-    if (after_shock==TRUE){
+    if (after_shock == TRUE) {
       # When reporting dietary indicators, good practice is to use the values as estimated in the diet module.
       # As the diet module also includes countries not covered by FAOSTAT, the diet module estimates
       # and the MAgPIE/FAOSTAT total do not match. Therefore, if you are interested in what drivers
       # MAgPIE, or if you want to use a version that matches FAOSTAT, you can used the magpie_input=TRUE.
 
-      if (magpie_input==FALSE){
-        out<-readGDX(gdx=gdx,"p15_kcal_pc_iso")
+      if (magpie_input == FALSE) {
+        out <- readGDX(gdx = gdx, "p15_kcal_pc_iso")
       } else {
-        if(level%in%c("reg","glo","regglo","GLO")){
-           kcal_pc_calibrated <- readGDX(gdx = gdx, "p15_kcal_pc_calibrated", react = "quiet")
-           if(is.null(kcal_pc_calibrated)){
-           kcal_pc_calibrated <- readGDX(gdx = gdx, "pm_kcal_pc_calibrated")
-           }
-          balance_flow<-readGDX(gdx=gdx,"p15_balanceflow_kcal")
+        if (level %in% c("reg", "glo", "regglo", "GLO")) {
+          kcal_pc_calibrated <- readGDX(gdx = gdx,
+                                        "p15_kcal_pc_calibrated",
+                                        react = "quiet")
+          if (is.null(kcal_pc_calibrated)) {
+            kcal_pc_calibrated <- readGDX(gdx = gdx, "pm_kcal_pc_calibrated")
+          }
+          balance_flow <- readGDX(gdx = gdx, "p15_balanceflow_kcal")
           #revert MAgPIE-internal calibration: While the food demand model estimates demand for all countries,
           #FAOSTAT only covers a subset. To match FAOSTAT totals, the food demand of countries not included
           #in FAOSTAT is calibrated to zero in MAgPIE.
           out <- kcal_pc_calibrated - balance_flow
-        } else {stop("magpie_input for kcal cannot be reported below regional level")}
+        } else {
+          stop("magpie_input for kcal cannot be reported below regional level")
+        }
       }
-    } else if (after_shock==FALSE){
-      out<-readGDX(gdx=gdx,"p15_kcal_pc_initial_iso")
-    } else {stop("after_shock has to be binary")}
+    } else if (after_shock == FALSE) {
+      out <- readGDX(gdx = gdx, "p15_kcal_pc_initial_iso")
+    } else {
+      stop("after_shock has to be binary")
+    }
   }
 
-  out<-gdxAggregate(gdx = gdx,x = out,weight = 'population',to = level,absolute = FALSE)
+  out <- gdxAggregate(
+    gdx = gdx,
+    x = out,
+    weight = "population",
+    to = level,
+    absolute = FALSE
+  )
 
 
-  if (identical("kall",products)){
-    missing=setdiff(readGDX(gdx,"kall"),getNames(out))
-    out<-add_columns(out,dim = 3.1,addnm = missing)
-    out[,,missing]=0
-  } else if (!all(products%in%readGDX(gdx,"kall"))){
-      products<-findset(products)
-      out<-out[,,products]
+  if (identical("kall", products)) {
+    missing = setdiff(readGDX(gdx, "kall"), getNames(out))
+    out <- add_columns(out, dim = 3.1, addnm = missing)
+    out[, , missing] = 0
+  } else if (!all(products %in% readGDX(gdx, "kall"))) {
+    products <- findset(products)
+    out <- out[, , products]
   } else {
-    out<-out[,,products]
+    out <- out[, , products]
   }
 
-  if(any(attributes!="kcal")){
-    att=att2=readGDX(gdx=gdx,"fm_nutrition_attributes")[,getYears(out),getNames(out,dim=1)]
-    att2<-att2[,,"protein"]*365/6.25/1000000
-    getNames(att2,dim = 2)<-"nr"
-    att<-mbind(att,att2)
-    out<-out/collapseNames(att[,,"kcal"],collapsedim = 2)*att[,,attributes]
-    out[is.na(out)]<-0
+  if (any(attributes != "kcal")) {
+    att = att2 = readGDX(gdx = gdx, "fm_nutrition_attributes")[, getYears(out), getNames(out, dim =
+                                                                                           1)]
+    att2 <- att2[, , "protein"] * 365 / 6.25 / 1000000
+    getNames(att2, dim = 2) <- "nr"
+    att <- mbind(att, att2)
+    out <- out / collapseNames(att[, , "kcal"], collapsedim = 2) * att[, , attributes]
+    out[is.na(out)] <- 0
   }
 
-  if(product_aggr){out<-dimSums(out,dim=3.1)}
+  if (product_aggr) {
+    out <- dimSums(out, dim = 3.1)
+  }
 
   if (per_capita) {
-    out=out
+    out = out
   } else {
-    pop=population(gdx=gdx,level=level)
-    out=out*pop
+    pop = population(gdx = gdx, level = level)
+    out = out * pop
   }
 
-
-  out(out,file)
+  out(out, file)
 }
