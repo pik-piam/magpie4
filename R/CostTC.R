fd2b5ec4fe599b9f68435689469d01ae7f81902a
Patrick R <hi@patrickrein.de>
2026-02-05 15:07:21 +0100

    Fixes a missing superAggregateX in CostTC

--- R/CostTC.R
+++ R/CostTC.R
@@ -8,26 +8,25 @@
 #' @param level Level of regional aggregation; "reg" (regional), "glo" (global), "regglo" (regional and global) or any other aggregation level defined in superAggregate
 #' @return A MAgPIE object containing values related with overall value of production [million US$17]
 #' @author David Chen
-#' @importFrom luscale superAggregate
 #' @examples
 #'
 #'   \dontrun{
 #'     x <- CostTC(gdx)
 #'   }
 #'
-
-CostTC <- function(gdx,file=NULL,level="reg"){
+CostTC <- function(gdx, file = NULL, level = "reg"){
 
   #Reads existing capital in each time step
 
-  tc_cost <- readGDX(gdx,"ov_tech_cost", format = "first_found", select = list(type="level"))
+  tc_cost <- readGDX(gdx, "ov_tech_cost", format = "first_found", select = list(type = "level"))
 
   tc_cost <- dimSums(tc_cost, dim = 3)
 
   getNames(tc_cost) <- "TC Costs"
 
-  if (level != "reg") tc_cost <- superAggregate(tc_cost, aggr_type = "sum", level = level)
-  out <- tc_cost
+  if (level != "reg") {
+    tc_cost <- superAggregateX(tc_cost, aggr_type = "sum", level = level)
+  }
 
-  out(out,file)
+  out(tc_cost, file)
 }
